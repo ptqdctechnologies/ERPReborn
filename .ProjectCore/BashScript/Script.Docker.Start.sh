6ea58e66383bd08009cfc73704a22ac3bea9bfd3
PT QDC Technologies <pt.qdc.technologies@gmail.com>
2020-06-17 17:47:54 +0700

    Perbaikan bugs KILL PROCESS mechanism di Script.Docker.Start.sh

--- .ProjectCore/BashScript/Script.Docker.Start.sh
+++ .ProjectCore/BashScript/Script.Docker.Start.sh
@@ -1,7 +1,7 @@
 #----------------------------------------------------------------------------------------------------
 # ▪ Nama               : Script.Docker.Start.sh
-# ▪ Versi              : 1.00.0000
-# ▪ Tanggal            : 2020-06-11
+# ▪ Versi              : 1.00.0001
+# ▪ Tanggal            : 2020-06-17
 # ▪ Input              : -
 # ▪ Output             : -
 # ▪ Deskripsi          : Script ini digunakan untuk menjalankan Docker melalui Compose-Up
@@ -19,24 +19,10 @@ clear;
 #sudo echo never > /sys/kernel/mm/transparent_hugepage/enabled;
 
 #---> General
-
 sudo docker network prune --force;
 sudo docker container prune --force;
-#nohup ./BashScript/Script.System.WatchDog.Docker.ContainerPostgreSQL.sh >/dev/null 2>&1;
-#nohup ./BashScript/Script.System.WatchDog.Docker.ContainerPostgreSQL.sh </dev/null >/dev/null 2>&1
-#nohup ./BashScript/Script.System.WatchDog.Docker.ContainerPostgreSQL.sh &
 sudo ./BashScript/Script.System.WatchDog.Docker.ContainerPostgreSQL.sh &
 sudo docker-compose up --remove-orphans;
-varProcessID=`ps aux | grep BashScript/Script.System.WatchDog.Docker.ContainerPostgreSQL.sh | grep /bin/bash | awk '{print $2}'`;
-sudo kill -s 9 $varProcessID;
-
-
-#sudo docker-compose up --remove-orphans \
-#  | parallel sleep 20 \
-#     && sudo docker exec -it postgresql service mysql status;
-
-  
-#   \
-#   && sleep 20 \
-#   && sudo docker exec -it postgresql "/usr/local/bin/Script.ServiceRestart.sh";
-#sudo docker exec -it postgresql -c "";
+varCmdExec="sudo kill -s 9 "`ps aux | grep "Script.System.WatchDog.Docker.ContainerPostgreSQL.sh" | grep -v "\-\-color" | awk '{print $2}'`";";
+#varResult=$(eval $varCmdExec) 2>/dev/null
+eval $varCmdExec 2>/dev/null;
