e2a51628b8f8731a076314e9e5aca9123d356108
PT QDC Technologies <pt.qdc.technologies@gmail.com>
2020-06-18 15:09:24 +0700

    Perubahan kepemilikan folder /bootstrap/cache agar dapat dilakukan penulisan data

--- .ProjectCore/BashScript/Script.Docker.Reinitializing.LaravelFolderOwnership.sh
+++ .ProjectCore/BashScript/Script.Docker.Reinitializing.LaravelFolderOwnership.sh
@@ -2,3 +2,5 @@
 
 sudo chown -R 33:33 ./Programming/WebBackEnd/storage/;
 sudo chown -R 33:33 ./Programming/WebFrontEnd/storage/;
+sudo chown -R 33:33 ./Programming/WebBackEnd/bootstrap/cache;
+sudo chown -R 33:33 ./Programming/WebFrontEnd/bootstrap/cache;
