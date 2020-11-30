e89fca50ae5bb34a1e260abbd08b236db179a5ca
Teguh Pratama Januzir S <teguhpjs@gmail.com>
2020-11-30 11:09:07 +0700

    Update Script.Docker.Reinitializing.Database.PostgreSQL.sh

--- .ProjectCore/BashScript/Script.Docker.Reinitializing.Database.PostgreSQL.sh
+++ .ProjectCore/BashScript/Script.Docker.Reinitializing.Database.PostgreSQL.sh
@@ -19,9 +19,9 @@ clear;
 
 varCmd='sudo docker exec -it postgresql /bin/bash -c';
 
-varCmdContainer='psql -U postgres -d postgres -c';
 varRoleName='SysEngine';
 varRolePassword='748159263';
+varCmdContainer='psql -U postgres -d postgres -c';
 echo "---> Initializing Role : "$varRoleName" With Password "$varRolePassword;
 $varCmd "$varCmdContainer \"REASSIGN OWNED BY \\\""$varRoleName"\\\" TO postgres; DROP OWNED BY \\\""$varRoleName"\\\";\"";
 $varCmd "$varCmdContainer \"DROP ROLE IF EXISTS \\\""$varRoleName"\\\";\"";
@@ -34,80 +34,119 @@ $varCmd "$varCmdContainer \"DROP ROLE IF EXISTS \\\""$varRoleName"\\\";\"";
 $varCmd "$varCmdContainer \"CREATE ROLE \\\""$varRoleName"\\\" LOGIN SUPERUSER CREATEROLE CREATEDB PASSWORD '"$varRolePassword"';\"";
 echo "";
 
+varRoleName='SysEngine';
+varRolePassword='748159263';
 
+#+-------------------------------------------------------------------------------------------------+
+#| Database : dbERPReborn-SysConfig                                                                |
+#+-------------------------------------------------------------------------------------------------+
 varDBName='dbERPReborn-SysConfig';
 varFileName='/var/lib/postgresql/temp/dump.sql';
 varDBMasterHost='192.168.1.24';
 varDBMasterPort='5432';
-#echo "---> Reinitializing Database : "$varDBName;
-#sudo docker exec -it postgresql /bin/bash -c "psql -U postgres -d postgres -c \"DROP DATABASE IF EXISTS \\\""$varDBName"\\\";\"";
-#sudo docker exec -it postgresql /bin/bash -c "psql -U postgres -d postgres -c \"CREATE DATABASE \\\""$varDBName"\\\" OWNER \\\""$varRoleName"\\\";\"";
-#echo "   ---> Database Cloning : "$varDBName;
-#sudo docker exec -it postgresql /bin/bash -c "PGPASSWORD=\""$varRolePassword"\" pg_dump -h "$varDBMasterHost" -p "$varDBMasterPort" -U \""$varRoleName"\" --format plain --encoding UTF8 \""$varDBName"\" > "$varFileName;
-#sudo docker exec -it postgresql /bin/bash -c "psql -U \""$varRoleName"\" -d \""$varDBName"\" < "$varFileName";";
-#sudo docker exec -it postgresql /bin/bash -c "rm -rf "$varFileName;
+
+echo "---> Reinitializing Database : "$varDBName;
+varCmdContainer='psql -U postgres -d postgres -c';
+$varCmd "$varCmdContainer \"DROP DATABASE IF EXISTS \\\""$varDBName"\\\";\"";
+$varCmd "$varCmdContainer \"CREATE DATABASE \\\""$varDBName"\\\" OWNER \\\""$varRoleName"\\\";\"";
+
+echo "   ---> Database Cloning : "$varDBName;
+$varCmd "PGPASSWORD=\""$varRolePassword"\" pg_dump -h "$varDBMasterHost" -p "$varDBMasterPort" -U \""$varRoleName"\" --format plain --encoding UTF8 \""$varDBName"\" > "$varFileName;
+$varCmd "psql -U \""$varRoleName"\" -d \""$varDBName"\" < "$varFileName";";
+$varCmd "rm -rf "$varFileName;
+
 echo "---> Reinitializing Environment Parameter on Database";
 varCmdContainer='psql -U "SysEngine" -d "'$varDBName'" -c';
 $varCmd "$varCmdContainer \"UPDATE \\\"SchSysConfig\\\".\\\"TblDBObject_Parameter\\\" SET \\\"Value\\\" = '172.28.0.2' WHERE \\\"Key\\\" = 'DB.BackEnd.DataOLAP.Host' OR \\\"Key\\\" = 'DB.BackEnd.DataBinaryObject.Host' OR \\\"Key\\\" = 'DB.BackEnd.DataOLTP.Host' OR \\\"Key\\\" = 'DB.BackEnd.SysConfig.Host' OR \\\"Key\\\" = 'DB.FrontEnd.Host';\"";
-#echo "";
+
+echo "";
 
 
+
+#+-------------------------------------------------------------------------------------------------+
+#| Database : dbERPReborn-Data-OLTP                                                                |
+#+-------------------------------------------------------------------------------------------------+
 varDBName='dbERPReborn-Data-OLTP';
 varFileName='/var/lib/postgresql/temp/dump.sql';
 varDBMasterHost='192.168.1.24';
 varDBMasterPort='5432';
-#echo "---> Reinitializing Database : "$varDBName;
-#sudo docker exec -it postgresql /bin/bash -c "psql -U postgres -d postgres -c \"DROP DATABASE IF EXISTS \\\""$varDBName"\\\";\"";
-#sudo docker exec -it postgresql /bin/bash -c "psql -U postgres -d postgres -c \"CREATE DATABASE \\\""$varDBName"\\\" OWNER \\\""$varRoleName"\\\";\"";
-#echo "   ---> Database Cloning : "$varDBName;
-#sudo docker exec -it postgresql /bin/bash -c "PGPASSWORD=\""$varRolePassword"\" pg_dump -h "$varDBMasterHost" -p "$varDBMasterPort" -U \""$varRoleName"\" --format plain --encoding UTF8 \""$varDBName"\" > "$varFileName;
-#sudo docker exec -it postgresql /bin/bash -c "psql -U \""$varRoleName"\" -d \""$varDBName"\" < "$varFileName";";
-#sudo docker exec -it postgresql /bin/bash -c "rm -rf "$varFileName;
-#echo "";
+
+echo "---> Reinitializing Database : "$varDBName;
+varCmdContainer='psql -U postgres -d postgres -c';
+$varCmd "$varCmdContainer \"DROP DATABASE IF EXISTS \\\""$varDBName"\\\";\"";
+$varCmd "$varCmdContainer \"CREATE DATABASE \\\""$varDBName"\\\" OWNER \\\""$varRoleName"\\\";\"";
+
+echo "   ---> Database Cloning : "$varDBName;
+$varCmd "PGPASSWORD=\""$varRolePassword"\" pg_dump -h "$varDBMasterHost" -p "$varDBMasterPort" -U \""$varRoleName"\" --format plain --encoding UTF8 \""$varDBName"\" > "$varFileName;
+$varCmd "psql -U \""$varRoleName"\" -d \""$varDBName"\" < "$varFileName";";
+$varCmd "rm -rf "$varFileName;
+
+echo "";
 
 
+
+#+-------------------------------------------------------------------------------------------------+
+#| Database : dbERPReborn-Data-OLAP                                                                |
+#+-------------------------------------------------------------------------------------------------+
 varDBName='dbERPReborn-Data-OLAP';
 varFileName='/var/lib/postgresql/temp/dump.sql';
 varDBMasterHost='192.168.1.24';
 varDBMasterPort='5432';
-#echo "---> Reinitializing Database : "$varDBName;
-#sudo docker exec -it postgresql /bin/bash -c "psql -U postgres -d postgres -c \"DROP DATABASE IF EXISTS \\\""$varDBName"\\\";\"";
-#sudo docker exec -it postgresql /bin/bash -c "psql -U postgres -d postgres -c \"CREATE DATABASE \\\""$varDBName"\\\" OWNER \\\""$varRoleName"\\\";\"";
-#echo "   ---> Database Cloning : "$varDBName;
-#sudo docker exec -it postgresql /bin/bash -c "PGPASSWORD=\""$varRolePassword"\" pg_dump -h "$varDBMasterHost" -p "$varDBMasterPort" -U \""$varRoleName"\" --format plain --encoding UTF8 \""$varDBName"\" > "$varFileName;
-#sudo docker exec -it postgresql /bin/bash -c "psql -U \""$varRoleName"\" -d \""$varDBName"\" < "$varFileName";";
-#sudo docker exec -it postgresql /bin/bash -c "rm -rf "$varFileName;
-#echo "";
+echo "---> Reinitializing Database : "$varDBName;
+varCmdContainer='psql -U postgres -d postgres -c';
+$varCmd "$varCmdContainer \"DROP DATABASE IF EXISTS \\\""$varDBName"\\\";\"";
+$varCmd "$varCmdContainer \"CREATE DATABASE \\\""$varDBName"\\\" OWNER \\\""$varRoleName"\\\";\"";
+echo "   ---> Database Cloning : "$varDBName;
+$varCmd "PGPASSWORD=\""$varRolePassword"\" pg_dump -h "$varDBMasterHost" -p "$varDBMasterPort" -U \""$varRoleName"\" --format plain --encoding UTF8 \""$varDBName"\" > "$varFileName;
+$varCmd "psql -U \""$varRoleName"\" -d \""$varDBName"\" < "$varFileName";";
+$varCmd "rm -rf "$varFileName;
+echo "";
 
 
+
+#+-------------------------------------------------------------------------------------------------+
+#| Database : dbERPReborn-Data-BinaryObject                                                        |
+#+-------------------------------------------------------------------------------------------------+
 varDBName='dbERPReborn-Data-BinaryObject';
 varFileName='/var/lib/postgresql/temp/dump.sql';
 varDBMasterHost='192.168.1.24';
 varDBMasterPort='5432';
-#echo "---> Reinitializing Database : "$varDBName;
-#sudo docker exec -it postgresql /bin/bash -c "psql -U postgres -d postgres -c \"DROP DATABASE IF EXISTS \\\""$varDBName"\\\";\"";
-#sudo docker exec -it postgresql /bin/bash -c "psql -U postgres -d postgres -c \"CREATE DATABASE \\\""$varDBName"\\\" OWNER \\\""$varRoleName"\\\";\"";
-#echo "   ---> Database Cloning : "$varDBName;
-#sudo docker exec -it postgresql /bin/bash -c "PGPASSWORD=\""$varRolePassword"\" pg_dump -h "$varDBMasterHost" -p "$varDBMasterPort" -U \""$varRoleName"\" --format plain --encoding UTF8 \""$varDBName"\" > "$varFileName;
-#sudo docker exec -it postgresql /bin/bash -c "psql -U \""$varRoleName"\" -d \""$varDBName"\" < "$varFileName";";
-#sudo docker exec -it postgresql /bin/bash -c "rm -rf "$varFileName;
-#echo "";
+
+echo "---> Reinitializing Database : "$varDBName;
+varCmdContainer='psql -U postgres -d postgres -c';
+$varCmd "$varCmdContainer \"DROP DATABASE IF EXISTS \\\""$varDBName"\\\";\"";
+$varCmd "$varCmdContainer \"CREATE DATABASE \\\""$varDBName"\\\" OWNER \\\""$varRoleName"\\\";\"";
+
+echo "   ---> Database Cloning : "$varDBName;
+$varCmd "PGPASSWORD=\""$varRolePassword"\" pg_dump -h "$varDBMasterHost" -p "$varDBMasterPort" -U \""$varRoleName"\" --format plain --encoding UTF8 \""$varDBName"\" > "$varFileName;
+$varCmd "psql -U \""$varRoleName"\" -d \""$varDBName"\" < "$varFileName";";
+$varCmd "rm -rf "$varFileName;
+
+echo "";
 
 
+
+#+-------------------------------------------------------------------------------------------------+
+#| Database : dbERPReborn                                                                          |
+#+-------------------------------------------------------------------------------------------------+
 varDBName='dbERPReborn';
 varFileName='/var/lib/postgresql/temp/dump.sql';
 varDBMasterHost='192.168.1.24';
 varDBMasterPort='5432';
 
-#echo "---> Reinitializing Database : "$varDBName;
-#sudo docker exec -it postgresql /bin/bash -c "psql -U postgres -d postgres -c \"DROP DATABASE IF EXISTS \\\""$varDBName"\\\";\"";
-#sudo docker exec -it postgresql /bin/bash -c "psql -U postgres -d postgres -c \"CREATE DATABASE \\\""$varDBName"\\\" OWNER \\\""$varRoleName"\\\";\"";
-#echo "   ---> Create Extension : MySQL_FDW";
-#sudo docker exec -it postgresql /bin/bash -c "psql -U \""$varRoleName"\" -d \""$varDBName"\" -c \"CREATE EXTENSION mysql_fdw;\"";
-#echo "   ---> Database Cloning : "$varDBName;
-#sudo docker exec -it postgresql /bin/bash -c "PGPASSWORD=\""$varRolePassword"\" pg_dump -h "$varDBMasterHost" -p "$varDBMasterPort" -U \""$varRoleName"\" --format plain --encoding UTF8 \""$varDBName"\" > "$varFileName;
-#sudo docker exec -it postgresql /bin/bash -c "psql -U \""$varRoleName"\" -d \""$varDBName"\" < "$varFileName";";
-#sudo docker exec -it postgresql /bin/bash -c "rm -rf "$varFileName;
+echo "---> Reinitializing Database : "$varDBName;
+varCmdContainer='psql -U postgres -d postgres -c';
+$varCmd "$varCmdContainer \"DROP DATABASE IF EXISTS \\\""$varDBName"\\\";\"";
+$varCmd "$varCmdContainer \"CREATE DATABASE \\\""$varDBName"\\\" OWNER \\\""$varRoleName"\\\";\"";
+
+echo "   ---> Create Extension : MySQL_FDW";
+$varCmd "psql -U \""$varRoleName"\" -d \""$varDBName"\" -c \"CREATE EXTENSION mysql_fdw;\"";
+
+echo "   ---> Database Cloning : "$varDBName;
+$varCmd "PGPASSWORD=\""$varRolePassword"\" pg_dump -h "$varDBMasterHost" -p "$varDBMasterPort" -U \""$varRoleName"\" --format plain --encoding UTF8 \""$varDBName"\" > "$varFileName;
+$varCmd "psql -U \""$varRoleName"\" -d \""$varDBName"\" < "$varFileName";";
+$varCmd "rm -rf "$varFileName;
+
 echo "---> Initializing Database Link";
 varCmdContainer='psql -U postgres -d "'$varDBName'" -c';
 $varCmd "$varCmdContainer \"CREATE EXTENSION mysql_fdw SCHEMA public;\"";
@@ -115,5 +154,5 @@ $varCmd "$varCmdContainer \"DROP SERVER \\\"dbExtern-MySQL-ERP-QDC\\\" CASCADE;\
 $varCmd "$varCmdContainer \"CREATE SERVER \\\"dbExtern-MySQL-ERP-QDC\\\" FOREIGN DATA WRAPPER mysql_fdw OPTIONS (host '127.0.0.1', port '3306');\"";
 $varCmd "$varCmdContainer \"DROP USER MAPPING FOR \\\"SysEngine\\\" SERVER \\\"dbExtern-MySQL-ERP-QDC\\\";\"";
 $varCmd "$varCmdContainer \"CREATE USER MAPPING FOR \\\"SysEngine\\\" SERVER \\\"dbExtern-MySQL-ERP-QDC\\\" OPTIONS (password '748159263', username 'SysEngine');\"";
-varCmdContainer='psql -U "SysEngine" -d "'$varDBName'" -c';
+varCmdContainer='psql -U "'$varRoleName'" -d "'$varDBName'" -c';
 $varCmd "$varCmdContainer \"SELECT * FROM \\\"SchSysConfig\\\".\\\"FuncSys_KickStart1_ForeignSchema1\\\"('172.28.0.2', '5432');\"";
