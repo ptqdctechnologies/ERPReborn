97f3405068a214778aafaed52961384c47f59d40
Teguh Pratama Januzir S <teguhpjs@gmail.com>
2021-07-24 17:40:41 +0700

    Rekonfigurasi Script untuk mengatasi Error TCPDF terkait ownership fonts

--- .ProjectCore/BashScript/Script.Docker.Reinitializing.LaravelFolderOwnership.sh
+++ .ProjectCore/BashScript/Script.Docker.Reinitializing.LaravelFolderOwnership.sh
@@ -14,3 +14,5 @@ sudo chmod 666 ./Programming/WebFrontEnd/storage/logs/laravel.log;
 
 sudo chmod 777 ./Programming/WebBackEnd/bootstrap/cache;
 sudo chmod 777 ./Programming/WebFrontEnd/bootstrap/cache;
+
+sudo chmod 777 ./Programming/WebBackEnd/vendor/tecnickcom/tcpdf/fonts;
