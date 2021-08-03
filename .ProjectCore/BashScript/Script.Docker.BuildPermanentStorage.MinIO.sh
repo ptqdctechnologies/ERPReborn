51c1136caf72593695890fd62c8763ec29df46e0
Teguh Pratama Januzir S <teguhpjs@gmail.com>
2021-08-03 17:23:22 +0700

    Penambahan API, Penambahan Model, Penambahan Helper, Update Data API Catalogue, Update Store Procedure

--- .ProjectCore/BashScript/Script.Docker.BuildPermanentStorage.MinIO.sh
+++ .ProjectCore/BashScript/Script.Docker.BuildPermanentStorage.MinIO.sh
@@ -1,20 +1,21 @@
 #----------------------------------------------------------------------------------------------------
 # ▪ Nama               : Script.Docker.BuildPermanentStorage.MinIO.sh
-# ▪ Versi              : 1.00.0002
-# ▪ Tanggal            : 2020-11-12
+# ▪ Versi              : 1.00.0003
+# ▪ Tanggal            : 2021-08-03
 # ▪ Input              : -
 # ▪ Output             : -
 # ▪ Deskripsi          : Script ini digunakan untuk memetakan permanent storage MinIO didalam Docker
 # ▪ Execution Syntax   : ./BashScript/Script.Docker.BuildPermanentStorage.MinIO.sh
 #                        <FullPathFromRoot>/BashScript/Script.Docker.BuildPermanentStorage.MinIO.sh
-# ▪ Copyright          : Zheta © 2020
+# ▪ Copyright          : Zheta © 2020, 2021
 #----------------------------------------------------------------------------------------------------
 
 #!/bin/bash
 
 clear;
 
-varDirectory="./../ERPReborn-PermanentStorage/MinIO";
+#varDirectory="./../ERPReborn-PermanentStorage/MinIO";
+varDirectory="./../ERPReborn-PermanentStorage/MinIO/erp-reborn";
 
 if [ ! -d $varDirectory ]; then
    sudo mkdir -p $varDirectory;
