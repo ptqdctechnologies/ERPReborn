f3fb31b6e6554bdbdb6b0a815a073405a1244b78
teguhpjs <teguhpjs@gmail.com>
2021-09-01 09:36:57 +0700

    Penambahan ./BashScript/Script.Docker.Backup.PostgreSQL.StructureOnly.sh

---
+++ .ProjectCore/BashScript/Script.Docker.Backup.PostgreSQL.StructureOnly.sh
@@ -0,0 +1,61 @@
+#----------------------------------------------------------------------------------------------------
+# ▪ Nama               : Script.Docker.Backup.PostgreSQL.StructureOnly.sh
+# ▪ Versi              : 1.00.0000
+# ▪ Tanggal            : 2021-09-01
+# ▪ Input              : -
+# ▪ Output             : -
+# ▪ Deskripsi          : Script ini digunakan untuk men-dump Struktur Database dari Local Database
+#                        pada Container PostgreSQL
+# ▪ Execution Syntax   : ./BashScript/Script.Docker.Backup.PostgreSQL.StructureOnly.sh
+#                        <FullPathFromRoot>/BashScript/Script.Docker.Backup.PostgreSQL.StructureOnly.sh
+# ▪ Copyright          : Zheta © 2021
+#----------------------------------------------------------------------------------------------------
+
+#!/bin/bash
+
+clear;
+
+varCmd='sudo docker exec -it postgresql /bin/bash -c';
+
+varRoleName='SysEngine';
+varRolePassword='748159263';
+varCmdContainer='psql -U postgres -d postgres -c';
+
+#+-------------------------------------------------------------------------------------------------+
+#| Database : dbERPReborn-SysConfig                                                                |
+#+-------------------------------------------------------------------------------------------------+
+varDBName='dbERPReborn-SysConfig';
+varCmdContainer='pg_dump --username "SysEngine" --schema-only --format plain --encoding UTF8 --verbose --file "/zhtConf/databaseStructure/'$varDBName'.sql" "'$varDBName'";';
+$varCmd "$varCmdContainer";
+
+
+#+-------------------------------------------------------------------------------------------------+
+#| Database : dbERPReborn-Data-OLTP                                                                |
+#+-------------------------------------------------------------------------------------------------+
+varDBName='dbERPReborn-Data-OLTP';
+varCmdContainer='pg_dump --username "SysEngine" --schema-only --format plain --encoding UTF8 --verbose --file "/zhtConf/databaseStructure/'$varDBName'.sql" "'$varDBName'";';
+$varCmd "$varCmdContainer";
+
+
+#+-------------------------------------------------------------------------------------------------+
+#| Database : dbERPReborn-Data-OLAP                                                                |
+#+-------------------------------------------------------------------------------------------------+
+varDBName='dbERPReborn-Data-OLAP';
+varCmdContainer='pg_dump --username "SysEngine" --schema-only --format plain --encoding UTF8 --verbose --file "/zhtConf/databaseStructure/'$varDBName'.sql" "'$varDBName'";';
+$varCmd "$varCmdContainer";
+
+
+#+-------------------------------------------------------------------------------------------------+
+#| Database : dbERPReborn-Data-BinaryObject                                                        |
+#+-------------------------------------------------------------------------------------------------+
+varDBName='dbERPReborn-Data-BinaryObject';
+varCmdContainer='pg_dump --username "SysEngine" --schema-only --format plain --encoding UTF8 --verbose --file "/zhtConf/databaseStructure/'$varDBName'.sql" "'$varDBName'";';
+$varCmd "$varCmdContainer";
+
+
+#+-------------------------------------------------------------------------------------------------+
+#| Database : dbERPReborn                                                                          |
+#+-------------------------------------------------------------------------------------------------+
+varDBName='dbERPReborn';
+varCmdContainer='pg_dump --username "SysEngine" --schema-only --format plain --encoding UTF8 --verbose --file "/zhtConf/databaseStructure/'$varDBName'.sql" "'$varDBName'";';
+$varCmd "$varCmdContainer";
