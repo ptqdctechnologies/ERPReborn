fae8646de8e27782a4c32427b81337139b1003ad
teguhpjs <teguhpjs@gmail.com>
2021-10-22 18:14:38 +0700

    Penambahan dan Update API dan Model

--- Programming/.LaravelCore/app/Http/Controllers/Application/BackEnd/System/Transaction/Engines/delete/humanResource/setWorkerType/v1/setWorkerType.php
+++ Programming/.LaravelCore/app/Http/Controllers/Application/BackEnd/System/Transaction/Engines/delete/humanResource/setWorkerType/v1/setWorkerType.php
@@ -3,28 +3,27 @@
 /*
 +----------------------------------------------------------------------------------------------------------------------------------+
 | ▪ Category   : API Engine Controller                                                                                             |
-| ▪ Name Space : \App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\delete\humanResource                         |
-|                \setEmployee\v1                                                                                                   |
+| ▪ Name Space : \App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\delete\humanResource\setWorkerType\v1        |
 |                                                                                                                                  |
-| ▪ Copyleft 🄯 2020 Zheta (teguhpjs@gmail.com)                                                                                     |
+| ▪ Copyleft 🄯 2021 Zheta (teguhpjs@gmail.com)                                                                                     |
 +----------------------------------------------------------------------------------------------------------------------------------+
 */
-namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\delete\humanResource\setEmployee\v1
+namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\delete\humanResource\setWorkerType\v1
     {
     /*
     +------------------------------------------------------------------------------------------------------------------------------+
-    | ▪ Class Name  : setEmployee                                                                                                  |
-    | ▪ Description : Menangani API transaction.delete.humanResource.setEmployee Version 1                                         |
+    | ▪ Class Name  : setWorkerType                                                                                                    |
+    | ▪ Description : Menangani API transaction.delete.humanResource.setWorkerType Version 1                                           |
     +------------------------------------------------------------------------------------------------------------------------------+
     */
-    class setEmployee extends \App\Http\Controllers\Controller
+    class setWorkerType extends \App\Http\Controllers\Controller
         {
         /*
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Method Name     : __construct                                                                                          |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Version         : 1.0000.0000000                                                                                       |
-        | ▪ Last Update     : 2020-11-23                                                                                           |
+        | ▪ Last Update     : 2021-10-22                                                                                           |
         | ▪ Description     : System's Default Constructor                                                                         |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Input Variable  :                                                                                                      |
@@ -43,7 +42,7 @@ namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\de
         | ▪ Method Name     : main                                                                                                 |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Version         : 1.0000.0000000                                                                                       |
-        | ▪ Last Update     : 2020-11-23                                                                                           |
+        | ▪ Last Update     : 2021-10-22                                                                                           |
         | ▪ Description     : Fungsi Utama Engine                                                                                  |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Input Variable  :                                                                                                      |
@@ -57,11 +56,11 @@ namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\de
             {
             $varReturn = \App\Helpers\ZhtHelper\Logger\Helper_SystemLog::setLogOutputMethodHeader($varUserSession, null, __CLASS__, __FUNCTION__);
             try {
-                $varSysDataProcess = \App\Helpers\ZhtHelper\Logger\Helper_SystemLog::setLogOutputMethodProcessHeader($varUserSession, __CLASS__, __FUNCTION__, 'Delete Employee (version 1)');
+                $varSysDataProcess = \App\Helpers\ZhtHelper\Logger\Helper_SystemLog::setLogOutputMethodProcessHeader($varUserSession, __CLASS__, __FUNCTION__, 'Delete Worker Type Data (version 1)');
                 try {
                     //---- ( MAIN CODE ) ------------------------------------------------------------------------- [ START POINT ] -----
                     try{
-                        if(!($varDataSend = \App\Helpers\ZhtHelper\System\BackEnd\Helper_API::getEngineDataSend_DataDelete($varUserSession, (new \App\Models\Database\SchData_OLTP_HumanResource\TblEmployee())->setDataDelete(
+                        if(!($varDataSend = \App\Helpers\ZhtHelper\System\BackEnd\Helper_API::getEngineDataSend_DataDelete($varUserSession, (new \App\Models\Database\SchData_OLTP_HumanResource\TblWorkerType())->setDataDelete(
                             $varUserSession,
                             $varData['recordID']
                             ))))
