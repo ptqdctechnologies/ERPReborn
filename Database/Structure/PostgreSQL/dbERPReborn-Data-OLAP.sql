fe2846247de5d61bb0d29846b5789c7df62315d1
Aldi Mulyadi <aldimulyadi@localhost.localdomain>
2021-11-05 17:49:35 +0700

    Update Database

--- Database/Structure/PostgreSQL/dbERPReborn-Data-OLAP.sql
+++ Database/Structure/PostgreSQL/dbERPReborn-Data-OLAP.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 14.0 (Debian 14.0-1.pgdg110+1)
 -- Dumped by pg_dump version 14.0 (Debian 14.0-1.pgdg110+1)
 
--- Started on 2021-10-21 11:42:18 WIB
+-- Started on 2021-11-05 17:49:31 WIB
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -19,7 +19,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 6 (class 2615 OID 680411)
+-- TOC entry 5 (class 2615 OID 382260)
 -- Name: SchSystem; Type: SCHEMA; Schema: -; Owner: SysEngine
 --
 
@@ -29,7 +29,7 @@ CREATE SCHEMA "SchSystem";
 ALTER SCHEMA "SchSystem" OWNER TO "SysEngine";
 
 --
--- TOC entry 2 (class 3079 OID 680412)
+-- TOC entry 2 (class 3079 OID 382261)
 -- Name: dblink; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -46,7 +46,7 @@ COMMENT ON EXTENSION dblink IS 'connect to other PostgreSQL databases from withi
 
 
 --
--- TOC entry 256 (class 1255 OID 680458)
+-- TOC entry 256 (class 1255 OID 382307)
 -- Name: FuncSys_General_CreateSequence(character varying, character varying); Type: FUNCTION; Schema: SchSystem; Owner: SysEngine
 --
 
@@ -87,7 +87,7 @@ $_$;
 ALTER FUNCTION "SchSystem"."FuncSys_General_CreateSequence"(character varying, character varying) OWNER TO "SysEngine";
 
 --
--- TOC entry 265 (class 1255 OID 680459)
+-- TOC entry 265 (class 1255 OID 382308)
 -- Name: FuncSys_General_GetSequence(character varying, character varying); Type: FUNCTION; Schema: SchSystem; Owner: SysEngine
 --
 
@@ -125,7 +125,7 @@ $_$;
 ALTER FUNCTION "SchSystem"."FuncSys_General_GetSequence"(character varying, character varying) OWNER TO "SysEngine";
 
 --
--- TOC entry 266 (class 1255 OID 680460)
+-- TOC entry 266 (class 1255 OID 382309)
 -- Name: FuncSys_General_SetSequence(character varying, character varying, bigint); Type: FUNCTION; Schema: SchSystem; Owner: SysEngine
 --
 
@@ -191,7 +191,7 @@ $_$;
 
 ALTER FUNCTION "SchSystem"."FuncSys_General_SetSequence"(character varying, character varying, bigint) OWNER TO "SysEngine";
 
--- Completed on 2021-10-21 11:42:18 WIB
+-- Completed on 2021-11-05 17:49:31 WIB
 
 --
 -- PostgreSQL database dump complete
