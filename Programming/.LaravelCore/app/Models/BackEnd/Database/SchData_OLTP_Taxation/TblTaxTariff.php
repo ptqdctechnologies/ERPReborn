ed1de35082f0c4ecc4bb873f11e2f93ced69e47f
ptqdctechnologies <pt.qdc.technologies@gmail.com>
2022-03-15 12:10:17 +0700

    Update Pertanggal 15 Maret 2022 12:10

---
+++ Programming/.LaravelCore/app/Models/BackEnd/Database/SchData_OLTP_Taxation/TblTaxTariff.php
@@ -0,0 +1,150 @@
+<?php
+
+/*
++----------------------------------------------------------------------------------------------------------------------------------+
+| ▪ Category   : Laravel Models                                                                                                    |
+| ▪ Name Space : \App\Models\Database\SchData_OLTP_Taxation                                                                        |
+|                                                                                                                                  |
+| ▪ Copyleft 🄯 2022 Zheta (teguhpjs@gmail.com)                                                                                     |
++----------------------------------------------------------------------------------------------------------------------------------+
+*/
+namespace App\Models\Database\SchData_OLTP_Taxation
+    {
+    /*
+    +------------------------------------------------------------------------------------------------------------------------------+
+    | ▪ Class Name  : TblTaxType                                                                                                   |
+    | ▪ Description : Menangani Models Database ► SchData-OLTP-Taxation ► TblTaxTariff                                             |
+    +------------------------------------------------------------------------------------------------------------------------------+
+    */
+    class TblTaxTariff extends \App\Models\Database\DefaultClassPrototype
+        {
+        /*
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Method Name     : __construct                                                                                          |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Version         : 1.0000.0000000                                                                                       |
+        | ▪ Creation Date   : 2022-03-15                                                                                           |
+        | ▪ Last Update     : 2022-03-15                                                                                           |
+        | ▪ Description     : System's Default Constructor                                                                         |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Input Variable  :                                                                                                      |
+        |      ▪ (void)                                                                                                            |
+        | ▪ Output Variable :                                                                                                      |
+        |      ▪ (void)                                                                                                            |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        */
+        function __construct()
+            {
+            parent::__construct(__CLASS__);
+            }
+
+
+        /*
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Method Name     : setDataInsert                                                                                        |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Version         : 1.0000.0000000                                                                                       |
+        | ▪ Creation Date   : 2022-03-15                                                                                           |
+        | ▪ Last Update     : 2022-03-15                                                                                           |
+        | ▪ Description     : Data Insert                                                                                          |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Input Variable  :                                                                                                      |
+        |      ▪ (mixed)  varUserSession ► User Session                                                                            |
+        |      ▪ (string) varSysDataAnnotation ► System Data Annotation                                                            |
+        |      ▪ (string) varSysPartitionRemovableRecordKeyRefType ► System Partition Removable Record Key Reference Type          |
+        |      ▪ (int)    varSysBranchRefID ► System Branch Reference ID                                                           |
+        |      ▪ (int)    varTaxType_RefID ► Tax Type Reference ID                                                                 |
+        |      ▪ (string) varValidStartDateTimeTZ ► Valid Start DateTimeTZ                                                         |
+        |      ▪ (string) varValidFinishDateTimeTZ ► Valid Finish DateTimeTZ                                                       |
+        |      ▪ (float)  varTariffPercentage ► Tariff Percentage                                                                  |
+        |      ▪ (int)    varRoundUnit ► Round Unit                                                                                |
+        |      ▪ (bool)   varSignRoundUp ► RoundUp Sign                                                                            |
+        | ▪ Output Variable :                                                                                                      |
+        |      ▪ (array)  varReturn                                                                                                | 
+        +--------------------------------------------------------------------------------------------------------------------------+
+        */
+        public function setDataInsert(
+            $varUserSession, 
+            string $varSysDataAnnotation = null, int $varSysPartitionRemovableRecordKeyRefType = null, int $varSysBranchRefID = null,
+            int $varTaxType_RefID = null, string $varValidStartDateTimeTZ = null, string $varValidFinishDateTimeTZ = null, float $varTariffPercentage = null, int $varRoundUnit = null, bool $varSignRoundUp = null)
+            {
+            $varReturn = \App\Helpers\ZhtHelper\Database\Helper_PostgreSQL::getQueryExecution(
+                $varUserSession, 
+                \App\Helpers\ZhtHelper\Database\Helper_PostgreSQL::getBuildStringLiteral_StoredProcedure(
+                    $varUserSession,
+                    parent::getSchemaName($varUserSession).'.Func_'.parent::getTableName($varUserSession).'_SET',
+                    [
+                        [$varUserSession, 'bigint'],
+                        [null, 'bigint'],
+                        [$varSysDataAnnotation, 'varchar'],
+                        [$varSysPartitionRemovableRecordKeyRefType, 'varchar'],
+                        [$varSysBranchRefID, 'bigint'],
+
+                        [$varTaxType_RefID, 'bigint'],
+                        [$varValidStartDateTimeTZ, 'timestamptz'],
+                        [$varValidFinishDateTimeTZ, 'timestamptz'],
+                        [$varTariffPercentage, 'numeric'],
+                        [$varRoundUnit, 'smallint'],
+                        [$varSignRoundUp, 'boolean']
+                    ]
+                    )
+                );
+            return $varReturn['Data'][0];
+            }
+
+
+        /*
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Method Name     : setDataUpdate                                                                                        |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Version         : 1.0000.0000000                                                                                       |
+        | ▪ Creation Date   : 2022-03-15                                                                                           |
+        | ▪ Last Update     : 2022-03-15                                                                                           |
+        | ▪ Description     : Data Update                                                                                          |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Input Variable  :                                                                                                      |
+        |      ▪ (mixed)  varUserSession ► User Session                                                                            |
+        |      ▪ (int)    varSysID ► System Record ID                                                                              |
+        |      ▪ (string) varSysDataAnnotation ► System Data Annotation                                                            |
+        |      ▪ (string) varSysPartitionRemovableRecordKeyRefType ► System Partition Removable Record Key Reference Type          |
+        |      ▪ (int)    varSysBranchRefID ► System Branch Reference ID                                                           |
+        |      ▪ (int)    varTaxType_RefID ► Tax Type Reference ID                                                                 |
+        |      ▪ (string) varValidStartDateTimeTZ ► Valid Start DateTimeTZ                                                         |
+        |      ▪ (string) varValidFinishDateTimeTZ ► Valid Finish DateTimeTZ                                                       |
+        |      ▪ (float)  varTariffPercentage ► Tariff Percentage                                                                  |
+        |      ▪ (int)    varRoundUnit ► Round Unit                                                                                |
+        |      ▪ (bool)   varSignRoundUp ► RoundUp Sign                                                                            |
+        | ▪ Output Variable :                                                                                                      |
+        |      ▪ (array)  varReturn                                                                                                | 
+        +--------------------------------------------------------------------------------------------------------------------------+
+        */
+        public function setDataUpdate(
+            $varUserSession, 
+            int $varSysID, string $varSysDataAnnotation = null, int $varSysPartitionRemovableRecordKeyRefType = null, int $varSysBranchRefID = null,
+            int $varTaxType_RefID = null, string $varValidStartDateTimeTZ = null, string $varValidFinishDateTimeTZ = null, float $varTariffPercentage = null, int $varRoundUnit = null, bool $varSignRoundUp = null)
+            {
+            $varReturn = \App\Helpers\ZhtHelper\Database\Helper_PostgreSQL::getQueryExecution(
+                $varUserSession, 
+                \App\Helpers\ZhtHelper\Database\Helper_PostgreSQL::getBuildStringLiteral_StoredProcedure(
+                    $varUserSession,
+                    parent::getSchemaName($varUserSession).'.Func_'.parent::getTableName($varUserSession).'_SET',
+                    [
+                        [$varUserSession, 'bigint'],
+                        [$varSysID, 'bigint'],
+                        [$varSysDataAnnotation, 'varchar'],
+                        [$varSysPartitionRemovableRecordKeyRefType, 'varchar'],
+                        [$varSysBranchRefID, 'bigint'],
+
+                        [$varTaxType_RefID, 'bigint'],
+                        [$varValidStartDateTimeTZ, 'timestamptz'],
+                        [$varValidFinishDateTimeTZ, 'timestamptz'],
+                        [$varTariffPercentage, 'numeric'],
+                        [$varRoundUnit, 'smallint'],
+                        [$varSignRoundUp, 'boolean']
+                    ],
+                    )
+                );
+            return $varReturn['Data'][0];
+            }
+        }
+    }
