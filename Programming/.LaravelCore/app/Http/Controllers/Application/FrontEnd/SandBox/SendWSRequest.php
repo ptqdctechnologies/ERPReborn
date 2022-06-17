029d81394b762279df4ed530a7b8d715edaa8674
ptqdctechnologies <pt.qdc.technologies@gmail.com>
2022-06-17 17:24:15 +0700

    Update Pertanggal 17 Juni 2022 17:24

--- Programming/.LaravelCore/app/Http/Controllers/Application/FrontEnd/SandBox/SendWSRequest.php
+++ Programming/.LaravelCore/app/Http/Controllers/Application/FrontEnd/SandBox/SendWSRequest.php
@@ -15563,6 +15563,54 @@ namespace App\Http\Controllers\Application\FrontEnd\SandBox
 
         /*
         +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ API Key     : transaction.read.dataEntities.fixedAsset.getGoodsIdentity                                                |
+        | ▪ API Version : 1                                                                                                        |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        */
+        public function APIGateway_getDataEntitiesGoodsIdentity()
+            {
+            //---Parameter Set---
+            $varAPIWebToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoic3lzYWRtaW4iLCJpYXQiOjE2NTU0Mzk5NzF9.jf9yHi5Cd4ytSlfdEliNDNiyhlnkDAXijjXfvIdNL3c';
+            //---Core---
+            $varData = \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGateway(
+                \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
+                $varAPIWebToken, 
+                'transaction.read.dataEntities.fixedAsset.getGoodsIdentity', 
+                'latest', 
+                [
+                'parameter' => [
+                    'recordIDSet' => '{17000000000001, 17000000000002}'
+                    ]
+                ]
+                );
+            var_dump($varData);
+            }
+        public function APIGatewayJQuery_getDataEntitiesGoodsIdentity()
+            {
+            //---Parameter Set---
+            $varAPIWebToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoic3lzYWRtaW4iLCJpYXQiOjE2NTU0Mzk5NzF9.jf9yHi5Cd4ytSlfdEliNDNiyhlnkDAXijjXfvIdNL3c';
+            //---Core---
+            echo '<input type="text" id="dataInput_RecordIDSet" value="{17000000000001, 17000000000002}">';
+            echo \App\Helpers\ZhtHelper\General\Helper_JavaScript::setLibrary(\App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System());
+            $varJQueryFunction = \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGatewayJQuery(
+                \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(), 
+                $varAPIWebToken, 
+                'transaction.read.dataEntities.fixedAsset.getGoodsIdentity', 
+                'latest', 
+                '{'.
+                    '"parameter" : '.
+                        '{'.
+                        '"recordIDSet" : document.getElementById("dataInput_RecordIDSet").value'.
+                        '}'.
+                '}'
+                ); 
+            echo "<button type='button' onclick='javascript:var varData = ".$varJQueryFunction."; $(\"body\").append(JSON.stringify(varData));'>Submit Data</button>";
+            dd($varJQueryFunction);
+            }
+
+
+        /*
+        +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ API Key     : transaction.read.dataEntities.humanResource.getWorkerCareerInternal                                      |
         | ▪ API Version : 1                                                                                                        |
         +--------------------------------------------------------------------------------------------------------------------------+
