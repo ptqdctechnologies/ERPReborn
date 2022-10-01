bcabfceb3e9dcb92444026d99c2b2937cb191a30
ptqdctechnologies <pt.qdc.technologies@gmail.com>
2022-10-01 13:13:38 +0700

    Update Vendor Laravel

--- Programming/WebBackEnd/vendor/laravel/framework/src/Illuminate/Foundation/Concerns/ResolvesDumpSource.php
+++ Programming/WebBackEnd/vendor/laravel/framework/src/Illuminate/Foundation/Concerns/ResolvesDumpSource.php
@@ -7,14 +7,14 @@ trait ResolvesDumpSource
     /**
      * The source resolver.
      *
-     * @var (callable(): (array{0: string, 1: string, 2: int}|null))|null
+     * @var (callable(): (array{0: string, 1: string, 2: int|null}|null))|null
      */
     protected static $dumpSourceResolver;
 
     /**
      * Resolve the source of the dump call.
      *
-     * @return array{0: string, 1: string, 2: int}|null
+     * @return array{0: string, 1: string, 2: int|null}|null
      */
     public function resolveDumpSource()
     {
@@ -22,16 +22,38 @@ trait ResolvesDumpSource
             return call_user_func(static::$dumpSourceResolver);
         }
 
-        $trace = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT | DEBUG_BACKTRACE_IGNORE_ARGS, 20);
+        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 20);
 
-        $file = $trace[7]['file'] ?? null;
-        $line = $trace[7]['line'] ?? null;
+        $sourceKey = null;
+
+        foreach ($trace as $traceKey => $traceFile) {
+            if (isset($traceFile['file']) && str_ends_with(
+                $traceFile['file'],
+                'symfony/var-dumper/Resources/functions/dump.php'
+            )) {
+                $sourceKey = $traceKey + 1;
+
+                break;
+            }
+        }
+
+        if (is_null($sourceKey)) {
+            return;
+        }
+
+        $file = $trace[$sourceKey]['file'] ?? null;
+        $line = $trace[$sourceKey]['line'] ?? null;
 
         if (is_null($file) || is_null($line)) {
             return;
         }
 
         $relativeFile = $file;
+
+        if ($this->isCompiledViewFile($file)) {
+            $file = $this->getOriginalFileForCompiledView($file);
+            $line = null;
+        }
 
         if (str_starts_with($file, $this->basePath)) {
             $relativeFile = substr($file, strlen($this->basePath) + 1);
@@ -41,9 +63,37 @@ trait ResolvesDumpSource
     }
 
     /**
+     * Determine if the given file is a view compiled.
+     *
+     * @param  string  $file
+     * @return bool
+     */
+    protected function isCompiledViewFile($file)
+    {
+        return str_starts_with($file, $this->compiledViewPath);
+    }
+
+    /**
+     * Get the original view compiled file by the given compiled file.
+     *
+     * @param  string  $file
+     * @return string
+     */
+    protected function getOriginalFileForCompiledView($file)
+    {
+        preg_match('/\/\*\*PATH\s(.*)\sENDPATH/', file_get_contents($file), $matches);
+
+        if (isset($matches[1])) {
+            $file = $matches[1];
+        }
+
+        return $file;
+    }
+
+    /**
      * Set the resolver that resolves the source of the dump call.
      *
-     * @param  (callable(): (array{0: string, 1: string, 2: int}|null))|null  $callable
+     * @param  (callable(): (array{0: string, 1: string, 2: int|null}|null))|null  $callable
      * @return void
      */
     public static function resolveDumpSourceUsing($callable)
--- Programming/WebFrontEnd/vendor/laravel/framework/tests/Foundation/Http/HtmlDumperTest.php
+++ Programming/WebFrontEnd/vendor/laravel/framework/tests/Foundation/Http/HtmlDumperTest.php
@@ -4,6 +4,7 @@ namespace Illuminate\Tests\Foundation\Http;
 
 use Illuminate\Foundation\Http\HtmlDumper;
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
 use stdClass;
 use Symfony\Component\VarDumper\Caster\ReflectionCaster;
 use Symfony\Component\VarDumper\Cloner\VarCloner;
@@ -25,7 +26,7 @@ class HtmlDumperTest extends TestCase
     {
         $output = $this->dump('string');
 
-        $expected = "string</span>\"<span style=\"color: #A0A0A0; font-family: Menlo\"> // app/routes/console.php:18</span>\n</pre>";
+        $expected = "string</span>\"<span style=\"color: #A0A0A0;\"> // app/routes/console.php:18</span>\n</pre>";
 
         $this->assertStringContainsString($expected, $output);
     }
@@ -34,7 +35,7 @@ class HtmlDumperTest extends TestCase
     {
         $output = $this->dump(1);
 
-        $expected = "1</span><span style=\"color: #A0A0A0; font-family: Menlo\"> // app/routes/console.php:18</span>\n</pre>";
+        $expected = "1</span><span style=\"color: #A0A0A0;\"> // app/routes/console.php:18</span>\n</pre>";
 
         $this->assertStringContainsString($expected, $output);
     }
@@ -43,7 +44,7 @@ class HtmlDumperTest extends TestCase
     {
         $output = $this->dump(1.1);
 
-        $expected = "1.1</span><span style=\"color: #A0A0A0; font-family: Menlo\"> // app/routes/console.php:18</span>\n</pre>";
+        $expected = "1.1</span><span style=\"color: #A0A0A0;\"> // app/routes/console.php:18</span>\n</pre>";
 
         $this->assertStringContainsString($expected, $output);
     }
@@ -52,7 +53,7 @@ class HtmlDumperTest extends TestCase
     {
         $output = $this->dump(['string', 1, 1.1, ['string', 1, 1.1]]);
 
-        $expected = '<samp data-depth=1 class=sf-dump-expanded><span style="color: #A0A0A0; font-family: Menlo"> // app/routes/console.php:18</span>';
+        $expected = '<samp data-depth=1 class=sf-dump-expanded><span style="color: #A0A0A0;"> // app/routes/console.php:18</span>';
 
         $this->assertStringContainsString($expected, $output);
     }
@@ -61,7 +62,7 @@ class HtmlDumperTest extends TestCase
     {
         $output = $this->dump(true);
 
-        $expected = "true</span><span style=\"color: #A0A0A0; font-family: Menlo\"> // app/routes/console.php:18</span>\n</pre>";
+        $expected = "true</span><span style=\"color: #A0A0A0;\"> // app/routes/console.php:18</span>\n</pre>";
 
         $this->assertStringContainsString($expected, $output);
     }
@@ -73,7 +74,7 @@ class HtmlDumperTest extends TestCase
 
         $output = $this->dump($user);
 
-        $expected = '<samp data-depth=1 class=sf-dump-expanded><span style="color: #A0A0A0; font-family: Menlo"> // app/routes/console.php:18</span>';
+        $expected = '<samp data-depth=1 class=sf-dump-expanded><span style="color: #A0A0A0;"> // app/routes/console.php:18</span>';
 
         $this->assertStringContainsString($expected, $output);
     }
@@ -82,7 +83,7 @@ class HtmlDumperTest extends TestCase
     {
         $output = $this->dump(null);
 
-        $expected = "null</span><span style=\"color: #A0A0A0; font-family: Menlo\"> // app/routes/console.php:18</span>\n</pre>";
+        $expected = "null</span><span style=\"color: #A0A0A0;\"> // app/routes/console.php:18</span>\n</pre>";
 
         $this->assertStringContainsString($expected, $output);
     }
@@ -98,11 +99,100 @@ class HtmlDumperTest extends TestCase
         $this->assertStringContainsString($expected, $output);
     }
 
+    public function testWhenIsFileViewIsNotViewCompiled()
+    {
+        $file = '/my-work-directory/routes/web.php';
+
+        $dumper = new HtmlDumper(
+            '/my-work-directory',
+            '/my-work-directory/storage/framework/views'
+        );
+
+        $reflection = new ReflectionClass($dumper);
+        $method = $reflection->getMethod('isCompiledViewFile');
+        $method->setAccessible(true);
+        $isCompiledViewFile = $method->invoke($dumper, $file);
+
+        $this->assertFalse($isCompiledViewFile);
+    }
+
+    public function testWhenIsFileViewIsViewCompiled()
+    {
+        $file = '/my-work-directory/storage/framework/views/6687c33c38b71a8560.php';
+
+        $dumper = new HtmlDumper(
+            '/my-work-directory',
+            '/my-work-directory/storage/framework/views'
+        );
+
+        $reflection = new ReflectionClass($dumper);
+        $method = $reflection->getMethod('isCompiledViewFile');
+        $method->setAccessible(true);
+        $isCompiledViewFile = $method->invoke($dumper, $file);
+
+        $this->assertTrue($isCompiledViewFile);
+    }
+
+    public function testGetOriginalViewCompiledFile()
+    {
+        $compiled = __DIR__.'/../fixtures/fake-compiled-view.php';
+        $original = '/my-work-directory/resources/views/welcome.blade.php';
+
+        $dumper = new HtmlDumper(
+            '/my-work-directory',
+            '/my-work-directory/storage/framework/views'
+        );
+
+        $reflection = new ReflectionClass($dumper);
+        $method = $reflection->getMethod('getOriginalFileForCompiledView');
+        $method->setAccessible(true);
+
+        $this->assertSame($original, $method->invoke($dumper, $compiled));
+    }
+
+    public function testWhenGetOriginalViewCompiledFileFails()
+    {
+        $compiled = __DIR__.'/../fixtures/fake-compiled-view-without-source-map.php';
+        $original = $compiled;
+
+        $dumper = new HtmlDumper(
+            '/my-work-directory',
+            '/my-work-directory/storage/framework/views'
+        );
+
+        $reflection = new ReflectionClass($dumper);
+        $method = $reflection->getMethod('getOriginalFileForCompiledView');
+        $method->setAccessible(true);
+
+        $this->assertSame($original, $method->invoke($dumper, $compiled));
+    }
+
+    public function testUnresolvableLine()
+    {
+        HtmlDumper::resolveDumpSourceUsing(function () {
+            return [
+                '/my-work-directory/resources/views/welcome.blade.php',
+                'resources/views/welcome.blade.php',
+                null,
+            ];
+        });
+
+        $output = $this->dump('hey from view');
+
+        $expected = "hey from view</span>\"<span style=\"color: #A0A0A0;\"> // resources/views/welcome.blade.php</span>\n</pre>";
+
+        $this->assertStringContainsString($expected, $output);
+    }
+
     protected function dump($value)
     {
         $outputFile = stream_get_meta_data(tmpfile())['uri'];
 
-        $dumper = new HtmlDumper('/my-work-directory');
+        $dumper = new HtmlDumper(
+            '/my-work-directory',
+            '/my-work-directory/storage/framework/views'
+        );
+
         $dumper->setOutput($outputFile);
 
         $cloner = tap(new VarCloner())->addCasters(ReflectionCaster::UNSET_CLOSURE_FILE_INFO);
