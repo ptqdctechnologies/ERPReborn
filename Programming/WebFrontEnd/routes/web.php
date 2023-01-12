466857d1e66516a5fb1492c2f3d9c5cfd3f29faf
aldimulyadi11 <aldimulyadi160@gmail.com>
2023-01-12 17:46:23 +0700

    update 12 januari 2023

--- Programming/WebFrontEnd/routes/web.php
+++ Programming/WebFrontEnd/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 //---[ Example Code - Dynamic Route ]----------------------------------------------------[START]---
 $varUserSession = \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System();
-$varAPIWebToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoic3lzYWRtaW4iLCJpYXQiOjE2NzM0OTY3MTZ9.OWVmOGQ3MjU4OGRjZWJiMzk2OWI5OGJkY2I0OGI3MmE4YjJlZGNhZjAwMjk2ZjQ2ZjU4OWIxZjkzMjExZjcwMg';
+$varAPIWebToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoic3lzYWRtaW4iLCJpYXQiOjE2NzM1MDU5MjB9.NjdkZjNmYWVmOWI2MTc5MmE1OTEzNWIyYzg1MDFkNGY2N2QyYTBjMmE2NGFjMjZmNTUzZDY1OTI5OWYyMTcxZQ';
 
 
 \App\Helpers\ZhtHelper\System\FrontEnd\Helper_LaravelRoute::setDynamicRoute_Examples_APICall(
