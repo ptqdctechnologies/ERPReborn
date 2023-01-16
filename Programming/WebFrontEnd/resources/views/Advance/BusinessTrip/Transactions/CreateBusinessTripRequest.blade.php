7acd2ceff2e2a65cbde335fecd42b090b173d83e
aldimulyadi11 <aldimulyadi160@gmail.com>
2023-01-16 17:47:19 +0700

    update 16 januari 2023

--- Programming/WebFrontEnd/resources/views/Advance/BusinessTrip/Transactions/CreateBusinessTripRequest.blade.php
+++ Programming/WebFrontEnd/resources/views/Advance/BusinessTrip/Transactions/CreateBusinessTripRequest.blade.php
@@ -40,6 +40,54 @@
                 </div>
               </div>
             </div>
+
+
+            <div class="tab-pane fade show active" id="product-desc" role="tabpanel" aria-labelledby="product-desc-tab">
+              <div class="row">
+                <div class="col-12">
+                  <div class="card">
+                    <div class="card-header">
+                      <label class="card-title">
+                        File Attachment
+                      </label>
+                      <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                          <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
+                        </button>
+                      </div>
+                    </div>
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-md-12">
+                          <input type="text" id="dataInput_Log_FileUpload_Pointer_RefID" value="" readonly="true" name="dataInput_Log_FileUpload_Pointer_RefID">
+                          <input type="file" id="dataInput_Log_FileUpload_Pointer_RefID_Action" name="dataInput_Log_FileUpload_Pointer_RefID_Action" multiple="multiple" onchange="javascript: @php echo \App\Helpers\ZhtHelper\General\Helper_JavaScript::getSyntaxFunc_DOMInputFileContent(\App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(), $varAPIWebToken, 'Upload', 'dataInput_Log_FileUpload_Pointer_RefID', 'dataShow_ActionPanel', 'dataShow_MasterFileRecord'); @endphp;" />
+                        </div>
+                        <br><br>
+                        <div class="col-md-12">
+                          <div class="card-body table-responsive p-0" style="height:90px;">
+                            
+                          <table class="table table-head-fixed table-sm text-nowrap">
+                              <div class="form-group input_fields_wrap">
+
+                                <div class="input-group control-group">
+
+                                  <!-- <div id="dataShow_MasterFileRecord" style="border-style:solid; border-width:1px;"></div> -->
+                                  <div id="dataShow_ActionPanel" style="border-style:solid; border-width:1px;"></div>
+                                  
+                                </div>
+                              </div>
+
+                            </table>
+                              
+                          </div>
+                        </div>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
             
             <div class="row">
               <div class="col-12">
