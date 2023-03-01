ac960df0075b0d5acc4d5d47908d0f71f981d6e9
ptqdctechnologies <pt.qdc.technologies@gmail.com>
2023-03-01 12:27:34 +0700

    Update Pertanggal 1 Maret 2023 12:27 WIB

--- Programming/WebFrontEnd/routes/web.php
+++ Programming/WebFrontEnd/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 //---[ Example Code - Dynamic Route ]----------------------------------------------------[START]---
 $varUserSession = \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System();
-$varAPIWebToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoic3lzYWRtaW4iLCJpYXQiOjE2Nzc1NTAzNTN9.MTJjM2U3ODUwODk1Yjc3MzFlNDBkZDhhZjAxOWUxMTVmZWRjOWY4NDYxZGI3NDU1ZDQ2ZjM1NTM3MzEwMjFmZQ';
+$varAPIWebToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoic3lzYWRtaW4iLCJpYXQiOjE2Nzc2NDY2NTV9.MTc0YjIwMzU2MDJjMmZkOWIzNmQ5MzYzYTg2MzBmOTE3NGQ1NTQ1YjA5YWQ1NDQwYzI5NTZiYTgzMjgxYjNlOA';
 
 \App\Helpers\ZhtHelper\System\FrontEnd\Helper_LaravelRoute::setDynamicRoute_Examples_APICall(
     \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
