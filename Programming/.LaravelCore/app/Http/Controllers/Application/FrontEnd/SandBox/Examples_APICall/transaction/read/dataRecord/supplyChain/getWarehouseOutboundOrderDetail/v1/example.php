d69e7185d71711280763f69ec26bae0bb777caf1
ptqdctechnologies <pt.qdc.technologies@gmail.com>
2023-03-21 18:14:38 +0700

    Update Pertanggal 21 Maret 2023 18:14 WIB

---
+++ Programming/.LaravelCore/app/Http/Controllers/Application/FrontEnd/SandBox/Examples_APICall/transaction/read/dataRecord/supplyChain/getWarehouseOutboundOrderDetail/v1/example.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
++----------------------------------------------------------------------------------------------------------------------------------+
+| ▪ Category    : Example - API Call Controller                                                                                    |
+| ▪ Name Space  : \App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\transaction\read\dataRecord\supplyChain      |
+|                 \getWarehouseOutboundOrderDetail\v1                                                                              |
+| ▪ API Key     : transaction.read.dataRecord.supplyChain.getWarehouseOutboundOrderDetail                                          |
+| ▪ API Version : 1                                                                                                                |
+|                                                                                                                                  |
+| ▪ Copyleft 🄯 2023 Zheta (teguhpjs@gmail.com)                                                                                     |
++----------------------------------------------------------------------------------------------------------------------------------+
+*/
+namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\transaction\read\dataRecord\supplyChain\getWarehouseOutboundOrderDetail\v1
+    {
+    class example extends \App\Http\Controllers\Controller
+        {
+        /*
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Call URL        : http(s)://<HOST>/                                                                                    |
+        |                     transaction.read.dataRecord.supplyChain.getWarehouseOutboundOrderDetail.v1_throughAPIGateway         |
+        |                     ► http://172.28.0.4/                                                                                 |
+        |                       transaction.read.dataRecord.supplyChain.getWarehouseOutboundOrderDetail.v1_throughAPIGateway       |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Version         : 1.0000.0000000                                                                                       |
+        | ▪ Last Update     : 2023-03-21                                                                                           |
+        | ▪ Creation Date   : 2023-03-21                                                                                           |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        */
+        public function throughAPIGateway($varAPIWebToken)
+            {
+            //---Parameter Set---
+            if (!$varAPIWebToken) {
+                $varAPIWebToken = \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System();
+                }
+            //---Core---
+            $varData = \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGateway(
+                \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
+                $varAPIWebToken, 
+                'transaction.read.dataRecord.supplyChain.getWarehouseOutboundOrderDetail', 
+                'latest', 
+                [
+                'recordID' => 179000000000001
+                ]
+                );
+            var_dump($varData);
+            }
+
+
+        /*
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Call URL        : http(s)://<HOST>/                                                                                    |
+        |                     transaction.read.dataRecord.supplyChain.getWarehouseOutboundOrderDetail.v1_throughAPIGatewayJQuery   |
+        |                     ► http://172.28.0.4/                                                                                 |
+        |                       transaction.read.dataRecord.supplyChain.getWarehouseOutboundOrderDetail.v1_throughAPIGatewayJQuery |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Version         : 1.0000.0000000                                                                                       |
+        | ▪ Last Update     : 2023-03-21                                                                                           |
+        | ▪ Creation Date   : 2023-03-21                                                                                           |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        */
+        public function throughAPIGatewayJQuery($varAPIWebToken)
+            {
+            //---Parameter Set---
+            if (!$varAPIWebToken) {
+                $varAPIWebToken = \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System();
+                }
+            //---Core---
+            echo '<input type="text" id="dataInput_RecordID" value=179000000000001>';
+            echo \App\Helpers\ZhtHelper\General\Helper_JavaScript::setLibrary(\App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System());
+            $varJQueryFunction = \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGatewayJQuery(
+                \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(), 
+                $varAPIWebToken, 
+                'transaction.read.dataRecord.supplyChain.getWarehouseOutboundOrderDetail', 
+                'latest', 
+                '{'.
+                    '"recordID" : parseInt(document.getElementById("dataInput_RecordID").value)'.
+                '}'
+                ); 
+            echo "<button type='button' onclick='javascript:var varData = ".$varJQueryFunction."; $(\"body\").append(JSON.stringify(varData));'>Submit Data</button>";
+            dd($varJQueryFunction);
+            }
+        }
+    }
