420cf7d479cd10231f18e07ac29d10a0549df4a6
ptqdctechnologies <pt.qdc.technologies@gmail.com>
2023-11-28 14:39:40 +0700

    Update Pertanggal 28 November 2023 14:39 WIB

--- Programming/.LaravelCore/app/Http/Controllers/Application/BackEnd/System/Transaction/Engines/update/finance/setPurchaseInvoiceDetail/v1/setPurchaseInvoiceDetail.php
+++ Programming/.LaravelCore/app/Http/Controllers/Application/BackEnd/System/Transaction/Engines/update/finance/setPurchaseInvoiceDetail/v1/setPurchaseInvoiceDetail.php
@@ -3,28 +3,28 @@
 /*
 +----------------------------------------------------------------------------------------------------------------------------------+
 | ▪ Category   : API Engine Controller                                                                                             |
-| ▪ Name Space : \App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\create\finance\setInvoiceSupplierDetail\v1   |
+| ▪ Name Space : \App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\update\finance\setPurchaseInvoiceDetail\v1   |
 |                                                                                                                                  |
 | ▪ Copyleft 🄯 2022 Zheta (teguhpjs@gmail.com)                                                                                     |
 +----------------------------------------------------------------------------------------------------------------------------------+
 */
-namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\create\finance\setInvoiceSupplierDetail\v1
+namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\update\finance\setPurchaseInvoiceDetail\v1
     {
     /*
     +------------------------------------------------------------------------------------------------------------------------------+
-    | ▪ Class Name  : setInvoiceSupplierDetail                                                                                     |
-    | ▪ Description : Menangani API transaction.create.finance.setInvoiceSupplierDetail Version 1                                  |
+    | ▪ Class Name  : setPurchaseInvoiceDetail                                                                                     |
+    | ▪ Description : Menangani API transaction.update.finance.setPurchaseInvoiceDetail Version 1                                  |
     +------------------------------------------------------------------------------------------------------------------------------+
     */
-    class setInvoiceSupplierDetail extends \App\Http\Controllers\Controller
+    class setPurchaseInvoiceDetail extends \App\Http\Controllers\Controller
         {
         /*
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Method Name     : __construct                                                                                          |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Version         : 1.0000.0000000                                                                                       |
-        | ▪ Last Update     : 2022-09-16                                                                                           |
-        | ▪ Creation Date   : 2022-09-16                                                                                           |
+        | ▪ Last Update     : 2022-09-19                                                                                           |
+        | ▪ Creation Date   : 2022-09-19                                                                                           |
         | ▪ Description     : System's Default Constructor                                                                         |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Input Variable  :                                                                                                      |
@@ -43,8 +43,8 @@ namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\cr
         | ▪ Method Name     : main                                                                                                 |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Version         : 1.0000.0000000                                                                                       |
-        | ▪ Last Update     : 2022-09-16                                                                                           |
-        | ▪ Creation Date   : 2022-09-16                                                                                           |
+        | ▪ Last Update     : 2022-09-19                                                                                           |
+        | ▪ Creation Date   : 2022-09-19                                                                                           |
         | ▪ Description     : Fungsi Utama Engine                                                                                  |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Input Variable  :                                                                                                      |
@@ -58,18 +58,19 @@ namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\cr
             {
             $varReturn = \App\Helpers\ZhtHelper\Logger\Helper_SystemLog::setLogOutputMethodHeader($varUserSession, null, __CLASS__, __FUNCTION__);
             try {
-                $varSysDataProcess = \App\Helpers\ZhtHelper\Logger\Helper_SystemLog::setLogOutputMethodProcessHeader($varUserSession, __CLASS__, __FUNCTION__, 'Create Supplier Invoice Detail Data (version 1)');
+                $varSysDataProcess = \App\Helpers\ZhtHelper\Logger\Helper_SystemLog::setLogOutputMethodProcessHeader($varUserSession, __CLASS__, __FUNCTION__, 'Update Purchase Invoice Detail Data (version 1)');
                 try {
                     //---- ( MAIN CODE ) ------------------------------------------------------------------------- [ START POINT ] -----
                     try {
-                        if(!($varDataSend = \App\Helpers\ZhtHelper\System\BackEnd\Helper_API::getEngineDataSend_DataCreate($varUserSession, (new \App\Models\Database\SchData_OLTP_Finance\TblInvoiceSupplierDetail())->setDataInsert(
-                            $varUserSession, 
-                            null, 
+                        if (!($varDataSend = \App\Helpers\ZhtHelper\System\BackEnd\Helper_API::getEngineDataSend_DataUpdate($varUserSession, (new \App\Models\Database\SchData_OLTP_Finance\TblPurchaseInvoiceDetail())->setDataUpdate(
+                            $varUserSession,
+                            $varData['recordID'],
+                            null,
                             null,
                             (\App\Helpers\ZhtHelper\System\BackEnd\Helper_API::getUserLoginSessionEntityByAPIWebToken($varUserSession))['branchID'],
                             \App\Helpers\ZhtHelper\General\Helper_SystemParameter::getApplicationParameter_BaseCurrencyID($varUserSession, (\App\Helpers\ZhtHelper\System\BackEnd\Helper_API::getUserLoginSessionEntityByAPIWebToken($varUserSession))['branchID'], 'Env.System.BaseCurrency.ID'),
 
-                            $varData['entities']['invoiceSupplier_RefID'],
+                            $varData['entities']['purchaseInvoice_RefID'],
                             $varData['entities']['purchaseOrderDetail_RefID'],
                             $varData['entities']['product_RefID'],
                             $varData['entities']['quantity'],
@@ -85,8 +86,7 @@ namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\cr
                         $varReturn = \App\Helpers\ZhtHelper\System\BackEnd\Helper_API::setEngineResponseDataReturn_Success($varUserSession, $varDataSend);
                         } 
                     catch (\Exception $ex) {
-                        $varErrorMessage = $ex->getMessage();
-                        $varReturn = \App\Helpers\ZhtHelper\System\BackEnd\Helper_API::setEngineResponseDataReturn_Fail($varUserSession, 500, 'Invalid SQL Syntax'.($varErrorMessage ? ' ('.$varErrorMessage.')' : ''));
+                        $varReturn = \App\Helpers\ZhtHelper\System\BackEnd\Helper_API::getEngineDataSend_DataUpdateException($varUserSession, $ex);
                         }
                     //---- ( MAIN CODE ) --------------------------------------------------------------------------- [ END POINT ] -----
                     \App\Helpers\ZhtHelper\Logger\Helper_SystemLog::setLogOutputMethodProcessStatus($varUserSession, $varSysDataProcess, 'Success');
--- Programming/.LaravelCore/app/Http/Controllers/Application/FrontEnd/SandBox/Examples_APICall/transaction/create/finance/setPurchaseInvoiceDetail/v1/example.php
+++ Programming/.LaravelCore/app/Http/Controllers/Application/FrontEnd/SandBox/Examples_APICall/transaction/create/finance/setPurchaseInvoiceDetail/v1/example.php
@@ -3,22 +3,22 @@
 /*
 +----------------------------------------------------------------------------------------------------------------------------------+
 | ▪ Category    : Example - API Call Controller                                                                                    |
-| ▪ Name Space  : \App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\transaction\update\finance                   |
-|                 \setInvoiceSupplierDetail\v1                                                                                     |
-| ▪ API Key     : transaction.update.finance.setInvoiceSupplierDetail                                                              |
+| ▪ Name Space  : \App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\transaction\create\finance                   |
+|                 \setPurchaseInvoiceDetail\v1                                                                                     |
+| ▪ API Key     : transaction.create.finance.setPurchaseInvoiceDetail                                                              |
 | ▪ API Version : 1                                                                                                                |
 |                                                                                                                                  |
 | ▪ Copyleft 🄯 2022 Zheta (teguhpjs@gmail.com)                                                                                     |
 +----------------------------------------------------------------------------------------------------------------------------------+
 */
-namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\transaction\update\finance\setInvoiceSupplierDetail\v1
+namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\transaction\create\finance\setPurchaseInvoiceDetail\v1
     {
     class example extends \App\Http\Controllers\Controller
         {
         /*
         +--------------------------------------------------------------------------------------------------------------------------+
-        | ▪ Call URL        : http(s)://<HOST>/transaction.update.finance.setInvoiceSupplierDetail.v1_throughAPIGateway            |
-        |                     ► http://172.28.0.4/transaction.update.finance.setInvoiceSupplierDetail.v1_throughAPIGateway         |
+        | ▪ Call URL        : http(s)://<HOST>/transaction.create.finance.setPurchaseInvoiceDetail.v1_throughAPIGateway            |
+        |                     ► http://172.28.0.4/transaction.create.finance.setPurchaseInvoiceDetail.v1_throughAPIGateway         |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Version         : 1.0000.0000000                                                                                       |
         | ▪ Last Update     : 2022-09-19                                                                                           |
@@ -35,12 +35,11 @@ namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\tra
             $varData = \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGateway(
                 \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
                 $varAPIWebToken, 
-                'transaction.update.finance.setInvoiceSupplierDetail', 
+                'transaction.create.finance.setPurchaseInvoiceDetail', 
                 'latest', 
                 [
-                'recordID' => 209000000000001,
                 'entities' => [
-                    "invoiceSupplier_RefID" => 208000000000001,
+                    "purchaseInvoice_RefID" => 207000000000001,
                     "purchaseOrderDetail_RefID" => 86000000000001,
                     "product_RefID" => 88000000000689,
                     "quantity" => 10.00,
@@ -58,8 +57,8 @@ namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\tra
 
         /*
         +--------------------------------------------------------------------------------------------------------------------------+
-        | ▪ Call URL        : http(s)://<HOST>/transaction.update.finance.setInvoiceSupplierDetail.v1_throughAPIGatewayJQuery      |
-        |                     ► http://172.28.0.4/transaction.update.finance.setInvoiceSupplierDetail.v1_throughAPIGatewayJQuery   |
+        | ▪ Call URL        : http(s)://<HOST>/transaction.create.finance.setPurchaseInvoiceDetail.v1_throughAPIGatewayJQuery      |
+        |                     ► http://172.28.0.4/transaction.create.finance.setPurchaseInvoiceDetail.v1_throughAPIGatewayJQuery   |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Version         : 1.0000.0000000                                                                                       |
         | ▪ Last Update     : 2022-09-19                                                                                           |
@@ -75,9 +74,8 @@ namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\tra
             //---Core---
             echo \App\Helpers\ZhtHelper\General\Helper_JavaScript::setLibrary(\App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System());
             echo '<table border="1" style="border-collapse: collapse;">';
-            echo    '<tr><td colspan="2" bgcolor="#6666cc" align="middle"><p style="color:#ffffff">Invoice Supplier Main Data</p></td></tr>';
-            echo    '<tr><td>RecordID</td><td><input type="text" id="dataInput_RecordID" value=209000000000001></td></tr>';
-            echo    '<tr><td>InvoiceSupplier_RefID</td><td><input type="text" id="dataInput_InvoiceSupplier_RefID" value=208000000000001></td></tr>';
+            echo    '<tr><td colspan="2" bgcolor="#6666cc" align="middle"><p style="color:#ffffff">Purchase Invoice Detail Main Data</p></td></tr>';
+            echo    '<tr><td>PurchaseInvoice_RefID</td><td><input type="text" id="dataInput_PurchaseInvoice_RefID" value=207000000000001></td></tr>';
             echo    '<tr><td>PurchaseOrderDetail_RefID</td><td><input type="text" id="dataInput_PurchaseOrderDetail_RefID" value=86000000000001></td></tr>';
             echo    '<tr><td>Product_RefID</td><td><input type="text" id="dataInput_Product_RefID" value=88000000000689></td></tr>';
             echo    '<tr><td>Quantity</td><td><input type="text" id="dataInput_Quantity" value=10></td></tr>';
@@ -90,12 +88,11 @@ namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\tra
             $varJQueryFunction = \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGatewayJQuery(
                 \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(), 
                 $varAPIWebToken, 
-                'transaction.update.finance.setInvoiceSupplierDetail', 
+                'transaction.create.finance.setPurchaseInvoiceDetail', 
                 'latest', 
                 '{'.
-                    '"recordID" : parseInt(document.getElementById("dataInput_RecordID").value), '.
                     '"entities" : {'.
-                        '"invoiceSupplier_RefID" : parseInt(document.getElementById("dataInput_InvoiceSupplier_RefID").value), '.
+                        '"purchaseInvoice_RefID" : parseInt(document.getElementById("dataInput_PurchaseInvoice_RefID").value), '.
                         '"purchaseOrderDetail_RefID" : parseInt(document.getElementById("dataInput_PurchaseOrderDetail_RefID").value), '.
                         '"product_RefID" : parseInt(document.getElementById("dataInput_Product_RefID").value), '.
                         '"quantity" : parseFloat(document.getElementById("dataInput_Quantity").value), '.
