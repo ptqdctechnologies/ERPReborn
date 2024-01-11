ab848adf39d8efb800ecace11faee7ef8cb26622
aldimulyadi11 <aldimulyadi160@gmail.com>
2024-01-11 17:36:36 +0700

    update 11 januari 2024 #3

--- Programming/WebFrontEnd/app/Http/Controllers/Master/PrivilageMenu/PrivilageMenuController.php
+++ Programming/WebFrontEnd/app/Http/Controllers/Master/PrivilageMenu/PrivilageMenuController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Master\PrivilageMenu;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Session;
 
 class PrivilageMenuController extends Controller
 {
@@ -15,7 +17,41 @@ class PrivilageMenuController extends Controller
      */
     public function index()
     {
-        return view('Master.PrivilageMenu.Transactions.index');
+        if (Redis::get("Departement") == null) {
+
+            $varAPIWebToken = Session::get('SessionLogin');
+            $varData = \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGateway(
+                \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
+                $varAPIWebToken,
+                'transaction.read.dataList.humanResource.getOrganizationalDepartment',
+                'latest',
+                [
+                    'parameter' => [],
+                    'SQLStatement' => [
+                        'pick' => null,
+                        'sort' => null,
+                        'filter' => null,
+                        'paging' => null
+                    ]
+                ],
+                false
+            );
+        }
+
+        $Departement = json_decode(
+            \App\Helpers\ZhtHelper\Cache\Helper_Redis::getValue(
+                \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
+                "Departement"
+            ),
+            true
+        );
+
+        return view('Master.PrivilageMenu.Transactions.index', compact('Departement'));
+    }
+
+
+    public function store(Request $request){
+        dd($request->all());
     }
 
 }
--- Programming/WebFrontEnd/resources/views/Master/PrivilageMenu/Functions/Footer/FooterPrivilageMenu.blade.php
+++ Programming/WebFrontEnd/resources/views/Master/PrivilageMenu/Functions/Footer/FooterPrivilageMenu.blade.php
@@ -1,49 +1,224 @@
-<!-- SHOW DATA FOR FIRST LOADING  -->
-<!-- <script type="text/javascript">
+<!-- SELECT FOR FILTER BY DOCUMENT TYPE  -->
+<script>
+    var checkedValue = [];
+</script>
+
+<script>
     $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
 
-    var keys = 0;
-
     $.ajax({
         type: 'GET',
-        url: '{!! route("getPrivilageMenu") !!}',
+        url: '{!! route("getDocumentType") !!}',
         success: function(data) {
-            $('#TablePrivilageMenu').find('tbody').empty();
-            var keys = 0;
-            var no = 1;
-            var t = $('.TablePrivilageMenu').DataTable();
-            t.clear().draw();
-            $.each(data, function(key, val) {
-                const date = dateFns.format(
-                    dateFns.parse(val.businessDocumentDateTimeTZ, "yyyy-MM-dd hh:mm:ss"),
-                    'DD-MM-YYYY HH:mm');
+            $(".Modul").empty();
 
-                var remark = val.workFlowPathSubmitterRemarks;
-                if (val.workFlowPathSubmitterRemarks == null) {
-                    remark = "-";
-                }
+            var option = "<option value='" + '' + "'>" + 'Select Modul' + "</option>";
+            $(".Modul").append(option);
 
-                keys += 1;
-                t.row.add([
-                    '<tbody><tr><input class="businessDocument_RefID' + keys + '" value="' + val.formDocumentNumber_RefID + '" type="hidden"><td><span style="position:relative;left:10px;">' + no++ + '</span></td>',
-                    '<td><span style="position:relative;left:10px;">' + val.businessDocumentNumber + '</span></td>',
-                    '<td><span style="position:relative;left:10px;">' + val.combinedBudgetCode + '</span></td>',
-                    '<td><span style="position:relative;left:10px;">' + val.previousWorkFlowPathApproverName + '</span></td>',
-                    '<td><span style="position:relative;left:10px;">' + date + '</span></td>',
-                    '<td><span style="position:relative;left:10px;">' + val.previousWorkFlowPathActionName + '</span></td>',
-                    '<td><span style="position:relative;left:10px;white-space: pre-line">' + remark + '</span></td></tr></tbody>',
-                ]).draw();
-            });
-
-            HideLoading();
-        },
-        error: function(response) {
-            // CALL FUNCTION ERROR NOTIFICATION
-            ErrorNotif("Data Not Found !");
-        },
+            var len = data.length;
+            for (var i = 0; i < len; i++) {
+                var ids = data[i].Sys_ID;
+                var names = data[i].Name;
+                var option2 = "<option value='" + ids + "'>" + names + "</option>";
+                $(".Modul").append(option2);
+            }
+        }
     });
-</script> -->
+</script>
+
+<script>
+    $('#Modul').on("select2:select", function(e) {
+
+        $('#TableSubMenu').find('tbody').empty();
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $.ajax({
+            type: 'GET',
+            url: '{!! route("CheckDocument.ShowDocumentListData") !!}?DocumentType=' + $('#Modul').val(),
+            success: function(data) {
+                $.each(data, function(key, val) {
+                    
+
+                    var checked = "";
+                    var result = checkedValue.filter(function(elem) {
+                        if(elem == val.Sys_ID){
+                            console.log(val.Sys_ID);
+                            checked = "checked";
+                        }
+                        else{
+                            checked = "";
+                        }
+                    });
+
+                    // console.log(checked);
+
+                    var html = '<tr>' +
+                        '<td>' +
+                            '<div class="input-group">&nbsp;&nbsp;' +
+                                '<span class="input-group-text">' +
+                                    '<input type="checkbox" ' + checked + ' name="Sub_Menu" id="Sub_Menu" class="Sub_Menu" value="' + val.Sys_ID +'">' +
+                                '</span>' +
+                                '<span style="position: relative;top:7px;left:15px;">' + val.DocumentNumber +'</span>' +
+                            '</div>' +
+                        '</td>' +
+                        '</tr>';
+                    $('table.TableSubMenu tbody').append(html);
+
+                });
+
+                $('.Sub_Menu').click(function() {
+                    var id = $(this).val();
+                    if ($(this).is(":checked")) {
+                        $('#UnSelectAll').prop("checked", false);
+                        checkedValue.push(id);
+                    } else {
+                        var result = checkedValue.filter(function(elem) {
+                            return elem != id;
+                        });
+                        checkedValue = result;
+                    }
+
+                    console.log(checkedValue);
+
+                });
+            }
+        });
+    });
+</script>
+
+<script>
+    $('#SelectAll').click(function() {
+
+        if ($(this).is(":checked")) {
+            $('.Sub_Menu').prop("checked", true);
+            $('#UnSelectAll').prop("checked", false);
+            var inputElements = document.getElementsByClassName('Sub_Menu');
+
+            if (checkedValue.length > 0) {
+                checkedValue = [];
+            }
+
+            $.each(inputElements, function(key, value) {
+                checkedValue.push(value.value);
+            });
+        }
+
+        $('#SelectAll').prop("checked", true);
+
+        // console.log(checkedValue);
+
+    });
+
+    $('#UnSelectAll').change(function() {
+        ($(this).is(":checked") ? $('.Sub_Menu').prop("checked", false) : $('.Sub_Menu').prop("checked", false))
+
+        $('#SelectAll').prop("checked", false);
+        $('#UnSelectAll').prop("checked", true);
+
+        checkedValue = [];
+
+
+    });
+</script>
+
+<script>
+    $('#SavePrivilageMenu').click(function() {
+
+        console.log(checkedValue);
+        const swalWithBootstrapButtons = Swal.mixin({
+            confirmButtonClass: 'btn btn-success btn-sm',
+            cancelButtonClass: 'btn btn-danger btn-sm',
+            buttonsStyling: true,
+        })
+
+        swalWithBootstrapButtons.fire({
+
+            title: 'Are you sure?',
+            text: "Save this data?",
+            type: 'question',
+
+            showCancelButton: true,
+            confirmButtonText: '<img src="{{ asset("AdminLTE-master/dist/img/save.png") }}" width="13" alt=""><span style="color:black;">Yes, save it </span>',
+            cancelButtonText: '<img src="{{ asset("AdminLTE-master/dist/img/cancel.png") }}" width="13" alt=""><span style="color:black;"> No, cancel </span>',
+            confirmButtonColor: '#e9ecef',
+            cancelButtonColor: '#e9ecef',
+            reverseButtons: true
+        }).then((result) => {
+
+            if (result.value) {
+
+                var data = {
+                    checkedValue: checkedValue,
+                    user_role_id: $("#user_role_id").val(),
+                };
+
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+
+                $.ajax({
+                    type: 'POST',
+                    data: data,
+                    url: '{!! route("PrivilageMenu.store") !!}',
+                    success: function(data) {
+                        HideLoading();
+                        if (data.status == 200) {
+                            const swalWithBootstrapButtons = Swal.mixin({
+                                confirmButtonClass: 'btn btn-success btn-sm',
+                                cancelButtonClass: 'btn btn-danger btn-sm',
+                                buttonsStyling: true,
+                            })
+
+                            swalWithBootstrapButtons.fire({
+
+                                title: 'Successful !',
+                                type: 'success',
+                                html: 'Data has been saved',
+                                showCloseButton: false,
+                                showCancelButton: false,
+                                focusConfirm: false,
+                                confirmButtonText: '<span style="color:black;"> OK </span>',
+                                confirmButtonColor: '#4B586A',
+                                confirmButtonColor: '#e9ecef',
+                                reverseButtons: true
+                            }).then((result) => {
+                                if (result.value) {
+                                    ShowLoading();
+                                    window.location.href = '/PrivilageMenu';
+                                }
+                            })
+                        } else {
+                            ErrorNotif("Data Cancel Inputed");
+                        }
+
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        // FUNCTION ERROR NOTIFICATION 
+                        ErrorNotif("Data Cancel Inputed");
+                    }
+                });
+
+            } else if (
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+                HideLoading();
+                // FUNCTION ERROR NOTIFICATION 
+                CancelNotif("Data Cancel Inputed", '/PrivilageMenu');
+            }
+        })
+
+
+
+    });
+</script>
