27c14b60deeddd0d40c03272a5821c412b92af6d
ptqdctechnologies <pt.qdc.technologies@gmail.com>
2024-01-16 16:40:15 +0700

    Update Pertanggal 16 Januari 2023 16:40 WIB

--- Programming/.LaravelCore/app/Http/Controllers/Application/FrontEnd/SandBox/Examples_APICall/transaction/read/dataList/sysConfig/getAppObject_UserRoleGroupMember/v1/example.php
+++ Programming/.LaravelCore/app/Http/Controllers/Application/FrontEnd/SandBox/Examples_APICall/transaction/read/dataList/sysConfig/getAppObject_UserRoleGroupMember/v1/example.php
@@ -4,25 +4,27 @@
 +----------------------------------------------------------------------------------------------------------------------------------+
 | ▪ Category    : Example - API Call Controller                                                                                    |
 | ▪ Name Space  : \App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\transaction\read\dataList\sysConfig          |
-|                 \getAppObject_UserRole\v1                                                                                        |
-| ▪ API Key     : transaction.read.dataList.sysConfig.getAppObject_UserRole                                                        |
+|                 \getAppObject_UserRoleGroupMember\v1                                                                             |
+| ▪ API Key     : transaction.read.dataList.sysConfig.getAppObject_UserRoleGroupMember                                             |
 | ▪ API Version : 1                                                                                                                |
 |                                                                                                                                  |
-| ▪ Copyleft 🄯 2022 Zheta (teguhpjs@gmail.com)                                                                                     |
+| ▪ Copyleft 🄯 2024 Zheta (teguhpjs@gmail.com)                                                                                     |
 +----------------------------------------------------------------------------------------------------------------------------------+
 */
-namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\transaction\read\dataList\sysConfig\getAppObject_UserRole\v1
+namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\transaction\read\dataList\sysConfig\getAppObject_UserRoleGroupMember\v1
     {
     class example extends \App\Http\Controllers\Controller
         {
         /*
         +--------------------------------------------------------------------------------------------------------------------------+
-        | ▪ Call URL        : http(s)://<HOST>/transaction.read.dataList.sysConfig.getAppObject_UserRole.v1_throughAPIGateway      |
-        |                     ► http://172.28.0.4/transaction.read.dataList.sysConfig.getAppObject_UserRole.v1_throughAPIGateway   |
+        | ▪ Call URL        : http(s)://<HOST>/                                                                                    |
+        |                     transaction.read.dataList.sysConfig.getAppObject_UserRoleGroupMember.v1_throughAPIGateway            |
+        |                     ► http://172.28.0.4/                                                                                 |
+        |                       transaction.read.dataList.sysConfig.getAppObject_UserRoleGroupMember.v1_throughAPIGateway          |
         +--------------------------------------------------------------------------------------------------------------------------+
-        | ▪ Version         : 1.0001.0000000                                                                                       |
+        | ▪ Version         : 1.0000.0000000                                                                                       |
         | ▪ Last Update     : 2024-01-16                                                                                           |
-        | ▪ Creation Date   : 2022-12-30                                                                                           |
+        | ▪ Creation Date   : 2024-01-16                                                                                           |
         +--------------------------------------------------------------------------------------------------------------------------+
         */
         public function throughAPIGateway($varAPIWebToken)
@@ -35,7 +37,7 @@ namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\tra
             $varData = \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGateway(
                 \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
                 $varAPIWebToken, 
-                'transaction.read.dataList.sysConfig.getAppObject_UserRole', 
+                'transaction.read.dataList.sysConfig.getAppObject_UserRoleGroupMember', 
                 'latest', 
                 [
                 'parameter' => [
@@ -56,13 +58,13 @@ namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\tra
         /*
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Call URL        : http(s)://<HOST>/                                                                                    |
-        |                     transaction.read.dataList.sysConfig.getAppObject_UserRole.v1_throughAPIGatewayJQuery                 |
+        |                     transaction.read.dataList.sysConfig.getAppObject_UserRoleGroupMember.v1_throughAPIGatewayJQuery      |
         |                     ► http://172.28.0.4/                                                                                 |
-        |                       transaction.read.dataList.sysConfig.getAppObject_UserRole.v1_throughAPIGatewayJQuery               |
+        |                       transaction.read.dataList.sysConfig.getAppObject_UserRoleGroupMember.v1_throughAPIGatewayJQuery    |
         +--------------------------------------------------------------------------------------------------------------------------+
-        | ▪ Version         : 1.0001.0000000                                                                                       |
+        | ▪ Version         : 1.0000.0000000                                                                                       |
         | ▪ Last Update     : 2024-01-16                                                                                           |
-        | ▪ Creation Date   : 2022-12-30                                                                                           |
+        | ▪ Creation Date   : 2024-01-16                                                                                           |
         +--------------------------------------------------------------------------------------------------------------------------+
         */
         public function throughAPIGatewayJQuery($varAPIWebToken)
@@ -85,7 +87,7 @@ namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\tra
             $varJQueryFunction = \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGatewayJQuery(
                 \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(), 
                 $varAPIWebToken, 
-                'transaction.read.dataList.sysConfig.getAppObject_UserRole', 
+                'transaction.read.dataList.sysConfig.getAppObject_UserRoleGroupMember', 
                 'latest', 
                 '{'.
                     '"parameter" : {'.
