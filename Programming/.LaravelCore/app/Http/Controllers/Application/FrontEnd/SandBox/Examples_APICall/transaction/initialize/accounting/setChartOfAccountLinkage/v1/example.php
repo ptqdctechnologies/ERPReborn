8aa8c8caec3733c61fa83413d70d85c1927d17db
Teguh Pratama Januzir S <teguhpjs@gmail.com>
2024-05-02 18:13:13 +0700

    Update Pertanggal 2 Mei 2024 18:13 WIB

--- Programming/.LaravelCore/app/Http/Controllers/Application/FrontEnd/SandBox/Examples_APICall/transaction/initialize/accounting/setChartOfAccountLinkage/v1/example.php
+++ Programming/.LaravelCore/app/Http/Controllers/Application/FrontEnd/SandBox/Examples_APICall/transaction/initialize/accounting/setChartOfAccountLinkage/v1/example.php
@@ -4,21 +4,21 @@
 +----------------------------------------------------------------------------------------------------------------------------------+
 | ▪ Category    : Example - API Call Controller                                                                                    |
 | ▪ Name Space  : \App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\transaction\initialize\accounting            |
-|                 \setChartOfAccount\v1                                                                                     |
-| ▪ API Key     : transaction.initialize.accounting.setChartOfAccount                                                       |
+|                 \setChartOfAccountLinkage\v1                                                                                     |
+| ▪ API Key     : transaction.initialize.accounting.setChartOfAccountLinkage                                                       |
 | ▪ API Version : 1                                                                                                                |
 |                                                                                                                                  |
 | ▪ Copyleft 🄯 2024 Zheta (teguhpjs@gmail.com)                                                                                     |
 +----------------------------------------------------------------------------------------------------------------------------------+
 */
-namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\transaction\initialize\accounting\setChartOfAccount\v1
+namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\transaction\initialize\accounting\setChartOfAccountLinkage\v1
     {
     class example extends \App\Http\Controllers\Controller
         {
         /*
         +--------------------------------------------------------------------------------------------------------------------------+
-        | ▪ Call URL        : http(s)://<HOST>/transaction.initialize.accounting.setChartOfAccount.v1_throughAPIGateway            |
-        |                     ► http://172.28.0.4/transaction.initialize.accounting.setChartOfAccount.v1_throughAPIGateway         |
+        | ▪ Call URL        : http(s)://<HOST>/transaction.initialize.accounting.setChartOfAccountLinkage.v1_throughAPIGateway     |
+        |                     ► http://172.28.0.4/transaction.initialize.accounting.setChartOfAccountLinkage.v1_throughAPIGateway  |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Version         : 1.0000.0000000                                                                                       |
         | ▪ Last Update     : 2024-05-02                                                                                           |
@@ -36,7 +36,7 @@ namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\tra
                 \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGateway(
                     \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
                     $varAPIWebToken, 
-                    'transaction.initialize.accounting.setChartOfAccount', 
+                    'transaction.initialize.accounting.setChartOfAccountLinkage', 
                     'latest', 
                     [
                     ]
@@ -47,8 +47,10 @@ namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\tra
 
         /*
         +--------------------------------------------------------------------------------------------------------------------------+
-        | ▪ Call URL        : http(s)://<HOST>/transaction.initialize.accounting.setChartOfAccount.v1_throughAPIGatewayJQuery      |
-        |                     ► http://172.28.0.4/transaction.initialize.accounting.setChartOfAccount.v1_throughAPIGatewayJQuery   |
+        | ▪ Call URL        : http(s)://<HOST>/                                                                                    |
+        |                     transaction.initialize.accounting.setChartOfAccountLinkage.v1_throughAPIGatewayJQuery                |
+        |                     ► http://172.28.0.4/                                                                                 |
+        |                       transaction.initialize.accounting.setChartOfAccountLinkage.v1_throughAPIGatewayJQuery              |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Version         : 1.0000.0000000                                                                                       |
         | ▪ Last Update     : 2024-05-02                                                                                           |
@@ -67,7 +69,7 @@ namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\tra
                 \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGatewayJQuery(
                     \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(), 
                     $varAPIWebToken, 
-                    'transaction.initialize.accounting.setChartOfAccount', 
+                    'transaction.initialize.accounting.setChartOfAccountLinkage', 
                     'latest', 
                     '{'.
                     '}'
