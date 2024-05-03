bae2f9ea5eb15aae21cedfb633a05070d641fe5e
Teguh Pratama Januzir S <teguhpjs@gmail.com>
2024-05-03 11:38:43 +0700

    Update Pertanggal 3 Mei 2024 11:38 WIB

--- Programming/.LaravelCore/app/Http/Controllers/Application/BackEnd/System/UserAction/Engines/documentWorkFlow/approvalStage/setUserApprovalAcceptation/v1/setUserApprovalAcceptation.php
+++ Programming/.LaravelCore/app/Http/Controllers/Application/BackEnd/System/UserAction/Engines/documentWorkFlow/approvalStage/setUserApprovalAcceptation/v1/setUserApprovalAcceptation.php
@@ -63,19 +63,24 @@ namespace App\Http\Controllers\Application\BackEnd\System\UserAction\Engines\doc
                 try {
                     //---- ( MAIN CODE ) ------------------------------------------------------------------------- [ START POINT ] -----
                     try {
-                        if (!($varDataSend = \App\Helpers\ZhtHelper\System\BackEnd\Helper_API::getEngineDataSend_DataRead($varUserSession, (new \App\Models\Database\SchSysConfig\General())->setActionForBusinessDocumentWorkFlowPath(
-                            $varUserSession, 
-                            (\App\Helpers\ZhtHelper\System\BackEnd\Helper_API::getUserLoginSessionEntityByAPIWebToken($varUserSession))['branchID'],
-                                
-                            $varData['entities']['businessDocument_RefID'],
-                            (new \App\Models\Database\SchSysConfig\General())->getWorkFlowPathOfBusinessDocument(
-                                $varUserSession, 
-                                $varData['entities']['businessDocument_RefID']
-                                ),
-                            118000000000006,
-                            $varData['entities']['remarks'],
-                            $varData['entities']['approverEntity_RefID']
-                            ))))
+                        if (!($varDataSend =
+                            \App\Helpers\ZhtHelper\System\BackEnd\Helper_API::getEngineDataSend_DataRead(
+                                $varUserSession,
+                                (new \App\Models\Database\SchSysConfig\General())->setActionForBusinessDocumentWorkFlowPath(
+                                    $varUserSession, 
+                                    (\App\Helpers\ZhtHelper\System\BackEnd\Helper_API::getUserLoginSessionEntityByAPIWebToken($varUserSession))['branchID'],
+
+                                    $varData['entities']['businessDocument_RefID'],
+                                    (new \App\Models\Database\SchSysConfig\General())->getWorkFlowPathOfBusinessDocument(
+                                        $varUserSession, 
+                                        $varData['entities']['businessDocument_RefID']
+                                        ),
+                                    118000000000006,
+                                    $varData['entities']['remarks'],
+                                    $varData['entities']['approverEntity_RefID']
+                                    )
+                                )
+                            ))
                             {
                             throw new \Exception();
                             }
