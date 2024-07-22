d7eb993b579e7361c4639533fdd37ad1e54b1485
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2024-07-22 13:22:46 +0700

    Membuat Komponen Header ReportPRSummary

--- Programming/WebFrontEnd/resources/views/Purchase/PurchaseRequisition/Reports/ReportPurchaseRequisitionSummary.blade.php
+++ Programming/WebFrontEnd/resources/views/Purchase/PurchaseRequisition/Reports/ReportPurchaseRequisitionSummary.blade.php
@@ -15,126 +15,37 @@
             <div class="card">
                 <div class="tab-content p-3" id="nav-tabContent">
                     <div class="row">
-                        @if($var == 1)
-                            <div class="col-12 ShowDocument">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-md-3">
-                                                <div class="form-group">
-                                                    <table>
-                                                        <tr>
-                                                            <th style="padding-top: 7px;"><label>Budget&nbsp;</label></th>
-                                                            <td>
-                                                                <div class="input-group">
-                                                                    <input id="budget_id" style="border-radius:0;" class="form-control" name="budget_id" type="hidden">
-                                                                    <input id="budget" style="border-radius:0;background-color:white;" class="form-control myProject" name="budget" readonly data-toggle="modal" data-target="#myProject">
-                                                                    <div class="input-group-append">
-                                                                        <span style="border-radius:0;" class="input-group-text form-control">
-                                                                            <a href="#" id="budget_popup" data-toggle="modal" data-target="#myProject" class="myProject"><img src="{{ asset('AdminLTE-master/dist/img/box.png') }}" width="13" alt=""></a>
-                                                                        </span>
-                                                                    </div>
-                                                                </div>
-                                                            </td>
-                                                        </tr>
-                                                    </table>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-3">
-                                                <div class="form-group">
-                                                    <table>
-                                                        <tr>
-                                                            <th style="padding-top: 7px;"><label>Sub&nbsp;Budget&nbsp;</label></th>
-                                                            <td>
-                                                                <div class="input-group">
-                                                                    <input id="sub_budget_id" style="border-radius:0;" class="form-control" name="sub_budget_id" type="hidden">
-                                                                    <input id="sub_budget" style="border-radius:0;background-color:white;" class="form-control myProject" name="sub_budget" readonly data-toggle="modal" data-target="#myProject">
-                                                                    <div class="input-group-append">
-                                                                        <span style="border-radius:0;" class="input-group-text form-control">
-                                                                            <a href="#" id="sub_budget_popup" data-toggle="modal" data-target="#myProject" class="myProject"><img src="{{ asset('AdminLTE-master/dist/img/box.png') }}" width="13" alt=""></a>
-                                                                        </span>
-                                                                    </div>
-                                                                </div>
-                                                            </td>
-                                                        </tr>
-                                                    </table>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-3">
-                                                <div class="form-group">
-                                                    <table>
-                                                        <tr>
-                                                            <th style="padding-top: 7px;"><label>Supplier&nbsp;Code&nbsp;</label></th>
-                                                            <td>
-                                                                <div class="input-group">
-                                                                    <input id="work_id" style="border-radius:0;" class="form-control" name="work_id" type="hidden">
-                                                                    <input id="work" style="border-radius:0;background-color:white;" class="form-control myProject" name="work_id" readonly data-toggle="modal" data-target="#myProject">
-                                                                    <div class="input-group-append">
-                                                                        <span style="border-radius:0;" class="input-group-text form-control">
-                                                                            <a href="#" id="work_popup" data-toggle="modal" data-target="#myProject" class="myProject"><img src="{{ asset('AdminLTE-master/dist/img/box.png') }}" width="13" alt=""></a>
-                                                                        </span>
-                                                                    </div>
-                                                                </div>
-                                                            </td>
-                                                        </tr>
-                                                    </table>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-3">
-                                                <div class="form-group">
-                                                    <table>
-                                                        <tr>
-                                                            <td>
-                                                                <button class="btn btn-default btn-sm" type="submit">
-                                                                    <img src="{{ asset('AdminLTE-master/dist/img/backwards.png') }}" width="12" alt="" title="Show"> Show
-                                                                </button>
-                                                            </td>
-                                                            <td>
-                                                                <select name="" id="" class="form-control">
-                                                                    <option value="PDF">PDF</option>
-                                                                    <option value="Excel">Excel</option>
-                                                                </select>
-                                                            </td>
-                                                            <td>
-                                                                &nbsp;&nbsp;<span><img src="{{ asset('AdminLTE-master/dist/img/printer.png') }}" width="17" alt=""></span>
-                                                            </td>
-                                                        </tr>
-                                                    </table>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                        <div class="col-12 ShowDocument">
+                            @include('Purchase.PurchaseRequisition.Functions.Header.HeaderReportPurchaseRequisitionSummary')
+                        </div>
+                        <div class="col-12 ShowTableReportAdvanceSummary">
+                            <div class="card">
+                                <div class="card-body table-responsive p-0">
+                                    <table class="table table-head-fixed text-nowrap TableReportAdvanceSummary" id="TableReportPurchaseRequisitionSummary">
+                                        <thead>
+                                            <tr>
+                                                <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;background-color:#4B586A;color:white;">No</th>
+                                                <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;background-color:#4B586A;color:white;">Transaction Number</th>
+                                                <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;background-color:#4B586A;color:white;">Date</th>
+                                                <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;background-color:#4B586A;color:white;">Total IDR</th>
+                                                <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;background-color:#4B586A;color:white;">Total Other Currency</th>
+                                                <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;background-color:#4B586A;color:white;">Requestor</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>PO01-23000031</td>
+                                                <td>26/03/2023</td>
+                                                <td>200,000</td>
+                                                <td>0</td>
+                                                <td>Ferdian</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
-                            <div class="col-12 ShowTableReportAdvanceSummary">
-                                <div class="card">
-                                    <div class="card-body table-responsive p-0">
-                                        <table class="table table-head-fixed text-nowrap TableReportAdvanceSummary" id="TableReportAdvanceSummary" data-advance='@json($dataListAdvance)'>
-                                            <thead>
-                                                <tr>
-                                                    <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;background-color:#4B586A;color:white;">No</th>
-                                                    <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;background-color:#4B586A;color:white;">Transaction Number</th>
-                                                    <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;background-color:#4B586A;color:white;">Date</th>
-                                                    <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;background-color:#4B586A;color:white;">Total IDR</th>
-                                                    <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;background-color:#4B586A;color:white;">Total Other Currency</th>
-                                                    <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;background-color:#4B586A;color:white;">Requestor</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody id="dataBody">
-                                                <!-- <tr>
-                                                    <td>1</td>
-                                                    <td>PO01-23000031</td>
-                                                    <td>26/03/2023</td>
-                                                    <td>200,000</td>
-                                                    <td>0</td>
-                                                    <td>Ferdian</td>
-                                                </tr> -->
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        @endif
+                        </div>
                     </div>
                 </div>
             </div>
