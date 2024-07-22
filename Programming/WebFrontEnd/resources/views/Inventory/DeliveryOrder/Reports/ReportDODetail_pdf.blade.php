21cfe00604ad014c70b02f363bca3eb65fa5009d
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2024-07-22 10:58:03 +0700

    Mengganti Halaman ReportDODetail_pdf

--- Programming/WebFrontEnd/resources/views/Inventory/DeliveryOrder/Reports/ReportDODetail_pdf.blade.php
+++ Programming/WebFrontEnd/resources/views/Inventory/DeliveryOrder/Reports/ReportDODetail_pdf.blade.php
@@ -15,11 +15,155 @@
 <body>
     <div class="card-body table-responsive p-0">
         <div style="text-align: right; font-size: 14px;">July 18, 2024</div>
-        <div style="text-align: center; font-size: 20px; font-weight: bold;">DO Summary Report</div>
+        <div style="text-align: center; font-size: 20px; font-weight: bold;">DO Detail Report</div>
         <div style="text-align: right; font-size: 14px;">11.58 AM</div>
+        <table style="margin: 30px 0px 15px 1px;">
+            <tr>
+                <td style=" width: 350px;">
+                    <table>
+                        <tr>
+                            <td style="width: 90px; height: 20px;">
+                                <div style="font-size: 12px; font-weight: bold; line-height: 14px;">
+                                    DOR Number
+                                </div>
+                            </td>
+                            <td style="width: 5px;">
+                                :
+                            </td>
+                            <td style="height: 20px;">
+                                <div style="line-height: 14px;">
+                                    PR02-23000210
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <td style=" width: 350px;">
+                    <table>
+                        <tr>
+                            <td style="width: 90px; height: 20px;">
+                                <div style="font-size: 12px; font-weight: bold; line-height: 14px;">
+                                    Delivery From
+                                </div>
+                            </td>
+                            <td style="width: 5px;">
+                                :
+                            </td>
+                            <td style="height: 20px;">
+                                <div style="line-height: 14px;">
+                                    QDC
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td style=" width: 350px;">
+                    <table>
+                        <tr>
+                            <td style="width: 90px; height: 20px;">
+                                <div style="font-size: 12px; font-weight: bold; line-height: 14px;">
+                                    Budget
+                                </div>
+                            </td>
+                            <td style="width: 5px;">
+                                :
+                            </td>
+                            <td style="height: 20px;">
+                                <div style="line-height: 14px;">
+                                    Q000195 - Civil Work Batch 2 Pembangunan Pabrik Smelter Feronikel Kolaka
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <td style=" width: 350px;">
+                    <table>
+                        <tr>
+                            <td style="width: 90px; height: 20px;">
+                                <div style="font-size: 12px; font-weight: bold; line-height: 14px;">
+                                    Delivery To
+                                </div>
+                            </td>
+                            <td style="width: 5px;">
+                                :
+                            </td>
+                            <td style="height: 20px;">
+                                <div style="line-height: 14px;">
+                                    Gudang Tigaraksa
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td style=" width: 350px;">
+                    <table>
+                        <tr>
+                            <td style="width: 90px; height: 20px;">
+                                <div style="font-size: 12px; font-weight: bold; line-height: 14px;">
+                                    Sub Budget
+                                </div>
+                            </td>
+                            <td style="width: 5px;">
+                                :
+                            </td>
+                            <td style="height: 20px;">
+                                <div style="line-height: 14px;">
+                                    1000 - Overhead
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <td style=" width: 350px;">
+                    <table>
+                        <tr>
+                            <td style="width: 90px; height: 20px;">
+                                <div style="font-size: 12px; font-weight: bold; line-height: 14px;">
+                                    PIC
+                                </div>
+                            </td>
+                            <td style="width: 5px;">
+                                :
+                            </td>
+                            <td style="height: 20px;">
+                                <div style="line-height: 14px;">
+                                    PM
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td style=" width: 350px;">
+                    <table>
+                        <tr>
+                            <td style="width: 90px; height: 20px;">
+                                <div style="font-size: 12px; font-weight: bold; line-height: 14px;">
+                                    Date
+                                </div>
+                            </td>
+                            <td style="width: 5px;">
+                                :
+                            </td>
+                            <td style="height: 20px;">
+                                <div style="line-height: 14px;">
+                                    11/15/2023
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <td></td>
+            </tr>
+        </table>
 
         <!-- DISINI -->
-        <table class="TableReportAdvanceSummary" style="margin-left: 1px; width: 100%; margin-top: 30px;" id="TableReportAdvanceSummary">
+        <table class="TableReportAdvanceSummary" style="margin-left: 1px; width: 100%;" id="TableReportAdvanceSummary">
             <tr style="border-top: 1px solid black; border-bottom: 1px dotted black;">
                 <td style="border-top: 1px solid black; border-bottom: 1px dotted black; height: 20px;">
                     <div style="font-size: 12px; font-weight: bold; margin: 4px 0px 16px 0px;">
@@ -28,17 +172,22 @@
                 </td>
                 <td style="border-top: 1px solid black; border-bottom: 1px dotted black; height: 20px;">
                     <div style="font-size: 12px; font-weight: bold; margin: 4px 0px 16px 0px;">
-                        DOR Number
+                        DO Number
                     </div>
                 </td>
                 <td style="border-top: 1px solid black; border-bottom: 1px dotted black; height: 20px;">
                     <div style="font-size: 12px; font-weight: bold; margin: 4px 0px 16px 0px;">
-                        Budget Code
+                        Product Id
                     </div>
                 </td>
                 <td style="border-top: 1px solid black; border-bottom: 1px dotted black; height: 20px;">
                     <div style="font-size: 12px; font-weight: bold; margin: 4px 0px 16px 0px;">
-                        Date
+                        Qty
+                    </div>
+                </td>
+                <td style="border-top: 1px solid black; border-bottom: 1px dotted black; height: 20px;">
+                    <div style="font-size: 12px; font-weight: bold; margin: 4px 0px 16px 0px;">
+                        Unit Price
                     </div>
                 </td>
                 <td style="border-top: 1px solid black; border-bottom: 1px dotted black; height: 20px;">
@@ -48,7 +197,7 @@
                 </td>
                 <td style="border-top: 1px solid black; border-bottom: 1px dotted black; height: 20px;">
                     <div style="font-size: 12px; font-weight: bold; margin: 4px 0px 16px 0px;">
-                        Total Other Currency
+                        Remark
                     </div>
                 </td>
             </tr>
@@ -60,34 +209,39 @@
                 </td>
                 <td>
                     <div style="margin-top: 4px;">
-                        <?= Session::get("dataPDFReportDODetail.title"); ?>
+                        <?= Session::get("dataDetailReportDODetail.title"); ?>
                     </div>
                 </td>
                 <td>
                     <div style="margin-top: 4px;">
-                        <?= Session::get("dataPDFReportDODetail.number"); ?>
+                        <?= Session::get("dataDetailReportDODetail.number"); ?>
                     </div>
                 </td>
                 <td>
                     <div style="margin-top: 4px;">
-                        <?= Session::get("dataPDFReportDODetail.recordID"); ?>
+                        <?= Session::get("dataDetailReportDODetail.recordID"); ?>
                     </div>
                 </td>
                 <td>
                     <div style="margin-top: 4px;">
-                        <?= Session::get("dataPDFReportDODetail.recordID"); ?>
+                        <?= Session::get("dataDetailReportDODetail.recordID"); ?>
                     </div>
                 </td>
                 <td>
                     <div style="margin-top: 4px;">
-                        <?= Session::get("dataPDFReportDODetail.businessDocumentType_RefID"); ?>
+                        <?= Session::get("dataDetailReportDODetail.businessDocumentType_RefID"); ?>
+                    </div>
+                </td>
+                <td>
+                    <div style="margin-top: 4px;">
+                        <?= Session::get("dataDetailReportDODetail.date"); ?>
                     </div>
                 </td>
             </tr>
         </table>
         <!-- DISINI -->
          
-        <?php Session::forget("dataDetailReportDOSummary"); ?>
+        <?php Session::forget("dataDetailReportDORDetail"); ?>
     </div>
 </body>
 
