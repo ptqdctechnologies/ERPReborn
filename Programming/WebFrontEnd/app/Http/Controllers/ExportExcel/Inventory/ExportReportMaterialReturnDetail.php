8f12106bf5ea2b3be00fcb4a7838c7521d9dcf11
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2024-07-25 13:12:00 +0700

    Export Excel ReportMatReturnDetail

--- Programming/WebFrontEnd/app/Http/Controllers/ExportExcel/Inventory/ExportReportMaterialReturnDetail.php
+++ Programming/WebFrontEnd/app/Http/Controllers/ExportExcel/Inventory/ExportReportMaterialReturnDetail.php
@@ -4,87 +4,170 @@ namespace App\Http\Controllers\ExportExcel\Inventory;
 
 use Illuminate\Support\Facades\Session;
 use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
-use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\BeforeSheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
 use PhpOffice\PhpSpreadsheet\Style\Border;
-use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class ExportReportMaterialReturnDetail implements FromCollection, WithHeadings, ShouldAutoSize, WithStyles
+class ExportReportMaterialReturnDetail implements FromCollection, WithHeadings, ShouldAutoSize, WithEvents
 {
     public function collection()
     {
-        $data = Session::get("dataExcelReportMatReturnDetail");
-        return collect($data);
+        $data = Session::get("dataReportMatReturnDetail");
+        $dataDetail = $data['dataDetail'];
+        
+        $collection = collect();
+        foreach ($dataDetail as $detail) {
+            $collection->push(
+                [
+                    $detail['no'],
+                    $detail['doNumber'],
+                    $detail['productId'],
+                    $detail['qty'],
+                    $detail['uom'],
+                    $detail['remark'],
+                ]
+            );
+        }
+
+        $collection->push(
+            [
+                '',
+                '',
+                'Total Qty',
+                $data['totalQty'],
+                '',
+                '',
+                ''
+            ]
+        );
+
+        return $collection;
     }
 
     public function headings(): array
     {
         return [
-            ["Material Return Detail Report", " ", " ", " ", " ", " ", " "],
-            ["", "", "", "", "", "", ""],
-            ["No", "DO Number", "Product Id", "Qty", "Unit Price", "Total", "Remark"]
+            ["", "", "", "", "", ""],
+            ["No", "DO Number", "Product Id", "Qty", "Unit of Measure", "Remark"]
         ];
     }
 
-    public function styles(Worksheet $sheet)
+    public function registerEvents(): array
     {
-        $styleArrayHeader1 = [
-            'font' => [
-                'bold' => true,
-                'color' => [
-                    'rgb' => '000000',
-                ],
-            ],
-            'alignment' => [
-                'horizontal' => Alignment::HORIZONTAL_CENTER,
-            ]
+        return [
+            BeforeSheet::class => function (BeforeSheet $event) {
+                $sheet = $event->sheet->getDelegate();
+
+                $data = Session::get("dataReportMatReturnDetail");
+                $dataHeader = $data['dataHeader'];
+
+                $sheet->setCellValue('A1', date('F j, Y'))
+                    ->mergeCells('A1:F1')
+                    ->getStyle('A1:F1')
+                    ->applyFromArray([
+                        'font' => [
+                            'bold' => true,
+                            'color' => ['rgb' => '000000'],
+                        ],
+                        'alignment' => [
+                            'horizontal' => Alignment::HORIZONTAL_RIGHT,
+                        ],
+                ]);
+
+                $sheet->setCellValue('A2', 'Material Return Detail Report')
+                    ->mergeCells('A2:F2')
+                    ->getStyle('A2:F2')
+                    ->applyFromArray([
+                        'font' => [
+                            'bold' => true,
+                            'color' => ['rgb' => '000000'],
+                        ],
+                        'alignment' => [
+                            'horizontal' => Alignment::HORIZONTAL_CENTER,
+                        ],
+                ]);
+
+                $sheet->setCellValue('A3', date('h:i A'))
+                    ->mergeCells('A3:F3')
+                    ->getStyle('A3:F3')
+                    ->applyFromArray([
+                        'font' => [
+                            'bold' => true,
+                            'color' => ['rgb' => '000000'],
+                        ],
+                        'alignment' => [
+                            'horizontal' => Alignment::HORIZONTAL_RIGHT,
+                        ],
+                ]);
+
+                $sheet->setCellValue('A4', 'MR Number')->getStyle('A4')->applyFromArray([
+                    'font'  => [
+                        'bold'  => true,
+                        'color' => ['rgb' => '000000']
+                    ]
+                ]);
+                $sheet->setCellValue('B4', ': ' . $dataHeader['number']);
+
+                $sheet->setCellValue('A5', 'Budget')->getStyle('A5')->applyFromArray([
+                    'font'  => [
+                        'bold'  => true,
+                        'color' => ['rgb' => '000000']
+                    ]
+                ]);
+                $sheet->setCellValue('B5', ': ' . $dataHeader['recordID']);
+
+                $sheet->setCellValue('A6', 'Sub Budget')->getStyle('A6')->applyFromArray([
+                    'font'  => [
+                        'bold'  => true,
+                        'color' => ['rgb' => '000000']
+                    ]
+                ]);
+                $sheet->setCellValue('B6', ': ' . $dataHeader['businessDocumentType_RefID']);
+
+                $sheet->setCellValue('A7', 'Date')->getStyle('A7')->applyFromArray([
+                    'font'  => [
+                        'bold'  => true,
+                        'color' => ['rgb' => '000000']
+                    ]
+                ]);
+                $sheet->setCellValue('B7', ': ' . $dataHeader['date']);
+
+                $sheet->setCellValue('C4', 'Transporter')->getStyle('C4')->applyFromArray([
+                    'font'  => [
+                        'bold'  => true,
+                        'color' => ['rgb' => '000000']
+                    ]
+                ]);
+                $sheet->setCellValue('D4', ': ' . 'VDR-2594 - Aman Jaya');
+
+                $sheet->setCellValue('C5', 'Delivery From')->getStyle('C5')->applyFromArray([
+                    'font'  => [
+                        'bold'  => true,
+                        'color' => ['rgb' => '000000']
+                    ]
+                ]);
+                $sheet->setCellValue('D5', ': ' . 'QDC');
+
+                $sheet->setCellValue('C6', 'Delivery To')->getStyle('C6')->applyFromArray([
+                    'font'  => [
+                        'bold'  => true,
+                        'color' => ['rgb' => '000000']
+                    ]
+                ]);
+                $sheet->setCellValue('D6', ': ' . 'Gudang Tigaraksa');
+
+                $sheet->setCellValue('C7', 'PIC')->getStyle('C7')->applyFromArray([
+                    'font'  => [
+                        'bold'  => true,
+                        'color' => ['rgb' => '000000']
+                    ]
+                ]);
+                $sheet->setCellValue('D7', ': ' . 'PM');
+            },
         ];
-
-        $sheet->getStyle('A1:G1')->applyFromArray($styleArrayHeader1);
-        $sheet->mergeCells('A1:G1');
-
-
-        $styleArrayHeader2 = [
-            'font' => [
-                'bold' => true,
-                'color' => [
-                    'rgb' => '000000',
-                ],
-            ],
-            'alignment' => [
-                'horizontal' => Alignment::HORIZONTAL_CENTER,
-            ],
-            'borders' => [
-                'allBorders' => [
-                    'borderStyle' => Border::BORDER_THIN,
-                ],
-            ],
-            'fill' => [
-                'fillType' => 'solid',
-                'rotation' => 0,
-                'color' => [
-                    'rgb' => 'E9ECEF',
-                ],
-            ],
-        ];
-
-        $sheet->getStyle('A3:G3')->applyFromArray($styleArrayHeader2);
-
-        $styleArrayContent = [
-            'borders' => [
-                'allBorders' => [
-                    'borderStyle' => Border::BORDER_THIN,
-                ],
-            ],
-            'alignment' => [
-                'horizontal' => Alignment::HORIZONTAL_LEFT,
-            ],
-        ];
-
-        $totalCell = count(Session::get("dataExcelReportMatReturnDetail"));
-        $lastCell = 'A4:G' . ($totalCell + 3);
-        $sheet->getStyle($lastCell)->applyFromArray($styleArrayContent);
     }
 }
