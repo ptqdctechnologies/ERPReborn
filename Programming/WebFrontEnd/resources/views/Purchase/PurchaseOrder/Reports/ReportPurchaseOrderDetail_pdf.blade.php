75ba2b4561b1a3fcb56579612363d85d09cd3e5c
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2024-08-07 14:50:56 +0700

    Sementara

--- Programming/WebFrontEnd/resources/views/Purchase/PurchaseOrder/Reports/ReportPurchaseOrderDetail_pdf.blade.php
+++ Programming/WebFrontEnd/resources/views/Purchase/PurchaseOrder/Reports/ReportPurchaseOrderDetail_pdf.blade.php
@@ -500,7 +500,7 @@
                 </td>
                 <td style="border-top: 1px solid black; border-bottom: 1px solid black; height: 20px;">
                     <div style="font-size: 12px; font-weight: bold; margin: 4px 0px 4px 0px;">
-                        Product Id
+                        Product Name
                     </div>
                 </td>
                 <td style="border-top: 1px solid black; border-bottom: 1px solid black; height: 20px;">
