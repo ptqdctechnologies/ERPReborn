d044841b95bb4273bc3dca88ca450d94db590cc4
Teguh Pratama Januzir S <teguhpjs@gmail.com>
2025-01-13 17:39:40 +0700

    Update Pertanggal 13 Januari 2025 17:39 WIB

--- Programming/.LaravelCore/app/Http/Controllers/Application/FrontEnd/SandBox/Examples_APICall/transaction/update/finance/setCreditNoteDetail/v1/example.php
+++ Programming/.LaravelCore/app/Http/Controllers/Application/FrontEnd/SandBox/Examples_APICall/transaction/update/finance/setCreditNoteDetail/v1/example.php
@@ -3,22 +3,22 @@
 /*
 +----------------------------------------------------------------------------------------------------------------------------------+
 | ▪ Category    : Example - API Call Controller                                                                                    |
-| ▪ Name Space  : \App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\transaction\create\finance                   |
-|                 \setDebitNoteDetail\v1                                                                                           |
-| ▪ API Key     : transaction.create.finance.setDebitNoteDetail                                                                    |
+| ▪ Name Space  : \App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\transaction\update\finance                   |
+|                 \setCreditNoteDetail\v1                                                                                          |
+| ▪ API Key     : transaction.update.finance.setCreditNoteDetail                                                                   |
 | ▪ API Version : 1                                                                                                                |
 |                                                                                                                                  |
 | ▪ Copyleft 🄯 2025 Zheta (teguhpjs@gmail.com)                                                                                     |
 +----------------------------------------------------------------------------------------------------------------------------------+
 */
-namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\transaction\create\finance\setDebitNoteDetail\v1
+namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\transaction\update\finance\setCreditNoteDetail\v1
     {
     class example extends \App\Http\Controllers\Controller
         {
         /*
         +--------------------------------------------------------------------------------------------------------------------------+
-        | ▪ Call URL        : http(s)://<HOST>/transaction.create.finance.setDebitNoteDetail.v1_throughAPIGateway                  |
-        |                     ► http://172.28.0.4/transaction.create.finance.setDebitNoteDetail.v1_throughAPIGateway               |
+        | ▪ Call URL        : http(s)://<HOST>/transaction.update.finance.setCreditNoteDetail.v1_throughAPIGateway                 |
+        |                     ► http://172.28.0.4/transaction.update.finance.setCreditNoteDetail.v1_throughAPIGateway              |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Version         : 1.0000.0000000                                                                                       |
         | ▪ Last Update     : 2025-01-13                                                                                           |
@@ -37,20 +37,15 @@ namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\tra
             $varData =
                 \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGateway(
                     \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
-                    $varAPIWebToken, 
-                    'transaction.create.finance.setDebitNoteDetail', 
+                    $varAPIWebToken,
+                    'transaction.update.finance.setCreditNoteDetail', 
                     'latest', 
                     [
+                    'recordID' => 243000000000001,
                     'entities' => [
-                        "debitNote_RefID" => 240000000000001,
-                        "paymentInstruction_RefID" => 211000000000001,
-                        "product_RefID" => 88000000000083,
-                        "quantity" => 0.05,
-                        "quantityUnit_RefID" => 73000000000009,
-                        "productUnitPriceCurrency_RefID" => 62000000000001,
-                        "productUnitPriceCurrencyValue" => 235000,
-                        "productUnitPriceCurrencyExchangeRate" => 1,
-                        "remarks" => 'Catatan'
+                        "creditNote_RefID" => 242000000000001,
+                        "customerBankAccount_RefID" => 167000000000005,
+                        "remarks" => 'Catatan 1'
                         ]
                     ]
                     );
@@ -61,8 +56,8 @@ namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\tra
 
         /*
         +--------------------------------------------------------------------------------------------------------------------------+
-        | ▪ Call URL        : http(s)://<HOST>/transaction.create.finance.setDebitNoteDetail.v1_throughAPIGatewayJQuery            |
-        |                     ► http://172.28.0.4/transaction.create.finance.setDebitNoteDetail.v1_throughAPIGatewayJQuery         |
+        | ▪ Call URL        : http(s)://<HOST>/transaction.update.finance.setCreditNoteDetail.v1_throughAPIGatewayJQuery            |
+        |                     ► http://172.28.0.4/transaction.update.finance.setCreditNoteDetail.v1_throughAPIGatewayJQuery         |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Version         : 1.0000.0000000                                                                                       |
         | ▪ Last Update     : 2025-01-13                                                                                           |
@@ -81,34 +76,24 @@ namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\tra
             echo \App\Helpers\ZhtHelper\General\Helper_JavaScript::setLibrary(\App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System());
 
             echo '<table border="1" style="border-collapse: collapse;">';
-            echo    '<tr><td colspan="2" bgcolor="#6666cc" align="middle"><p style="color:#ffffff">Debit Note Detail Main Data</p></td></tr>';
-            echo        '<tr><td>DebitNote_RefID</td><td><input type="text" id="dataInput_DebitNote_RefID" value=240000000000001></td></tr>';
-            echo        '<tr><td>PaymentInstruction_RefID</td><td><input type="text" id="dataInput_PaymentInstruction_RefID" value=211000000000001></td></tr>';
-            echo        '<tr><td>Product_RefID</td><td><input type="text" id="dataInput_Product_RefID" value=88000000000083></td></tr>';
-            echo        '<tr><td>Quantity</td><td><input type="text" id="dataInput_Quantity" value=0.05></td></tr>';
-            echo        '<tr><td>QuantityUnit_RefID</td><td><input type="text" id="dataInput_QuantityUnit_RefID" value=73000000000009></td></tr>';
-            echo        '<tr><td>ProductUnitPriceCurrency_RefID</td><td><input type="text" id="dataInput_ProductUnitPriceCurrency_RefID" value=62000000000001></td></tr>';
-            echo        '<tr><td>ProductUnitPriceCurrencyValue</td><td><input type="text" id="dataInput_ProductUnitPriceCurrencyValue" value=235000></td></tr>';
-            echo        '<tr><td>ProductUnitPriceCurrencyExchangeRate</td><td><input type="text" id="dataInput_ProductUnitPriceCurrencyExchangeRate" value=1></td></tr>';
-            echo        '<tr><td>Remarks</td><td><input type="text" id="dataInput_Remarks" value="Catatan"></td></tr>';
+            echo    '<tr><td colspan="2" bgcolor="#6666cc" align="middle"><p style="color:#ffffff">Credit Note Detail Main Data</p></td></tr>';
+            echo        '<tr><td>RecordID</td><td><input type="text" id="dataInput_RecordID" value=243000000000001></td></tr>';
+            echo        '<tr><td>CreditNote_RefID</td><td><input type="text" id="dataInput_CreditNote_RefID" value=242000000000001></td></tr>';
+            echo        '<tr><td>CustomerBankAccount_RefID</td><td><input type="text" id="dataInput_CustomerBankAccount_RefID" value=167000000000005></td></tr>';
+            echo        '<tr><td>Remarks</td><td><input type="text" id="dataInput_Remarks" value="Catatan 1"></td></tr>';
             echo '</table><br>';
 
             $varJQueryFunction =
                 \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGatewayJQuery(
                     \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(), 
                     $varAPIWebToken, 
-                    'transaction.create.finance.setDebitNoteDetail', 
+                    'transaction.update.finance.setCreditNoteDetail', 
                     'latest',
                     '{'.
+                        '"recordID" : parseInt(document.getElementById("dataInput_RecordID").value), '.
                         '"entities" : {'.
-                            '"debitNote_RefID" : parseInt(document.getElementById("dataInput_DebitNote_RefID").value), '.
-                            '"paymentInstruction_RefID" : parseInt(document.getElementById("dataInput_PaymentInstruction_RefID").value), '.
-                            '"product_RefID" : parseInt(document.getElementById("dataInput_Product_RefID").value), '.
-                            '"quantity" : parseFloat(document.getElementById("dataInput_Quantity").value), '.
-                            '"quantityUnit_RefID" : parseInt(document.getElementById("dataInput_QuantityUnit_RefID").value), '.
-                            '"productUnitPriceCurrency_RefID" : parseInt(document.getElementById("dataInput_ProductUnitPriceCurrency_RefID").value), '.
-                            '"productUnitPriceCurrencyValue" : parseFloat(document.getElementById("dataInput_ProductUnitPriceCurrencyValue").value), '.
-                            '"productUnitPriceCurrencyExchangeRate" : parseFloat(document.getElementById("dataInput_ProductUnitPriceCurrencyExchangeRate").value), '.
+                            '"creditNote_RefID" : parseInt(document.getElementById("dataInput_CreditNote_RefID").value), '.
+                            '"customerBankAccount_RefID" : parseInt(document.getElementById("dataInput_CustomerBankAccount_RefID").value), '.
                             '"remarks" : document.getElementById("dataInput_Remarks").value'.
                             '}'.
                     '}'
