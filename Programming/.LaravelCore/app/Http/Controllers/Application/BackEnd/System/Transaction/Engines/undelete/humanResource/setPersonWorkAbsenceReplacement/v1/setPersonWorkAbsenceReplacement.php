a2685d22bc2026846e2fc6be720e4dae95aedce6
Teguh Pratama Januzir S <teguhpjs@gmail.com>
2025-01-31 16:54:27 +0700

    Update Pertanggal 31 Januari 2025 16:54 WIB

--- Programming/.LaravelCore/app/Http/Controllers/Application/BackEnd/System/Transaction/Engines/undelete/humanResource/setPersonWorkAbsenceReplacement/v1/setPersonWorkAbsenceReplacement.php
+++ Programming/.LaravelCore/app/Http/Controllers/Application/BackEnd/System/Transaction/Engines/undelete/humanResource/setPersonWorkAbsenceReplacement/v1/setPersonWorkAbsenceReplacement.php
@@ -3,21 +3,21 @@
 /*
 +----------------------------------------------------------------------------------------------------------------------------------+
 | ▪ Category   : API Engine Controller                                                                                             |
-| ▪ Name Space : \App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\delete\humanResource                         |
-|                \setPersonBusinessTripSequenceDetail\v1                                                                           |
+| ▪ Name Space : \App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\undelete\humanResource                       |
+|                \setPersonWorkAbsenceReplacement\v1                                                                               |
 |                                                                                                                                  |
 | ▪ Copyleft 🄯 2020 - 2025 Zheta (teguhpjs@gmail.com)                                                                              |
 +----------------------------------------------------------------------------------------------------------------------------------+
 */
-namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\delete\humanResource\setPersonBusinessTripSequenceDetail\v1
+namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\undelete\humanResource\setPersonWorkAbsenceReplacement\v1
     {
     /*
     +------------------------------------------------------------------------------------------------------------------------------+
-    | ▪ Class Name  : setPersonBusinessTripSequenceDetail                                                                          |
-    | ▪ Description : Menangani API transaction.delete.humanResource.setPersonBusinessTripSequenceDetail Version 1                 |
+    | ▪ Class Name  : setPersonWorkAbsenceReplacement                                                                              |
+    | ▪ Description : Menangani API transaction.undelete.humanResource.setPersonWorkAbsenceReplacement Version 1                   |
     +------------------------------------------------------------------------------------------------------------------------------+
     */
-    class setPersonBusinessTripSequenceDetail extends \App\Http\Controllers\Controller
+    class setPersonWorkAbsenceReplacement extends \App\Http\Controllers\Controller
         {
         /*
         +--------------------------------------------------------------------------------------------------------------------------+
@@ -62,15 +62,15 @@ namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\de
             try {
                 $varSysDataProcess =
                     \App\Helpers\ZhtHelper\Logger\Helper_SystemLog::setLogOutputMethodProcessHeader($varUserSession, __CLASS__, __FUNCTION__,
-                        'Delete Person Business Trip Sequence Detail Data (version 1)');
+                        'Undelete Person Work Absence Replacement Data (version 1)');
 
                 try {
                     //-----[ MAIN CODE ]----------------------------------------------------------------------------( START POINT )-----
                     try {
-                        if (!($varDataSend = 
-                            \App\Helpers\ZhtHelper\System\BackEnd\Helper_API::getEngineDataSend_DataDelete(
-                                $varUserSession, 
-                                (new \App\Models\Database\SchData_OLTP_HumanResource\TblPersonBusinessTripSequenceDetail())->setDataDelete(
+                        if (!($varDataSend =
+                            \App\Helpers\ZhtHelper\System\BackEnd\Helper_API::getEngineDataSend_DataUndelete(
+                                $varUserSession,
+                                (new \App\Models\Database\SchData_OLTP_HumanResource\TblPersonWorkAbsenceReplacement())->unsetDataDelete(
                                     $varUserSession,
                                     $varData['recordID']
                                     )
@@ -80,12 +80,12 @@ namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\de
                             throw new \Exception();
                             }
 
-                        $varReturn = 
+                        $varReturn =
                             \App\Helpers\ZhtHelper\System\BackEnd\Helper_API::setEngineResponseDataReturn_Success(
                                 $varUserSession,
                                 $varDataSend
                                 );
-                        }
+                        } 
 
                     catch (\Exception $ex) {
                         $varErrorMessage = $ex->getMessage();
@@ -98,7 +98,7 @@ namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\de
                         }
                     //-----[ MAIN CODE ]------------------------------------------------------------------------------( END POINT )-----
                     \App\Helpers\ZhtHelper\Logger\Helper_SystemLog::setLogOutputMethodProcessStatus($varUserSession, $varSysDataProcess, 'Success');
-                    } 
+                    }
 
                 catch (\Exception $ex) {
                     $varReturn =
