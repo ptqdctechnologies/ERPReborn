a1ceb58b7cbdc25f4fd369ff6fa1612146f1c33c
Teguh Pratama Januzir S <teguhpjs@gmail.com>
2025-02-03 12:44:10 +0700

    Update Pertanggal 03 Februari 2025 12:44 WIB

--- Programming/.LaravelCore/app/Http/Controllers/Application/BackEnd/System/Transaction/Engines/undelete/master/setBusinessDocumentNumberingFormat/v1/setBusinessDocumentNumberingFormat.php
+++ Programming/.LaravelCore/app/Http/Controllers/Application/BackEnd/System/Transaction/Engines/undelete/master/setBusinessDocumentNumberingFormat/v1/setBusinessDocumentNumberingFormat.php
@@ -3,21 +3,21 @@
 /*
 +----------------------------------------------------------------------------------------------------------------------------------+
 | ▪ Category   : API Engine Controller                                                                                             |
-| ▪ Name Space : \App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\delete\master\setBusinessDocumentNumbering   |
-|                \v1                                                                                                               |
+| ▪ Name Space : \App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\undelete\master                              | 
+|                \setBusinessDocumentNumberingFormat\v1                                                                            |
 |                                                                                                                                  |
 | ▪ Copyleft 🄯 2020 - 2025 Zheta (teguhpjs@gmail.com)                                                                              |
 +----------------------------------------------------------------------------------------------------------------------------------+
 */
-namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\delete\master\setBusinessDocumentNumbering\v1
+    namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\undelete\master\setBusinessDocumentNumberingFormat\v1
     {
     /*
     +------------------------------------------------------------------------------------------------------------------------------+
-    | ▪ Class Name  : setBusinessDocumentNumbering                                                                                 |
-    | ▪ Description : Menangani API transaction.delete.master.setBusinessDocumentNumbering Version 1                               |
+    | ▪ Class Name  : setBusinessDocumentNumberingFormat                                                                                 |
+    | ▪ Description : Menangani API transaction.undelete.master.setBusinessDocumentNumberingFormat Version 1                             |
     +------------------------------------------------------------------------------------------------------------------------------+
     */
-    class setBusinessDocumentNumbering extends \App\Http\Controllers\Controller
+    class setBusinessDocumentNumberingFormat extends \App\Http\Controllers\Controller
         {
         /*
         +--------------------------------------------------------------------------------------------------------------------------+
@@ -45,7 +45,7 @@ namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\de
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Version         : 1.0000.0000001                                                                                       |
         | ▪ Last Update     : 2025-02-03                                                                                           |
-        | ▪ Creation Date   : 2020-12-07                                                                                           |
+        | ▪ Cretion Date    : 2020-11-30                                                                                           |
         | ▪ Description     : Fungsi Utama Engine                                                                                  |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Input Variable  :                                                                                                      |
@@ -62,15 +62,15 @@ namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\de
             try {
                 $varSysDataProcess =
                     \App\Helpers\ZhtHelper\Logger\Helper_SystemLog::setLogOutputMethodProcessHeader($varUserSession, __CLASS__, __FUNCTION__,
-                        'Delete Business Document Numbering Data (version 1)');
+                        'Undelete Business Document Numbering Format Data (version 1)');
 
                 try {
                     //-----[ MAIN CODE ]----------------------------------------------------------------------------( START POINT )-----
                     try {
                         if (!($varDataSend = 
-                            \App\Helpers\ZhtHelper\System\BackEnd\Helper_API::getEngineDataSend_DataDelete(
+                            \App\Helpers\ZhtHelper\System\BackEnd\Helper_API::getEngineDataSend_DataUndelete(
                                 $varUserSession, 
-                                (new \App\Models\Database\SchData_OLTP_Master\TblBusinessDocumentNumbering())->setDataDelete(
+                                (new \App\Models\Database\SchData_OLTP_Master\TblBusinessDocumentNumberingFormat())->unsetDataDelete(
                                     $varUserSession,
                                     $varData['recordID']
                                     )
@@ -80,12 +80,12 @@ namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\de
                             throw new \Exception();
                             }
 
-                        $varReturn = 
+                        $varReturn =
                             \App\Helpers\ZhtHelper\System\BackEnd\Helper_API::setEngineResponseDataReturn_Success(
                                 $varUserSession,
                                 $varDataSend
                                 );
-                        }
+                        } 
 
                     catch (\Exception $ex) {
                         $varErrorMessage = $ex->getMessage();
@@ -98,7 +98,7 @@ namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\de
                         }
                     //-----[ MAIN CODE ]------------------------------------------------------------------------------( END POINT )-----
                     \App\Helpers\ZhtHelper\Logger\Helper_SystemLog::setLogOutputMethodProcessStatus($varUserSession, $varSysDataProcess, 'Success');
-                    } 
+                    }
 
                 catch (\Exception $ex) {
                     $varReturn =
