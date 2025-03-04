eec6d53fbc68f2f45087a4fc1cca7d0614255e3b
ijonk7 <jookeo.rizal@gmail.com>
2025-03-04 10:48:58 +0700

    Create backend service 'transaction.read.dataList.humanResource.getBSFList'

--- Programming/.LaravelCore/app/Models/BackEnd/Database/SchData_OLTP_HumanResource/General.php
+++ Programming/.LaravelCore/app/Models/BackEnd/Database/SchData_OLTP_HumanResource/General.php
@@ -223,6 +223,53 @@ namespace App\Models\Database\SchData_OLTP_HumanResource
 
         /*
         +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Method Name     : getDataList_BSFList                                                                |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Version         : 1.0000.0000000                                                                                       |
+        | ▪ Last Update     : 2025-03-04                                                                                           |
+        | ▪ Creation Date   : 2025-03-04                                                                                           |
+        | ▪ Description     : Mendapatkan Daftar Settlement Perjalanan Bisnis Perorangan                                                      |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Input Variable  :                                                                                                      |
+        |      ▪ (mixed)  varUserSession ► User Session                                                                            |
+        |      ▪ (int)    varBranchID ► Branch ID                                                                                  |
+        |      ------------------------------                                                                                      |
+        |      ------------------------------                                                                                      |
+        |      ▪ (string) varPickStatement ► Pick Statement                                                                        |
+        |      ▪ (string) varSortStatement ► Sort Statement                                                                        |
+        |      ▪ (string) varFilterStatement ► Filter Statement                                                                    |
+        |      ▪ (string) varPagingStatement ► Paging Statement                                                                    |
+        | ▪ Output Variable :                                                                                                      |
+        |      ▪ (array)  varReturn                                                                                                |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        */
+       public function getDataList_BSFList(
+        $varUserSession, int $varBranchID,
+        string $varPickStatement = null, string $varSortStatement = null, string $varFilterStatement = null, string $varPagingStatement = null)
+        {
+        try {
+            $varReturn =
+                \App\Helpers\ZhtHelper\Database\Helper_PostgreSQL::getQueryExecution(
+                    $varUserSession,
+                    \App\Helpers\ZhtHelper\Database\Helper_PostgreSQL::getBuildStringLiteral_StoredProcedure(
+                        $varUserSession,
+                        'SchData-OLTP-HumanResource.Func_GetDataList_PersonBusinessTrip',
+                        [
+                            [$varBranchID, 'bigint' ]
+                        ]
+                        )
+                    );
+
+            return $varReturn['data'];
+            }
+        catch (\Exception $ex) {
+            return [];
+            }
+        }
+
+
+        /*
+        +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Method Name     : getDataList_BusinessTripCostComponentEntity                                                          |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Version         : 1.0000.0000000                                                                                       |
