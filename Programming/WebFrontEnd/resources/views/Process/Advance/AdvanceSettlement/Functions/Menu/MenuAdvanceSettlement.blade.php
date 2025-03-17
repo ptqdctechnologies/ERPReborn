ac80cb28321f53965e7e6836c7681f007f93d885
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-03-17 16:56:52 +0700

    Commit Sementara

--- Programming/WebFrontEnd/resources/views/Process/Advance/AdvanceSettlement/Functions/Menu/MenuAdvanceSettlement.blade.php
+++ Programming/WebFrontEnd/resources/views/Process/Advance/AdvanceSettlement/Functions/Menu/MenuAdvanceSettlement.blade.php
@@ -38,32 +38,32 @@
         }
     });
 
-    $(function() {
-        $('.myPopUpAdvanceSettlementRevision').on('click', function(e) {
+    // $(function() {
+        // $('.myPopUpAdvanceSettlementRevision').on('click', function(e) {
+        $(window).one('load', function(e) {
             e.preventDefault();
             var keys = 0;
             $.ajax({
                 type: 'GET',
                 url: '{!! route("AdvanceSettlement.AdvanceSettlementListData") !!}',
                 success: function(data) {
-                    console.log(data);
+                    console.log('data', data);
                     var no = 1; t = $('#TableSearchAsfRevision').DataTable();
                     t.clear();
                     $.each(data, function(key, val) {
                         keys += 1;
                         t.row.add([
-                            '<tbody><tr><input id="sys_id_advance_settlemetn_revision' + keys + '" value="' + val.Sys_ID + '" type="hidden"><td>' + no++ + '</td>',
-                            '<td>' + val.DocumentNumber + '</td>',
-                            '<td>' + val.CombinedBudgetCode + '</td>',
-                            '<td>' + val.CombinedBudgetName + '</td>',
-                            '<td>' + val.CombinedBudgetSectionCode + '</td>',
-                            '<td>' + val.CombinedBudgetSectionName + '</td></tr></tbody>'
+                            '<tbody><tr><input id="sys_id_advance_settlemetn_revision' + keys + '" value="' + val.sys_ID + '" type="hidden"><td>' + no++ + '</td>',
+                            '<td>' + val.documentNumber + '</td>',
+                            '<td>' + val.combinedBudgetCode + '</td>',
+                            '<td>' + val.combinedBudgetName + '</td>',
+                            '<td>' + val.combinedBudgetSectionCode + '</td>',
+                            '<td>' + val.combinedBudgetSectionName + '</td></tr></tbody>'
                         ]).draw();
-
                     });
                 }
             });
         });
 
-    });
+    // });
 </script>
--- Programming/WebFrontEnd/resources/views/Process/Advance/AdvanceSettlement/Transactions/CreateAdvanceSettlement.blade.php
+++ Programming/WebFrontEnd/resources/views/Process/Advance/AdvanceSettlement/Transactions/CreateAdvanceSettlement.blade.php
@@ -3,6 +3,7 @@
 @include('Partials.navbar')
 @include('Partials.sidebar')
 @include('getFunction.getAdvance')
+@include('Process.Advance.AdvanceSettlement.Functions.PopUp.PopUpAdvanceSettlementRevision')
 
 <div class="content-wrapper">
   <section class="content">
