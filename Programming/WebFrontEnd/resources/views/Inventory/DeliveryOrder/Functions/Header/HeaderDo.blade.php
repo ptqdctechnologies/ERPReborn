c6b0a555330230caf8c261e40aadc6fe30fcdcdd
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-04-03 00:16:46 +0700

    Menambahkan Delivery From & To

--- Programming/WebFrontEnd/resources/views/Inventory/DeliveryOrder/Functions/Header/HeaderDo.blade.php
+++ Programming/WebFrontEnd/resources/views/Inventory/DeliveryOrder/Functions/Header/HeaderDo.blade.php
@@ -1,7 +1,9 @@
 <!-- BODY -->
 <div class="card-body">
   <div class="row py-3" style="gap: 15px;">
+    <!-- LEFT COLUMN -->
     <div class="col-md-12 col-lg-5">
+      <!-- REFERENCE NUMBER -->
       <div class="row">
         <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">
           Reference Number
@@ -17,6 +19,33 @@
                 <img src="{{ asset('AdminLTE-master/dist/img/box.png') }}" width="13" alt="referenceNumberTrigger">
               </a>
             </span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- RIGHT COLUMN -->
+    <div class="col-md-12 col-lg-5">
+      <!-- DELIVERY FROM -->
+      <div class="row" style="margin-bottom: 1rem;">
+        <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">
+          Delivery From
+        </label>
+        <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0">
+          <div>
+            <textarea id="delivery_from" rows="3" style="border-radius:0;" class="form-control"></textarea>
+          </div>
+        </div>
+      </div>
+
+      <!-- DELIVERY TO -->
+      <div class="row">
+        <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">
+          Delivery To
+        </label>
+        <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0">
+          <div>
+            <textarea id="delivery_to" rows="3" style="border-radius:0;" class="form-control"></textarea>
           </div>
         </div>
       </div>
