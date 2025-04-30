8f28ccc74f40dd5496aaf767d615f18232bcd60d
ijonk7 <jookeo.rizal@gmail.com>
2025-04-30 16:12:59 +0700

    Create example API call 'report.form.documentForm.supplyChain.getPurchaseRequisitionSummary'

---
+++ Programming/.LaravelCore/app/Http/Controllers/Application/FrontEnd/SandBox/Examples_APICall/report/form/documentForm/supplyChain/getPurchaseRequisitionSummary/v1/example.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
++----------------------------------------------------------------------------------------------------------------------------------+
+| ▪ Category    : Example - API Call Controller                                                                                    |
+| ▪ Name Space  : \App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\report\form\documentForm\supplyChain         |
+|                 \getPurchaseRequisitionSummary\v1                                                                                       |
+| ▪ API Key     : report.form.documentForm.taxation.getPurchaseRequisitionSummary                                                         |
+| ▪ API Version : 1                                                                                                                |
+|                                                                                                                                  |
+| ▪ Copyleft 🄯 2025 ijonk7 (rizal.devapps@gmail.com)                                                                                     |
++----------------------------------------------------------------------------------------------------------------------------------+
+*/
+namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\report\form\documentForm\supplyChain\getPurchaseRequisitionSummary\v1
+    {
+    class example extends \App\Http\Controllers\Controller
+        {
+        /*
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Call URL        : http(s)://<HOST>/report.form.documentForm.supplyChain.getPurchaseRequisitionSummary.v1_throughAPIGateway    |
+        |                     ► http://172.28.0.4/report.form.documentForm.supplyChain.getPurchaseRequisitionSummary.v1_throughAPIGateway |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Version         : 1.0000.0000000                                                                                       |
+        | ▪ Last Update     : 2025-04-30                                                                                           |
+        | ▪ Creation Date   : 2025-04-30                                                                                           |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        */
+        public function throughAPIGateway($varAPIWebToken)
+            {
+            //---Parameter Set---
+            if (!$varAPIWebToken) {
+                $varAPIWebToken = \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System();
+                }
+            //---Core---
+            $varData = \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGateway(
+                \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
+                $varAPIWebToken,
+                'report.form.documentForm.supplyChain.getPurchaseRequisitionSummary',
+                'latest',
+                [
+                'parameter' => [
+                    'CombinedBudgetCode' => '',
+                    'CombinedBudgetSectionCode' => '222'
+                    ]
+                ]
+                );
+            return $varData;
+            }
+        }
+    }
