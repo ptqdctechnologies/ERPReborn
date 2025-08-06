57ab1ed363b4c0a9cb0006d2ab41d25bc27b9f63
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-08-06 12:01:20 +0700

    feat(form-pr): add confirmation modal

--- Programming/WebFrontEnd/resources/views/Purchase/PurchaseRequisition/Transactions/CreatePurchaseRequisition.blade.php
+++ Programming/WebFrontEnd/resources/views/Purchase/PurchaseRequisition/Transactions/CreatePurchaseRequisition.blade.php
@@ -141,7 +141,7 @@
           </div>
 
           <!-- PURCHASE REQUEST (CART) -->
-          <div class="tab-content px-3 pb-2" id="nav-tabContent">
+          {{-- <div class="tab-content px-3 pb-2" id="nav-tabContent">
             <div class="row">
               <div class="col-12">
                 <div class="card">
@@ -197,7 +197,7 @@
                 </div>
               </div>
             </div>
-          </div>
+          </div> --}}
 
           <!-- BUTTON -->
           <div class="tab-content px-3 pb-2" id="nav-tabContent">
@@ -207,9 +207,13 @@
                   <img src="{{ asset('AdminLTE-master/dist/img/cancel.png') }}" width="13" alt="" title="Cancel Advance List Cart"> Cancel
                 </a>
 
-                <button class="btn btn-default btn-sm float-right" type="submit" id="submitPR" style="margin-right: 5px;background-color:#e9ecef;border:1px solid #ced4da;">
+                <button type="button" id="budget-details-add" class="btn btn-default btn-sm float-right" data-toggle="modal" data-target="#purchaseRequestFormModal" style="margin-right: 5px;background-color:#e9ecef;border:1px solid #ced4da;">
                   <img src="{{ asset('AdminLTE-master/dist/img/save.png') }}" width="13" alt="" title="Submit to Advance"> Submit
                 </button>
+
+                {{-- <button class="btn btn-default btn-sm float-right" type="submit" id="submitPR" style="margin-right: 5px;background-color:#e9ecef;border:1px solid #ced4da;">
+                  <img src="{{ asset('AdminLTE-master/dist/img/save.png') }}" width="13" alt="" title="Submit to Advance"> Submit
+                </button> --}}
               </div>
             </div>
           </div>
@@ -221,17 +225,36 @@
 </div>
 
 <div class="modal fade" id="purchaseRequestFormModal" tabindex="-1" role="dialog">
-  <div class="modal-dialog modal-xl" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h3 style="margin: 0px;font-weight:bold;">Are you sure you want to save this data?</h3>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
       </div>
       <div class="modal-body">
-        <div class="wrapper-budget card-body table-responsive p-0" style="max-height:230px;">
-          <table class="table table-head-fixed text-nowrap table-sm" id="tablePurchaseRequisitionList">
+        <div class="wrapper-budget card-body table-responsive p-0" style="max-height:200px;">
+          <table class="table table-head-fixed text-nowrap table-sm" id="tablePurchaseRequisitionList" style="border: 1px solid #dee2e6;">
+            <tbody></tbody>
           </table>
         </div>
+        {{-- <div class="card-body">
+          <table style="float:right;">
+            <tr>
+              <th> Total Item :
+                <span id="GrandTotal">0.00</span>
+              </th>
+            </tr>
+          </table>
+        </div> --}}
+      </div>
+      <div class="modal-footer">
+        <button type="button" id="submitPR" class="btn btn-default btn-sm" onclick="SubmitForm();" style="margin-right: 5px;background-color:#e9ecef;border:1px solid #ced4da;">
+          <img src="{{ asset('AdminLTE-master/dist/img/save.png') }}" width="13" alt="" title="Submit to Advance"> Yes, save it
+        </button>
+
+        <button type="button" class="btn btn-default btn-sm" data-dismiss="modal" style="background-color:#e9ecef;border:1px solid #ced4da;">
+          <img src="{{ asset('AdminLTE-master/dist/img/cancel.png') }}" width="13" alt="" title="Cancel Advance List Cart"> No, cancel
+        </button>
       </div>
     </div>
   </div>
