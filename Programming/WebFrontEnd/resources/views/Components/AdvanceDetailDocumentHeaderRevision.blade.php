c29f51167d8d3f9f9271467bf662840a86c7d908
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-08-13 16:39:04 +0700

    feat(dt-arf): adjust scroll, and width table

--- Programming/WebFrontEnd/resources/views/Components/AdvanceDetailDocumentHeaderRevision.blade.php
+++ Programming/WebFrontEnd/resources/views/Components/AdvanceDetailDocumentHeaderRevision.blade.php
@@ -1,32 +1,63 @@
-<div class="wrapper-budget table-responsive table-height">
-    <table class="table table-striped table-hover table-sticky table-sm">
-        <thead>
-            <tr>
-                @if(sizeof($dataHeaderTransactionHistory))
-                    @for($i = count($dataHeaderTransactionHistory) - 1; $i >= 0; $i--) 
-                        <?php $entryDateTime = $dataHeaderTransactionHistory[$i]['content']['sys_Data_Entry_DateTimeTZ']; $editDateTime = $dataHeaderTransactionHistory[$i]['content']['sys_Data_Edit_DateTimeTZ']; ?>
+<div class="table-budget-wrapper" style="display: flex;">
+    <!-- Kolom Fixed -->
+    <div class="table-fixed" style="flex: 0 0 auto;">
+        <table class="table table-striped table-hover table-sm">
+            <thead>
+                <tr>
+                    @if(sizeof($dataHeaderTransactionHistory))
+                        @for($i = count($dataHeaderTransactionHistory) - 1; $i >= 0; $i--) 
+                            <?php $entryDateTime = $dataHeaderTransactionHistory[$i]['content']['sys_Data_Entry_DateTimeTZ']; $editDateTime = $dataHeaderTransactionHistory[$i]['content']['sys_Data_Edit_DateTimeTZ']; ?>
 
-                        @if ($i === count($dataHeaderTransactionHistory) - 1)
-                            <th colspan="5" style="vertical-align: middle;left: 0px;z-index: 10;line-height: normal;position: sticky;background-color: white;" class="text-center">
-                                Rev {{ $i }} (Actual) - {{ $dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['submitterWorkerName'] }} <br /> ( {{ date('Y-m-d', strtotime($dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['content']['sys_Data_Edit_DateTimeTZ'])) }} {{ date('H:i', strtotime($dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['content']['sys_Data_Edit_DateTimeTZ'])) }} )
-                            </th>
-                        @else
-                            <th colspan="5" style="text-align: center;background-color:#4B586A;color:white;border-right:1px solid #e9ecef;line-height: normal;vertical-align: middle;">
-                                {{ !empty($editDateTime) ? 'Rev ' . $i : 'Original' }} - {{ $dataHeaderTransactionHistory[$i]['submitterWorkerName'] }} <br /> ( {{ !empty($editDateTime) ? date('Y-m-d', strtotime($editDateTime)) . " " . date('H:i', strtotime($editDateTime)) : date('Y-m-d', strtotime($entryDateTime)) . " " . date('H:i', strtotime($entryDateTime)) }} )
-                            </th>
-                        @endif
-                    @endfor
-                @endif
-            </tr>
-            @if(sizeof($dataHeaderTransactionHistory))
+                            @if ($i === count($dataHeaderTransactionHistory) - 1)
+                                <th class="text-center" colspan="5" style="vertical-align: middle;left: 0px;z-index: 10;line-height: normal;position: sticky;background: #FFF;">
+                                    Rev {{ $i }} (Actual) - {{ $dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['submitterWorkerName'] }} <br />( {{ date('Y-m-d', strtotime($dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['content']['sys_Data_Edit_DateTimeTZ'])) }} {{ date('H:i', strtotime($dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['content']['sys_Data_Edit_DateTimeTZ'])) }} )
+                                </th>
+                            @endif
+                        @endfor
+                    @endif
+                </tr>
                 <tr>
                     <th style="vertical-align: middle; width: 125px; min-width: 125px; max-width: 125px; left: 0px; z-index: 10;position: sticky;background-color: white;" class="text-center">Requester</th>
                     <th style="vertical-align: middle; width: 125px; min-width: 125px; max-width: 125px; left: 125px; z-index: 10;position: sticky;background-color: white;" class="text-center">Beneficiary</th>
                     <th style="vertical-align: middle; width: 125px; min-width: 125px; max-width: 125px; left: 250px; z-index: 10;position: sticky;background-color: white;" class="text-center">Bank Name</th>
                     <th style="vertical-align: middle; width: 125px; min-width: 125px; max-width: 125px; left: 375px; z-index: 10;position: sticky;background-color: white;" class="text-center">Bank Account</th>
-                    {{-- <th style="vertical-align: middle; width: 70px; min-width: 70px; max-width: 70px; left: 500px; z-index: 10;position: sticky;background-color: white;" class="text-center">Currency</th> --}}
-                    <th style="vertical-align: middle; width: 250px; min-width: 250px; max-width: 250px; left: 500px; z-index: 10;position: sticky;background-color: white;" class="text-center">Remark</th>
+                    <th style="vertical-align: middle; width: 150px; min-width: 150px; max-width: 150px; left: 500px; z-index: 10;position: sticky;background-color: white;" class="text-center">Remark</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    @for($i = count($dataHeaderTransactionHistory) - 1; $i >= 0; $i--)
+                        @if ($i === count($dataHeaderTransactionHistory) - 1)
+                            <td style="padding: 8px; width: 125px; min-width: 125px; max-width: 125px; position: sticky; background-color: white; left: 0px; z-index: 10;">{{ $dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['requesterWorkerName'] }}</td>
+                            <td style="padding: 8px; width: 125px; min-width: 125px; max-width: 125px; position: sticky; background-color: white; left: 125px; z-index: 10;">{{ $dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['beneficiaryWorkerName'] }}</td>
+                            <td style="padding: 8px; width: 125px; min-width: 125px; max-width: 125px; position: sticky; background-color: white; left: 250px; z-index: 10;">{{ $dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['requesterWorkerName'] }}</td>
+                            <td style="padding: 8px; width: 125px; min-width: 125px; max-width: 125px; position: sticky; background-color: white; left: 375px; z-index: 10;">{{ $dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['beneficiaryWorkerName'] }}</td>
+                            <td style="padding: 8px; width: 150px; min-width: 150px; max-width: 150px; position: sticky; background-color: white; left: 500px; z-index: 10;">{{ $dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['content']['remarks'] }}</td>
+                        @endif
+                    @endfor
+                </tr>
+            </tbody>
+        </table>
+    </div>
 
+    <!-- Kolom Scroll -->
+    <div class="table-scroll" style="overflow-x: auto;">
+        <table class="table table-striped table-hover table-sm">
+            <thead>
+                <tr>
+                    @if(sizeof($dataHeaderTransactionHistory))
+                        @for($i = count($dataHeaderTransactionHistory) - 1; $i >= 0; $i--) 
+                            <?php $entryDateTime = $dataHeaderTransactionHistory[$i]['content']['sys_Data_Entry_DateTimeTZ']; $editDateTime = $dataHeaderTransactionHistory[$i]['content']['sys_Data_Edit_DateTimeTZ']; ?>
+
+                            @if ($i !== count($dataHeaderTransactionHistory) - 1)
+                                <th class="text-center" colspan="5" style="background-color:#4B586A;color:white;border-right:1px solid #e9ecef;line-height: normal;vertical-align: middle; min-width: 320px;">
+                                    {{ !empty($editDateTime) ? 'Rev ' . $i : 'Original' }} - {{ $dataHeaderTransactionHistory[$i]['submitterWorkerName'] }} <br /> ( {{ !empty($editDateTime) ? date('Y-m-d', strtotime($editDateTime)) . " " . date('H:i', strtotime($editDateTime)) : date('Y-m-d', strtotime($entryDateTime)) . " " . date('H:i', strtotime($entryDateTime)) }} )
+                                </th>
+                            @endif
+                        @endfor
+                    @endif
+                </tr>
+                <tr>
                     @for($i = 1; $i < count($dataHeaderTransactionHistory); $i++) 
                         <th class="text-center" style="width: 125px; min-width: 125px; max-width: 125px;background-color:#4B586A;color:white;border-right:1px solid #e9ecef;vertical-align: middle;">Requester</th>
                         <th class="text-center" style="width: 125px; min-width: 125px; max-width: 125px;background-color:#4B586A;color:white;border-right:1px solid #e9ecef;vertical-align: middle;">Beneficiary</th>
@@ -35,20 +66,11 @@
                         <th class="text-center" style="background-color:#4B586A;color:white;border-right:1px solid #e9ecef;vertical-align: middle;width: 250px; min-width: 250px; max-width: 250px;">Remark</th>
                     @endfor
                 </tr>
-            @endif
-        </thead>
-        <tbody>
-            <tr>
-                @if(sizeof($dataHeaderTransactionHistory))
-                    @for($i = count($dataHeaderTransactionHistory) - 1; $i >= 0; $i--) 
-                        @if ($i === count($dataHeaderTransactionHistory) - 1)
-                            <td style="padding: 8px; width: 125px; min-width: 125px; max-width: 125px; position: sticky; background-color: white; left: 0px; z-index: 10;">{{ $dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['requesterWorkerName'] }}</td>
-                            <td style="padding: 8px; width: 125px; min-width: 125px; max-width: 125px; position: sticky; background-color: white; left: 125px; z-index: 10;">{{ $dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['beneficiaryWorkerName'] }}</td>
-                            <td style="padding: 8px; width: 125px; min-width: 125px; max-width: 125px; position: sticky; background-color: white; left: 250px; z-index: 10;">{{ $dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['requesterWorkerName'] }}</td>
-                            <td style="padding: 8px; width: 125px; min-width: 125px; max-width: 125px; position: sticky; background-color: white; left: 375px; z-index: 10;">{{ $dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['beneficiaryWorkerName'] }}</td>
-                            {{-- <td style="padding: 8px; width: 70px; min-width: 70px; max-width: 70px; position: sticky; background-color: white; left: 500px; z-index: 10;">{{ $dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['beneficiaryWorkerName'] }}</td> --}}
-                            <td style="padding: 8px; width: 250px; min-width: 250px; max-width: 250px; position: sticky; background-color: white; left: 500px; z-index: 10;">{{ $dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['content']['remarks'] }}</td>
-                        @else
+            </thead>
+            <tbody>
+                <tr>
+                    @for($i = count($dataHeaderTransactionHistory) - 1; $i >= 0; $i--)
+                        @if ($i !== count($dataHeaderTransactionHistory) - 1)
                             <td style="padding: 8px;">{{ $dataHeaderTransactionHistory[$i]['requesterWorkerName'] }}</td>
                             <td style="padding: 8px;">{{ $dataHeaderTransactionHistory[$i]['beneficiaryWorkerName'] }}</td>
                             <td style="padding: 8px;">{{ $dataHeaderTransactionHistory[$i]['requesterWorkerName'] }}</td>
@@ -56,8 +78,8 @@
                             <td style="padding: 8px;">{{ $dataHeaderTransactionHistory[$i]['content']['remarks'] }}</td>
                         @endif
                     @endfor
-                @endif
-            </tr>
-        </tbody>
-    </table>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
--- Programming/WebFrontEnd/resources/views/Components/AdvanceDetailDocumentRevision.blade.php
+++ Programming/WebFrontEnd/resources/views/Components/AdvanceDetailDocumentRevision.blade.php
@@ -1,52 +1,73 @@
-<div class="wrapper-budget table-responsive table-height">
-    <table class="table table-striped table-hover table-sticky table-sm">
-        <thead>
-            <tr>
-                @if(sizeof($dataHeaderTransactionHistory))
-                    @for($i = count($dataHeaderTransactionHistory) - 1; $i >= 0; $i--) 
-                        <?php $entryDateTime = $dataHeaderTransactionHistory[$i]['content']['sys_Data_Entry_DateTimeTZ']; $editDateTime = $dataHeaderTransactionHistory[$i]['content']['sys_Data_Edit_DateTimeTZ']; ?>
+<div class="table-budget-wrapper" style="display: flex;">
+    <!-- Kolom Fixed -->
+    <div class="table-fixed" style="flex: 0 0 auto;">
+        <table class="table table-striped table-hover table-sm">
+            <thead>
+                <tr>
+                    @if(sizeof($dataHeaderTransactionHistory))
+                        @for($i = count($dataHeaderTransactionHistory) - 1; $i >= 0; $i--) 
+                            <?php $entryDateTime = $dataHeaderTransactionHistory[$i]['content']['sys_Data_Entry_DateTimeTZ']; $editDateTime = $dataHeaderTransactionHistory[$i]['content']['sys_Data_Edit_DateTimeTZ']; ?>
 
-                        @if ($i === count($dataHeaderTransactionHistory) - 1)
-                            <th class="text-center" colspan="6" style="vertical-align: middle;left: 0px;z-index: 10;line-height: normal;position: sticky;background: #FFF;">
-                                Rev {{ $i }} (Actual) - {{ $dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['submitterWorkerName'] }} <br />( {{ date('Y-m-d', strtotime($dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['content']['sys_Data_Edit_DateTimeTZ'])) }} {{ date('H:i', strtotime($dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['content']['sys_Data_Edit_DateTimeTZ'])) }} )
-                            </th>
-                        @else
-                            <th class="text-center" colspan="3" style="background-color:#4B586A;color:white;border-right:1px solid #e9ecef;line-height: normal;vertical-align: middle;">
-                                {{ !empty($editDateTime) ? 'Rev ' . $i : 'Original' }} - {{ $dataHeaderTransactionHistory[$i]['submitterWorkerName'] }} <br /> ( {{ !empty($editDateTime) ? date('Y-m-d', strtotime($editDateTime)) . " " . date('H:i', strtotime($editDateTime)) : date('Y-m-d', strtotime($entryDateTime)) . " " . date('H:i', strtotime($entryDateTime)) }} )
-                            </th>
-                        @endif
-                    @endfor
-                @endif
-            </tr>
-            @if(sizeof($dataDetailGetTransactionHistory))
+                            @if ($i === count($dataHeaderTransactionHistory) - 1)
+                                <th class="text-center" colspan="6" style="vertical-align: middle;left: 0px;z-index: 10;line-height: normal;position: sticky;background: #FFF;">
+                                    Rev {{ $i }} (Actual) - {{ $dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['submitterWorkerName'] }} <br />( {{ date('Y-m-d', strtotime($dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['content']['sys_Data_Edit_DateTimeTZ'])) }} {{ date('H:i', strtotime($dataHeaderTransactionHistory[count($dataHeaderTransactionHistory) - 1]['content']['sys_Data_Edit_DateTimeTZ'])) }} )
+                                </th>
+                            @endif
+                        @endfor
+                    @endif
+                </tr>
                 <tr>
                     <th class="text-center" style="vertical-align: middle; width: 125px; min-width: 125px; max-width: 125px; left: 0px; z-index: 10;position: sticky;background: #FFF;">Product Code</th>
-                    <th class="text-center" style="vertical-align: middle; width: 125px; min-width: 125px; max-width: 125px; left: 125px; z-index: 10;position: sticky;background: #FFF;">Product Name</th>
-                    <th class="text-center" style="vertical-align: middle; width: 40px; min-width: 40px; max-width: 40px; left: 250px; z-index: 10;position: sticky;background: #FFF;">UOM</th>
-                    <th class="text-center" style="vertical-align: middle; width: 80px; min-width: 80px; max-width: 80px; left: 290px; z-index: 10;position: sticky;background: #FFF;">Qty</th>
-                    <th class="text-center" style="vertical-align: middle; width: 125px; min-width: 125px; max-width: 125px; left: 370px; z-index: 10;position: sticky;background: #FFF;">Price</th>
-                    <th class="text-center" style="vertical-align: middle; width: 125px; min-width: 125px; max-width: 125px; left: 495px; z-index: 10;position: sticky;background: #FFF;">Total</th>
+                    <th class="text-center" style="vertical-align: middle; width: 155px; min-width: 155px; max-width: 155px; left: 125px; z-index: 10;position: sticky;background: #FFF;">Product Name</th>
+                    <th class="text-center" style="vertical-align: middle; width: 40px; min-width: 40px; max-width: 40px; left: 280px; z-index: 10;position: sticky;background: #FFF;">UOM</th>
+                    <th class="text-center" style="vertical-align: middle; width: 80px; min-width: 80px; max-width: 80px; left: 320px; z-index: 10;position: sticky;background: #FFF;">Qty</th>
+                    <th class="text-center" style="vertical-align: middle; width: 125px; min-width: 125px; max-width: 125px; left: 400px; z-index: 10;position: sticky;background: #FFF;">Price</th>
+                    <th class="text-center" style="vertical-align: middle; width: 125px; min-width: 125px; max-width: 125px; left: 525px; z-index: 10;position: sticky;background: #FFF;">Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                @for($i = count($dataDetailGetTransactionHistory) - 1; $i >= 0; $i--)
+                    <tr>
+                        <td style="padding: 8px;">{{ $dataDetailGetTransactionHistory[$i][count($dataDetailGetTransactionHistory[$i]) - 1]['content']['product_RefID'] }}</td>
+                        <td style="padding: 8px;">{{ $dataDetailGetTransactionHistory[$i][count($dataDetailGetTransactionHistory[$i]) - 1]['productName'] }}</td>
+                        <td style="padding: 8px;">{{ $dataDetailGetTransactionHistory[$i][count($dataDetailGetTransactionHistory[$i]) - 1]['quantityUnitName'] }}</td>
+                        <td style="padding: 8px;">{{ number_format($dataDetailGetTransactionHistory[$i][count($dataDetailGetTransactionHistory[$i]) - 1]['content']['quantity'], 2) }}</td>
+                        <td style="padding: 8px;">{{ number_format($dataDetailGetTransactionHistory[$i][count($dataDetailGetTransactionHistory[$i]) - 1]['content']['productUnitPriceCurrencyValue'], 2) }}</td>
+                        <td style="padding: 8px;">{{ number_format($dataDetailGetTransactionHistory[$i][count($dataDetailGetTransactionHistory[$i]) - 1]['content']['priceCurrencyValue'], 2) }}</td>
+                    </tr>
+                @endfor
+            </tbody>
+        </table>
+    </div>
 
+    <!-- Kolom Scroll -->
+    <div class="table-scroll" style="overflow-x: auto;">
+        <table class="table table-striped table-hover table-sm">
+            <thead>
+                <tr>
+                    @if(sizeof($dataHeaderTransactionHistory))
+                        @for($i = count($dataHeaderTransactionHistory) - 1; $i >= 0; $i--) 
+                            <?php $entryDateTime = $dataHeaderTransactionHistory[$i]['content']['sys_Data_Entry_DateTimeTZ']; $editDateTime = $dataHeaderTransactionHistory[$i]['content']['sys_Data_Edit_DateTimeTZ']; ?>
+
+                            @if ($i !== count($dataHeaderTransactionHistory) - 1)
+                                <th class="text-center" colspan="3" style="background-color:#4B586A;color:white;border-right:1px solid #e9ecef;line-height: normal;vertical-align: middle; min-width: 320px;">
+                                    {{ !empty($editDateTime) ? 'Rev ' . $i : 'Original' }} - {{ $dataHeaderTransactionHistory[$i]['submitterWorkerName'] }} <br /> ( {{ !empty($editDateTime) ? date('Y-m-d', strtotime($editDateTime)) . " " . date('H:i', strtotime($editDateTime)) : date('Y-m-d', strtotime($entryDateTime)) . " " . date('H:i', strtotime($entryDateTime)) }} )
+                                </th>
+                            @endif
+                        @endfor
+                    @endif
+                </tr>
+                <tr>
                     @for($i = 1; $i < count($dataDetailGetTransactionHistory[0]); $i++)
                         <th class="text-center" style="background-color:#4B586A;color:white;border-right:1px solid #e9ecef;vertical-align: middle;">Qty</th>
                         <th class="text-center" style="background-color:#4B586A;color:white;border-right:1px solid #e9ecef;vertical-align: middle;">Price</th>
                         <th class="text-center" style="background-color:#4B586A;color:white;border-right:1px solid #e9ecef;vertical-align: middle;">Total</th>
                     @endfor
                 </tr>
-            @endif
-        </thead>
-
-        <tbody>
-            @if(sizeof($dataDetailGetTransactionHistory))
+            </thead>
+            <tbody>
                 @for($i = count($dataDetailGetTransactionHistory) - 1; $i >= 0; $i--)
                     <tr>
-                        <td style="padding: 8px; width: 125px; min-width: 125px; max-width: 125px; position: sticky; background-color: white; left: 0px; z-index: 10;">{{ $dataDetailGetTransactionHistory[$i][count($dataDetailGetTransactionHistory[$i]) - 1]['content']['product_RefID'] }}</td>
-                        <td style="padding: 8px; width: 125px; min-width: 125px; max-width: 125px; position: sticky; background-color: white; left: 125px; z-index: 10;">{{ $dataDetailGetTransactionHistory[$i][count($dataDetailGetTransactionHistory[$i]) - 1]['productName'] }}</td>
-                        <td style="padding: 8px; width: 40px; min-width: 40px; max-width: 40px; position: sticky; background-color: white; left: 250px; z-index: 10;">{{ $dataDetailGetTransactionHistory[$i][count($dataDetailGetTransactionHistory[$i]) - 1]['quantityUnitName'] }}</td>
-                        <td style="padding: 8px; width: 80px; min-width: 80px; max-width: 80px; position: sticky; background-color: white; left: 290px; z-index: 10;">{{ number_format($dataDetailGetTransactionHistory[$i][count($dataDetailGetTransactionHistory[$i]) - 1]['content']['quantity'], 2) }}</td>
-                        <td style="padding: 8px; width: 125px; min-width: 125px; max-width: 125px; position: sticky; background-color: white; left: 370px; z-index: 10;">{{ number_format($dataDetailGetTransactionHistory[$i][count($dataDetailGetTransactionHistory[$i]) - 1]['content']['productUnitPriceCurrencyValue'], 2) }}</td>
-                        <td style="padding: 8px; width: 125px; min-width: 125px; max-width: 125px; position: sticky; background-color: white; left: 495px; z-index: 10;">{{ number_format($dataDetailGetTransactionHistory[$i][count($dataDetailGetTransactionHistory[$i]) - 1]['content']['priceCurrencyValue'], 2) }}</td>
-                        
                         @for($n = count($dataDetailGetTransactionHistory[$i]) - 1; $n >= 0; $n--)
                             @if ($n !== count($dataDetailGetTransactionHistory[$i]) - 1)
                                 <td style="padding: 8px;">{{ number_format($dataDetailGetTransactionHistory[$i][$n]['content']['quantity'], 2) }}</td>
@@ -56,7 +77,7 @@
                         @endfor
                     </tr>
                 @endfor
-            @endif
-        </tbody>
-    </table>
+            </tbody>
+        </table>
+    </div>
 </div>
