62321375311f347fbfb99435d6f315d6570db6b0
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-08-20 16:27:51 +0700

    style(form-reimbursement): rename vendor-customer label

--- Programming/WebFrontEnd/resources/views/Process/Reimbursement/Functions/Header/HeaderReimbursementDetail.blade.php
+++ Programming/WebFrontEnd/resources/views/Process/Reimbursement/Functions/Header/HeaderReimbursementDetail.blade.php
@@ -1,9 +1,9 @@
 <div class="card-body">
   <div class="row py-3" style="gap: 15px;">
     <div class="col-md-12 col-lg-5">
-      <!-- VENDOR/CUSTOMER -->
+      <!-- CUSTOMER -->
       <div class="row">
-        <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">Vendor/Customer</label>
+        <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">Customer</label>
         <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0">
           <div style="width: 42%;">
             <input id="date_customer" name="date_customer" style="border-radius:0;width: 100%;" type="date" class="form-control" />
@@ -14,7 +14,7 @@
         <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0"></label>
         <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0">
           <div class="text-red">
-            Vendor/Customer cannot be empty.
+            Customer cannot be empty.
           </div>
         </div>
       </div>
