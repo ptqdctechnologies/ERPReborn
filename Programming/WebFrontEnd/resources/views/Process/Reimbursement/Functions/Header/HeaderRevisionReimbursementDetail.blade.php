c3d0d5a19ff2aa2359cd94ef7ce88d198b8185f4
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-08-20 13:48:25 +0700

    feat(revision-reimbursement): create revision reimbursement pageg

---
+++ Programming/WebFrontEnd/resources/views/Process/Reimbursement/Functions/Header/HeaderRevisionReimbursementDetail.blade.php
@@ -0,0 +1,113 @@
+<div class="card-body">
+  <div class="row py-3" style="gap: 15px;">
+    <div class="col-md-12 col-lg-5">
+      <!-- VENDOR/CUSTOMER -->
+      <div class="row">
+        <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">Vendor/Customer</label>
+        <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0">
+          <div style="width: 42%;">
+            <input id="date_customer" name="date_customer" style="border-radius:0;width: 100%;" type="date" class="form-control" value="<?= $header['dateCustomer']; ?>" />
+          </div>
+        </div>
+      </div>
+      <div class="row" id="customerMessage" style="margin-top: .3rem; display: none;">
+        <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0"></label>
+        <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0">
+          <div class="text-red">
+            Vendor/Customer cannot be empty.
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-12 col-lg-5">
+      <!-- BENEFICIARY -->
+      <div class="row">
+        <label for="beneficiary_second_person_position" class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">Beneficiary</label>
+        <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0">
+          <div>
+            <input id="beneficiary_second_person_position" style="border-radius:0;" name="beneficiary_detail" class="form-control" size="17" value="<?= $header['beneficiaryPosition']; ?>" readonly />
+            <input id="beneficiary_second_id" style="border-radius:0;" name="beneficiary_id" class="form-control" value="<?= $header['beneficiary_RefID']; ?>" hidden />
+            <input id="beneficiary_second_person_ref_id" style="border-radius:0;" name="person_refID" class="form-control" hidden />
+          </div>
+          <div>
+            <span style="border-radius:0;" class="input-group-text form-control">
+              <a href="javascript:;" id="myBeneficiarySecondTrigger" data-toggle="modal" data-target="#myBeneficiarySecond">
+                <img src="{{ asset('AdminLTE-master/dist/img/box.png') }}" width="13" alt="">
+              </a>
+            </span>
+          </div>
+          <div style="flex: 100%;">
+            <input id="beneficiary_second_person_name" style="border-radius:0;" name="beneficiary" class="form-control" readonly aria-label="Beneficiary Name" value="<?= $header['beneficiaryName']; ?>" />
+          </div>
+        </div>
+      </div>
+      <div class="row" id="beneficiaryMessage" style="margin-top: .3rem;display: none;">
+        <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0"></label>
+        <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0">
+          <div class="text-red">
+            Beneficiary cannot be empty.
+          </div>
+        </div>
+      </div>
+
+      <!-- BANK NAME -->
+      <div class="row" style="margin-top: 1rem;">
+        <label for="bank_name_second_name" class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">Bank Name</label>
+        <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0">
+          <div>
+            <input id="bank_name_second_name" style="border-radius:0;" name="bank_name" class="form-control" size="17" readonly value="<?= $header['beneficiaryBankAcronym']; ?>" />
+            <input id="bank_name_second_id" style="border-radius:0;" class="form-control" name="bank_code" value="<?= $header['beneficiaryBank_RefID']; ?>" hidden />
+          </div>
+          <div>
+            <span style="border-radius:0;" class="input-group-text form-control">
+              <a href="javascript:;" id="myGetBankSecondTrigger" data-toggle="modal" data-target="#myGetBankSecond" class="myGetBankSecond">
+                <img src="{{ asset('AdminLTE-master/dist/img/box.png') }}" width="13" alt="">
+              </a>
+            </span>
+          </div>
+          <div style="flex: 100%;">
+            <input id="bank_name_second_detail" style="border-radius:0;" class="form-control" name="bank_name_detail" readonly aria-label="Bank Name" value="<?= $header['beneficiaryBankName']; ?>" />
+          </div>
+        </div>
+      </div>
+      <div class="row" id="bankNameMessage" style="margin-top: .3rem;display: none;">
+        <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0"></label>
+        <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0">
+          <div class="text-red">
+            Bank Name cannot be empty.
+          </div>
+        </div>
+      </div>
+
+      <!-- BANK ACCOUNT -->
+      <div class="row" style="margin-top: 1rem;">
+        <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">Bank Account</label>
+        <div for="bank_accounts" class="col-sm-9 col-md-8 col-lg-7 d-flex p-0">
+          <div>
+            <input id="bank_accounts" style="border-radius:0;" name="bank_account" class="form-control number-without-characters" size="17" autocomplete="off" readonly aria-label="Bank Accounts" value="<?= $header['beneficiaryBankAccountNumber']; ?>" />
+            <input id="bank_accounts_id" style="border-radius:0;" class="form-control" name="bank_account_id" hidden value="<?= $header['beneficiaryBankAccount_RefID']; ?>" />
+          </div>
+          <div>
+            <span style="border-radius:0;" class="input-group-text form-control">
+              <a href="javascript:;" id="myBankAccountTrigger" data-toggle="modal" data-target="#myBankAccount" class="myBankAccount">
+                <img src="{{ asset('AdminLTE-master/dist/img/box.png') }}" width="13" alt="">
+              </a>
+            </span>
+          </div>
+          <div style="flex: 100%;">
+            <input id="bank_accounts_detail" style="border-radius:0;" class="form-control" name="bank_account_detail" autocomplete="off" readonly aria-label="Bank Accounts Name" value="<?= $header['beneficiaryBankAccountName']; ?>" />
+          </div>
+        </div>
+      </div>
+      <div class="row" id="bankAccountMessage" style="margin-top: .3rem;display: none;">
+        <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0"></label>
+        <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0">
+          <div class="text-red">
+            Bank Account cannot be empty.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
