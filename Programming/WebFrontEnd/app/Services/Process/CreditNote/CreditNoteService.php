09e592e486a25ab69aea754bffda5fae39753291
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-08-27 09:51:12 +0700

    feat(service-cn): create new file service credit note

---
+++ Programming/WebFrontEnd/app/Services/Process/CreditNote/CreditNoteService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Services\Process\CreditNote;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall;
+use App\Helpers\ZhtHelper\System\Helper_Environment;
+
+class CreditNoteService
+{
+    public function create(Request $request)
+    {
+        $sessionToken   = Session::get('SessionLogin');
+        $careerRefID    = Session::get('SessionWorkerCareerInternal_RefID');
+
+        $data           = $request;
+        $detailItems    = json_decode($data['creditNoteDetail'], true);
+        $fileID         = $data['dataInput_Log_FileUpload_1'] ? (int) $data['dataInput_Log_FileUpload_1'] : null;
+
+        return Helper_APICall::setCallAPIGateway(
+            Helper_Environment::getUserSessionID_System(),
+            $sessionToken,
+            'transaction.create.finance.setCreditNote',
+            'latest',
+            [
+            'entities' => [
+                "documentDateTimeTZ"            => date('Y-m-d'),
+                "customer_RefID"                => 125000000000001,
+                "log_FileUpload_Pointer_RefID"  => $fileID,
+                "remarks"                       => null,
+                "workflow_Status"               => null,
+                "additionalData"                => [
+                    "itemList"      => [
+                        "items"     => $detailItems
+                            // [
+                            // "entities" => [
+                            //     "combinedBudgetSectionDetail_RefID" => 169000000000001,
+                            //     "product_RefID" => 88000000000002,
+                            //     "quantity" => 10,
+                            //     "quantityUnit_RefID" => 73000000000001,
+                            //     "productUnitPriceCurrency_RefID" => 62000000000001,
+                            //     "productUnitPriceCurrencyValue" => 30000,
+                            //     "productUnitPriceCurrencyExchangeRate" => 1,
+                            //     "chartOfAccount_RefID" => 65000000000005
+                            //     ]
+                            // ],
+                        ]
+                    ]
+                ]
+            ]
+        );
+    }
+}
