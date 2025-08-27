0eb2be3e566e0bc2baf9209a1b94d1137a9938ef
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-08-27 17:01:04 +0700

    feat(service-cn): add getDetail, and update functions

--- Programming/WebFrontEnd/app/Services/Process/CreditNote/CreditNoteService.php
+++ Programming/WebFrontEnd/app/Services/Process/CreditNote/CreditNoteService.php
@@ -9,12 +9,35 @@ use App\Helpers\ZhtHelper\System\Helper_Environment;
 
 class CreditNoteService
 {
+    public function getDetail($creditNoteRefID)
+    {
+        $sessionToken = Session::get('SessionLogin');
+
+        return Helper_APICall::setCallAPIGateway(
+            Helper_Environment::getUserSessionID_System(),
+            $sessionToken,
+            'transaction.read.dataList.finance.getCreditNoteDetail',
+            'latest',
+            [
+            'parameter' => [
+                'creditNote_RefID' => (int) $creditNoteRefID
+                ],
+            'SQLStatement' => [
+                'pick' => null,
+                'sort' => null,
+                'filter' => null,
+                'paging' => null
+                ]
+            ]
+        );
+    }
+
     public function create(Request $request)
     {
         $sessionToken   = Session::get('SessionLogin');
         $careerRefID    = Session::get('SessionWorkerCareerInternal_RefID');
 
-        $data           = $request;
+        $data           = $request->storeData;
         $detailItems    = json_decode($data['creditNoteDetail'], true);
         $fileID         = $data['dataInput_Log_FileUpload_1'] ? (int) $data['dataInput_Log_FileUpload_1'] : null;
 
@@ -45,6 +68,50 @@ class CreditNoteService
                             //     "chartOfAccount_RefID" => 65000000000005
                             //     ]
                             // ],
+                        ]
+                    ]
+                ]
+            ]
+        );
+    }
+
+    public function update(Request $request) 
+    {
+        $sessionToken   = Session::get('SessionLogin');
+        $careerRefID    = Session::get('SessionWorkerCareerInternal_RefID');
+
+        $data           = $request->storeData;
+        $detailItems    = json_decode($data['creditNoteDetail'], true);
+        $fileID         = isset($data['dataInput_Log_FileUpload_1']) ? (int) $data['dataInput_Log_FileUpload_1'] : null;
+
+        return Helper_APICall::setCallAPIGateway(
+            Helper_Environment::getUserSessionID_System(),
+            $sessionToken,
+            'transaction.update.finance.setCreditNote',
+            'latest',
+            [
+            'recordID'  => (int) $data['creditNote_RefID'],
+            'entities'  => [
+                "documentDateTimeTZ"            => date('Y-m-d'),
+                "log_FileUpload_Pointer_RefID"  => $fileID,
+                "remarks"                       => null,
+                "workflow_Status"               => null,
+                "additionalData"                => [
+                    "itemList"  => [
+                        "items" => $detailItems
+                            // [
+                            // "recordID" => 243000000000014,
+                            // "entities" => [
+                            //     "combinedBudgetSectionDetail_RefID" => 169000000000001,
+                            //     "product_RefID" => 88000000000002,
+                            //     "quantity" => 4,
+                            //     "quantityUnit_RefID" => 73000000000001,
+                            //     "productUnitPriceCurrency_RefID" => 62000000000001,
+                            //     "productUnitPriceCurrencyValue" => 30000,
+                            //     "productUnitPriceCurrencyExchangeRate"  => 1,
+                            //     "chartOfAccount_RefID" => 65000000000005
+                            //     ]
+                            // ]
                         ]
                     ]
                 ]
