7acc38b238e9d071c25e2555f04372a086d3f112
ijonk7 <jookeo.rizal@gmail.com>
2025-09-04 14:24:33 +0700

    Update backend service 'transaction.update.supplyChain.setWarehouseInboundOrder'

--- Programming/.LaravelCore/app/Models/BackEnd/Database/SchData_OLTP_SupplyChain/TblWarehouseInboundOrder.php
+++ Programming/.LaravelCore/app/Models/BackEnd/Database/SchData_OLTP_SupplyChain/TblWarehouseInboundOrder.php
@@ -126,7 +126,7 @@ namespace App\Models\Database\SchData_OLTP_SupplyChain
         | ▪ Method Name     : setDataUpdate                                                                                        |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Version         : 1.0001.0000000                                                                                       |
-        | ▪ Last Update     : 2025-06-30                                                                                           |
+        | ▪ Last Update     : 2025-09-04                                                                                           |
         | ▪ Creation Date   : 2022-03-08                                                                                           |
         | ▪ Description     : Data Insert                                                                                          |
         +--------------------------------------------------------------------------------------------------------------------------+
@@ -153,7 +153,7 @@ namespace App\Models\Database\SchData_OLTP_SupplyChain
             $varUserSession,
             int $varSysID,
             string $varSysDataAnnotation = null, string $varSysDataValidityStartDateTimeTZ = null, string $varSysDataValidityFinishDateTimeTZ = null, int $varSysPartitionRemovableRecordKeyRefType = null, int $varSysBranch_RefID = null, $varSysBaseCurrency_RefID = null,
-            string $varDocumentDateTimeTZ = null, int $varLog_FileUpload_Pointer_RefID = null, int $varRequesterPerson_RefID = null, int $varTransporter_RefID = null, string $varDeliveryDateTimeTZ = null, int $varDeliveryFrom_RefID = null, string $varDeliveryFrom_NonRefID = null, int $varDeliveryTo_RefID = null, string $varDeliveryTo_NonRefID = null, string $varRemarks = null,
+            string $varDocumentDateTimeTZ = null, int $varLog_FileUpload_Pointer_RefID = null, int $varRequesterPerson_RefID = null, int $varTransporter_RefID = null, string $varDeliveryDateTimeTZ = null, int $varDeliveryFrom_RefID = null, string $varDeliveryFrom_NonRefID = null, int $varDeliveryTo_RefID = null, string $varDeliveryTo_NonRefID = null, string $varRemarks = null, string $varReceiveDateTimeTZ = null,
             array $varAdditionalData = []
             )
             {
@@ -184,6 +184,7 @@ namespace App\Models\Database\SchData_OLTP_SupplyChain
                             [$varDeliveryTo_RefID, 'bigint'],
                             [$varDeliveryTo_NonRefID, 'varchar'],
                             [$varRemarks, 'varchar'],
+                            [$varReceiveDateTimeTZ, 'timestamptz'],
 
                             [
                                 ((count($varAdditionalData) === 0)
