cc6db930626258538a21740a7370fe8654eade9b
Suyanto05 <suyanto@qdc.co.id>
2025-09-09 18:59:14 +0700

    fix route web

--- Programming/WebFrontEnd/routes/web.php
+++ Programming/WebFrontEnd/routes/web.php
@@ -20,7 +20,7 @@ use App\Http\Controllers\UserController;
 $varUserSession = 
     \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System();
 
-$varAPIWebToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoid2lzbnUudHJlbmdnb25vIiwiaWF0IjoxNzU2ODk2Mzk5fQ.NDBhZjExYzdmMTRmZTRiOGQ5MWQ5NDBjNmIwZGI5MjQ2N2ExOGVlNGQyZmM3NzdkYTZhMDM1MjE1YzZkOGZjYw';
+$varAPIWebToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoid2lzbnUudHJlbmdnb25vIiwiaWF0IjoxNzU2ODY4MjMwfQ.Yjg1ZmRhMzU4NWYwYmM5ZGFkOTVjMjMzY2EzMmUwMzAzYjIwOTQyNzJiZmY4YzJiZTgzYTdjNDNmZGM5ZDE3Mw';
 
 \App\Helpers\ZhtHelper\System\FrontEnd\Helper_LaravelRoute::setDynamicRoute_Examples_APICall(
     \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
