be7447208686253d7b55e8868eb6eae05902c017
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-09-29 17:59:42 +0700

    feat(purchaseOrder): adjust position button

--- Programming/WebFrontEnd/resources/views/Purchase/PurchaseOrder/Functions/PopUp/PopUpPORevision.blade.php
+++ Programming/WebFrontEnd/resources/views/Purchase/PurchaseOrder/Functions/PopUp/PopUpPORevision.blade.php
@@ -22,7 +22,6 @@
                                                 @csrf
                                                     <input id="purchaseOrder_RefID" style="border-radius:0;" name="purchaseOrder_RefID" type="hidden" class="form-control">
                                                 </form>
-                                                <input required="" id="purchaseOrder_number" style="border-radius:0;" name="purchaseOrder_number" type="text" class="form-control" required readonly>
                                                 <div class="input-group-append" style="cursor: pointer;">
                                                     <span style="border-radius:0;" class="input-group-text form-control" id="purchaseOrder_number_icon">
                                                         <a data-toggle="modal" data-target="#mySearchPO">
@@ -30,6 +29,7 @@
                                                         </a>
                                                     </span>
                                                 </div>
+                                                <input required="" id="purchaseOrder_number" style="border-radius:0;" name="purchaseOrder_number" type="text" class="form-control" required readonly>
                                             </div>
                                         </td>                                            
                                     </tr>
@@ -37,11 +37,11 @@
                             </div>
                         </div>
                     </div>
-                    <a class="btn btn-sm btn-edit" style="margin-left: 38%;background-color:#e9ecef;border:1px solid #ced4da;">
-                        <img src="{{ asset('AdminLTE-master/dist/img/edit.png') }}" width="13" alt="" title="Edit"> Edit
-                    </a>
-                    <a class="btn btn-sm btn-cancel" style="background-color:#e9ecef;border:1px solid #ced4da;">
+                    <a class="btn btn-sm btn-cancel" style="margin-left: 38%;background-color:#e9ecef;border:1px solid #ced4da;">
                         <img src="{{ asset('AdminLTE-master/dist/img/cancel.png') }}" width="13" alt="" title="Cancel"> Cancel
+                    </a>
+                    <a class="btn btn-sm btn-edit" style="background-color:#e9ecef;border:1px solid #ced4da;">
+                        <img src="{{ asset('AdminLTE-master/dist/img/edit.png') }}" width="13" alt="" title="Edit"> Edit
                     </a>
                 </div>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
---
+++ Programming/WebFrontEnd/resources/views/Purchase/PurchaseOrder/Functions/PopUp/PopUpPurchaseOrderSummaryData.blade.php
@@ -0,0 +1,39 @@
+<div class="modal fade" id="purchaseOrderFormModal" tabindex="-1" role="dialog">
+  <div class="modal-dialog modal-lg" role="document" style="height: calc(100vh - 3.5rem); display: flex; align-items: center;">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h3 style="margin: 0px;font-weight:bold;">Are you sure you want to save this data?</h3>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      </div>
+      <div class="modal-body">
+        <div class="wrapper-budget table-responsive card-body p-0" style="max-height: 200px;">
+          <table class="table text-nowrap table-sm" id="tablePurchaseOrderList" style="border: 1px solid #dee2e6;">
+            <tbody></tbody>
+          </table>
+        </div>
+        <div class="card-body">
+          <table style="float:right;">
+            <tr>
+              <th id="GrandTotal"></th>
+            </tr>
+            <tr>
+                <th id="GrandVAT"></th>
+              </tr>
+              <tr>
+                <th id="GrandTotalVAT"></th>
+              </tr>
+          </table>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default btn-sm" data-dismiss="modal" style="background-color:#e9ecef;border:1px solid #ced4da;">
+          <img src="{{ asset('AdminLTE-master/dist/img/cancel.png') }}" width="13" alt="" title="Cancel Advance List Cart"> No, cancel
+        </button>
+
+        <button type="button" id="submitPurchaseOrder" class="btn btn-default btn-sm" onclick="SubmitForm();" style="margin-right: 5px;background-color:#e9ecef;border:1px solid #ced4da;">
+          <img src="{{ asset('AdminLTE-master/dist/img/save.png') }}" width="13" alt="" title="Submit to Advance"> Yes, save it
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
