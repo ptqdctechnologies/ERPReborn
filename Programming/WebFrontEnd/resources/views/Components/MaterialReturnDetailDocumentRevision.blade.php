2121e4e7b69824d36b2dc0b1a68e498595b7769b
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-09-30 13:45:29 +0700

    feat(material-return): add file revision document tracking

---
+++ Programming/WebFrontEnd/resources/views/Components/MaterialReturnDetailDocumentRevision.blade.php
@@ -0,0 +1,55 @@
+<div class="table-budget-wrapper" style="display: flex;">
+    <!-- Kolom Fixed -->
+    <div class="table-fixed" style="flex: 0 0 auto;">
+        <table class="table table-striped table-hover table-sm">
+            <thead>
+                <tr>
+                    <th class="text-center" colspan="7" style="vertical-align: middle;left: 0px;z-index: 10;line-height: normal;position: sticky;background: #FFF;">
+                        Rev 1 (Actual) - <br /> ( 2025-09-30 10:31 )
+                    </th>
+                </tr>
+                <tr>
+                    <th class="text-center" style="vertical-align: middle; width: 125px; min-width: 125px; max-width: 125px; left: 0px; z-index: 10;position: sticky; background-color: white;">Sub Budget Code</th>
+                    <th class="text-center" style="vertical-align: middle; width: 125px; min-width: 125px; max-width: 125px; left: 125px; z-index: 10;position: sticky; background-color: white;">Product Code</th>
+                    <th class="text-center" style="vertical-align: middle; width: 125px; min-width: 125px; max-width: 125px; left: 250px; z-index: 10;position: sticky; background-color: white;">Product Name</th>
+                    <th class="text-center" style="vertical-align: middle; width: 70px; min-width: 70px; max-width: 70px; left: 375px; z-index: 10;position: sticky; background-color: white;">UOM</th>
+                    <th class="text-center" style="vertical-align: middle; width: 80px; min-width: 80px; max-width: 80px; left: 445px; z-index: 10;position: sticky; background-color: white;">Qty</th>
+                    <th class="text-center" style="vertical-align: middle; width: 125px; min-width: 125px; max-width: 125px; left: 525px; z-index: 10;position: sticky; background-color: white;">Note</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="padding: 8px; width: 125px; min-width: 125px; max-width: 125px; position: sticky; background-color: white; left: 0px; z-index: 10;">240 - Cendana Andalas</td>
+                    <td style="padding: 8px; width: 125px; min-width: 125px; max-width: 125px; position: sticky; background-color: white; left: 125px; z-index: 10;">2000219</td>
+                    <td style="padding: 8px; width: 125px; min-width: 125px; max-width: 125px; position: sticky; background-color: white; left: 250px; z-index: 10;">Site Preparation</td>
+                    <td style="padding: 8px; width: 70px; min-width: 70px; max-width: 70px; position: sticky; background-color: white; left: 375px; z-index: 10;">m2</td>
+                    <td style="padding: 8px; width: 80px; min-width: 80px; max-width: 80px; position: sticky; background-color: white; left: 445px; z-index: 10;">4.00</td>
+                    <td style="padding: 8px; width: 125px; min-width: 125px; max-width: 125px; position: sticky; background-color: white; left: 525px; z-index: 10;">-</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <!-- Kolom Scroll -->
+    <div class="table-scroll" style="overflow-x: auto; width: 100%;">
+        <table class="table table-striped table-hover table-sm">
+            <thead>
+                <tr>
+                    <th class="text-center" colspan="3" style="background-color:#4B586A;color:white;border-right:1px solid #e9ecef;line-height: normal;vertical-align: middle; min-width: 320px;">
+                        Original - <br /> ( 2025-09-29 10:31 )
+                    </th>
+                </tr>
+                <tr>
+                    <th class="text-center" style="background-color:#4B586A;color:white;border-right:1px solid #e9ecef;vertical-align: middle;width: 80px; min-width: 80px; max-width: 80px;"> Qty</th>
+                    <th class="text-center" style="background-color:#4B586A;color:white;border-right:1px solid #e9ecef;vertical-align: middle;width: 125px; min-width: 125px; max-width: 125px;"> Note</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="padding: 8px;width: 80px; min-width: 80px; max-width: 80px;">6.00</td>
+                    <td style="padding: 8px;width: 125px; min-width: 125px; max-width: 125px;">2 Barang Rusak</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
