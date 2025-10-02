229713704f45363d813b45cb87298a9d606e98b1
ijonk7 <jookeo.rizal@gmail.com>
2025-10-02 16:47:03 +0700

    Create backend service 'transaction.update.supplyChain.setWarehouseOutboundOrder'

--- Programming/.LaravelCore/app/Http/Controllers/Application/BackEnd/System/Transaction/Engines/update/supplyChain/setWarehouseOutboundOrder/v1/setWarehouseOutboundOrder.php
+++ Programming/.LaravelCore/app/Http/Controllers/Application/BackEnd/System/Transaction/Engines/update/supplyChain/setWarehouseOutboundOrder/v1/setWarehouseOutboundOrder.php
@@ -44,7 +44,7 @@ namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\up
         | ▪ Method Name     : main                                                                                                 |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Version         : 1.0001.0000000                                                                                       |
-        | ▪ Last Update     : 2025-02-11                                                                                           |
+        | ▪ Last Update     : 2025-10-02                                                                                           |
         | ▪ Creation Date   : 2022-03-08                                                                                           |
         | ▪ Description     : Fungsi Utama Engine                                                                                  |
         +--------------------------------------------------------------------------------------------------------------------------+
@@ -96,7 +96,23 @@ namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\up
                                     $varData['entities']['documentDateTimeTZ'],
                                     $varData['entities']['log_FileUpload_Pointer_RefID'],
                                     $varData['entities']['requesterWorkerJobsPosition_RefID'],
-                                    $varData['entities']['remarks']
+                                    $varData['entities']['transporter_RefID'],
+                                    $varData['entities']['remarks'],
+
+                                    (\App\Helpers\ZhtHelper\General\Helper_Array::isKeyExist(
+                                        $varUserSession,
+                                        'additionalData',
+                                        $varData['entities']
+                                        )
+                                        ?   (
+                                                (
+                                                !is_null($varData['entities']['additionalData'])
+                                                )
+                                                ? $varData['entities']['additionalData']
+                                                : []
+                                            )
+                                        : []
+                                    )
                                     )
                                 )
                             ))
