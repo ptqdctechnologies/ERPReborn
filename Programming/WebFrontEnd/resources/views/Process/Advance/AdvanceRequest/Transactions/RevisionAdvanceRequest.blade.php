b5d0e66b8169601099e088fb6d5f5e2a5b6b6cb6
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-10-03 14:50:35 +0700

    feat(revision-advance): adjust table components

--- Programming/WebFrontEnd/resources/views/Process/Advance/AdvanceRequest/Transactions/RevisionAdvanceRequest.blade.php
+++ Programming/WebFrontEnd/resources/views/Process/Advance/AdvanceRequest/Transactions/RevisionAdvanceRequest.blade.php
@@ -2,13 +2,7 @@
 @section('main')
 @include('Partials.navbar')
 @include('Partials.sidebar')
-@include('getFunction.getProject')
 @include('getFunction.getAdvance')
-@include('getFunction.getSite')
-@include('getFunction.getWorker')
-@include('getFunction.getBeneficiary')
-@include('getFunction.getBank')
-@include('getFunction.getBankAccount')
 @include('getFunction.getProduct')
 @include('getFunction.getWorkFlow')
 @include('Process.Advance.AdvanceRequest.Functions.PopUp.PopUpAdvanceRevision')
@@ -149,49 +143,7 @@
                     </div>
                   </div>
 
-                  <!-- BODY -->
-                  <div class="wrapper-budget card-body table-responsive p-0" style="height: 230px;">
-                    <table class="table table-head-fixed text-nowrap table-sm" id="tableGetBudgetDetails">
-                      <thead>
-                        <tr>
-                          <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">Product Code</th>
-                          <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">Product Name</th>
-                          <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">Qty Budget</th>
-                          <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">Qty Avail</th>
-                          <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">UOM</th>
-                          <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">Price</th>
-                          <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">Total Budget</th>
-                          <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">Currency</th>
-                          <th class="sticky-col forth-col-arf" style="padding-top: 10px;padding-bottom: 10px;text-align: center;background-color:#4B586A;color:white;">Qty Req</th>
-                          <th class="sticky-col third-col-arf" style="padding-top: 10px;padding-bottom: 10px;text-align: center;background-color:#4B586A;color:white;">Price Req</th>
-                          <th class="sticky-col second-col-arf" style="padding-top: 10px;padding-bottom: 10px;text-align: center;background-color:#4B586A;color:white;">Total Req</th>
-                          <th class="sticky-col first-col-arf" style="padding-top: 10px;padding-bottom: 10px;text-align: center;background-color:#4B586A;color:white;">Balance Qty</th>
-                        </tr>
-                      </thead>
-                      <tbody></tbody>
-                      <tfoot>
-                        <tr class="loadingBudgetDetails">
-                          <td colspan="11" class="p-0" style="border: 0px; height: 150px;">
-                            <div class="d-flex flex-column justify-content-center align-items-center py-3">
-                              <div class="spinner-border" role="status">
-                                <span class="sr-only">Loading...</span>
-                              </div>
-                              <div class="mt-3" style="font-size: 0.75rem; font-weight: 700;">
-                                Loading...
-                              </div>
-                            </div>
-                          </td>
-                        </tr>
-                        <tr class="errorMessageContainerBudgetDetails">
-                          <td colspan="11" class="p-0" style="border: 0px;">
-                            <div class="d-flex flex-column justify-content-center align-items-center py-3">
-                              <div id="errorMessageBudgetDetails" class="mt-3 text-red" style="font-size: 1rem; font-weight: 700;"></div>
-                            </div>
-                          </td>
-                        </tr>
-                      </tfoot>
-                    </table>
-                  </div>
+                  @include('Process.Advance.AdvanceRequest.Functions.Table.TableAdvanceRevisions')
 
                   <!-- FOOTER -->
                   <div class="card-body tableShowHideBudget">
