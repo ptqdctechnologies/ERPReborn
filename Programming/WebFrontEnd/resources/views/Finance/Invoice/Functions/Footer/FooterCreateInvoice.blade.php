90d846c167514cff7635af60dac7bb35d94f02d0
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-10-06 10:38:00 +0700

    feat(invoice): adjust dummy data, width column

--- Programming/WebFrontEnd/resources/views/Finance/Invoice/Functions/Footer/FooterCreateInvoice.blade.php
+++ Programming/WebFrontEnd/resources/views/Finance/Invoice/Functions/Footer/FooterCreateInvoice.blade.php
@@ -33,7 +33,8 @@
                 customerCode: 'PLN1',
                 customerName: 'PLN UIP Jawa',
                 customerOrder: 1000000000.00,
-                balance: 1000000000.00
+                balance: 1000000000.00,
+                progressCompleted: 85
             },
             {
                 id: 1,
@@ -42,8 +43,99 @@
                 customerCode: 'PLN2',
                 customerName: 'PLN UIP Medan',
                 customerOrder: 500000.00,
-                balance: 500000.00
+                balance: 500000.00,
+                progressCompleted: 30
             },
+            {
+                id: 2,
+                subBudgetCode: '248',
+                subBudgetName: 'Sudirman Raya',
+                customerCode: 'PLN3',
+                customerName: 'PLN UIP Jakarta',
+                customerOrder: 250000000.00,
+                balance: 100000000.00,
+                progressCompleted: 60
+            },
+            {
+                id: 3,
+                subBudgetCode: '221',
+                subBudgetName: 'Gatot Subroto Line',
+                customerCode: 'PLN4',
+                customerName: 'PLN UIP Bandung',
+                customerOrder: 800000000.00,
+                balance: 400000000.00,
+                progressCompleted: 50
+            },
+            {
+                id: 4,
+                subBudgetCode: '254',
+                subBudgetName: 'Merdeka Selatan Grid',
+                customerCode: 'PLN5',
+                customerName: 'PLN UIP Surabaya',
+                customerOrder: 1200000000.00,
+                balance: 900000000.00,
+                progressCompleted: 25
+            },
+            {
+                id: 5,
+                subBudgetCode: '249',
+                subBudgetName: 'Panjang Timur Project',
+                customerCode: 'PLN6',
+                customerName: 'PLN UIP Lampung',
+                customerOrder: 450000000.00,
+                balance: 100000000.00,
+                progressCompleted: 78
+            },
+            {
+                id: 6,
+                subBudgetCode: '219',
+                subBudgetName: 'Barito Hulu Development',
+                customerCode: 'PLN7',
+                customerName: 'PLN UIP Kalimantan',
+                customerOrder: 950000000.00,
+                balance: 500000000.00,
+                progressCompleted: 47
+            },
+            {
+                id: 7,
+                subBudgetCode: '235',
+                subBudgetName: 'Rinjani Expansion',
+                customerCode: 'PLN8',
+                customerName: 'PLN UIP NTB',
+                customerOrder: 300000000.00,
+                balance: 200000000.00,
+                progressCompleted: 66
+            },
+            {
+                id: 8,
+                subBudgetCode: '217',
+                subBudgetName: 'Papua Tengah Electrification',
+                customerCode: 'PLN9',
+                customerName: 'PLN UIP Papua',
+                customerOrder: 700000000.00,
+                balance: 700000000.00,
+                progressCompleted: 10
+            },
+            {
+                id: 9,
+                subBudgetCode: '222',
+                subBudgetName: 'Nusantara Grid Phase 1',
+                customerCode: 'PLN10',
+                customerName: 'PLN UIP Nusantara',
+                customerOrder: 1500000000.00,
+                balance: 1000000000.00,
+                progressCompleted: 35
+            },
+            {
+                id: 10,
+                subBudgetCode: '256',
+                subBudgetName: 'Batam Island Upgrade',
+                customerCode: 'PLN11',
+                customerName: 'PLN UIP Batam',
+                customerOrder: 600000000.00,
+                balance: 300000000.00,
+                progressCompleted: 72
+            }
         ];
 
         let invoiceDetailsTable = $('#invoice_details_table tbody');
@@ -51,8 +143,9 @@
         $.each(data, function(key, val) {
             let row = `
                 <tr>
-                    <td style="text-align: center;">${val.subBudgetCode} - ${val.subBudgetName}</td>
+                    <td style="text-align: left;">${val.subBudgetCode} - ${val.subBudgetName}</td>
                     <td style="text-align: center;">${decimalFormat(parseFloat(val.customerOrder))}</td>
+                    <td style="text-align: center;">${val.progressCompleted}</td>
                     <td style="text-align: center;">${decimalFormat(parseFloat(val.balance))}</td>
                     <td style="text-align: center; width: 125px;">
                         <input class="form-control number-without-negative" id="invoice_value${key}" autocomplete="off" style="border-radius:0px;" />
