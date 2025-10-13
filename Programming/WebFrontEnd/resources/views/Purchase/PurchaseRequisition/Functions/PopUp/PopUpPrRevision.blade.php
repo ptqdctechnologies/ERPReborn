3ad4d744cb7f12389530ea7d52110b393c85def5
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-10-13 16:05:20 +0700

    style(popup-pr-revision): remove unused code

--- Programming/WebFrontEnd/resources/views/Purchase/PurchaseRequisition/Functions/PopUp/PopUpPrRevision.blade.php
+++ Programming/WebFrontEnd/resources/views/Purchase/PurchaseRequisition/Functions/PopUp/PopUpPrRevision.blade.php
@@ -4,42 +4,41 @@
             <div class="modal-header">
                 <div class="modal-body">
                     <span style="font-size: 15px;position:relative;left:17%;font-weight:bold;">PURCHASE REQUEST REVISION</span><br><br><br>
-                        <div class="card" style="margin-left: 8%;">
-                            <div class="card-body">
-                                <div class="form-group">
-                                    <table>
-                                        <tr>
-                                            <td style="padding-top: 20px;"><label>Revision Number &nbsp;&nbsp;&nbsp;&nbsp;</label></td>
-                                            <td>
-                                                <div class="input-group">
-                                                    <form id="edit_form" action="{{ route('PurchaseRequisition.RevisionPurchaseRequest') }}" method="post">
-                                                        @csrf
-                                                        <input id="modal_purchase_requisition_id" style="border-radius:0;" name="modal_purchase_requisition_id" type="hidden" class="form-control">
-                                                    </form>
+                    <div class="card" style="margin-left: 8%;">
+                        <div class="card-body">
+                            <div class="form-group">
+                                <table>
+                                    <tr>
+                                        <td style="padding-top: 20px;"><label>Revision Number &nbsp;&nbsp;&nbsp;&nbsp;</label></td>
+                                        <td>
+                                            <div class="input-group">
+                                                <form id="edit_form" action="{{ route('PurchaseRequisition.RevisionPurchaseRequest') }}" method="post">
+                                                    @csrf
+                                                    <input id="modal_purchase_requisition_id" style="border-radius:0;" name="modal_purchase_requisition_id" type="hidden" class="form-control">
+                                                </form>
 
-                                                    <div class="input-group-append">
-                                                        <span id="modal_purchase_requisition_document_number_icon" style="border-radius:0;cursor:pointer;" class="input-group-text form-control">
-                                                            <a data-toggle="modal" data-target="#purchaseRequisitionModal">
-                                                                <img src="{{ asset('AdminLTE-master/dist/img/box.png') }}" width="13" alt="" />
-                                                            </a>
-                                                        </span>
-                                                    </div>
-
-                                                    <input id="modal_purchase_requisition_document_number" style="border-radius:0;" type="text" class="form-control" readonly>
+                                                <div class="input-group-append">
+                                                    <span id="modal_purchase_requisition_document_number_icon" style="border-radius:0;cursor:pointer;" class="input-group-text form-control">
+                                                        <a data-toggle="modal" data-target="#purchaseRequisitionModal">
+                                                            <img src="{{ asset('AdminLTE-master/dist/img/box.png') }}" width="13" alt="" />
+                                                        </a>
+                                                    </span>
                                                 </div>
-                                            </td>
-                                        </tr>
-                                    </table>
-                                </div>
+
+                                                <input id="modal_purchase_requisition_document_number" style="border-radius:0;" type="text" class="form-control" readonly>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </table>
                             </div>
                         </div>
-                        <a id="cancel_button" class="btn btn-sm" data-dismiss="modal" style="margin-left: 38%;background-color:#e9ecef;border:1px solid #ced4da;">
-                            <img src="{{ asset('AdminLTE-master/dist/img/cancel.png') }}" width="13" alt="" title="Cancel"> Cancel
-                        </a>
-                        <a id="edit_button" class="btn btn-sm" style="background-color:#e9ecef;border:1px solid #ced4da;">
-                            <img src="{{ asset('AdminLTE-master/dist/img/edit.png') }}" width="13" alt="" title="Edit"> Edit
-                        </a>
-                    </span>
+                    </div>
+                    <a id="cancel_button" class="btn btn-sm" data-dismiss="modal" style="margin-left: 38%;background-color:#e9ecef;border:1px solid #ced4da;">
+                        <img src="{{ asset('AdminLTE-master/dist/img/cancel.png') }}" width="13" alt="" title="Cancel"> Cancel
+                    </a>
+                    <a id="edit_button" class="btn btn-sm" style="background-color:#e9ecef;border:1px solid #ced4da;">
+                        <img src="{{ asset('AdminLTE-master/dist/img/edit.png') }}" width="13" alt="" title="Edit"> Edit
+                    </a>
                 </div>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
