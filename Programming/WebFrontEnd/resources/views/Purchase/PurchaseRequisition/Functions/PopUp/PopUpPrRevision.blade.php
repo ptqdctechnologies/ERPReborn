baf6d54bd34e581128f3faed8404fe15db8e1d8b
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-10-13 15:54:53 +0700

    feat(popup-pr-revision): adjust button edit and cancel function

--- Programming/WebFrontEnd/resources/views/Purchase/PurchaseRequisition/Functions/PopUp/PopUpPrRevision.blade.php
+++ Programming/WebFrontEnd/resources/views/Purchase/PurchaseRequisition/Functions/PopUp/PopUpPrRevision.blade.php
@@ -4,8 +4,6 @@
             <div class="modal-header">
                 <div class="modal-body">
                     <span style="font-size: 15px;position:relative;left:17%;font-weight:bold;">PURCHASE REQUEST REVISION</span><br><br><br>
-                    <form action="{{ route('PurchaseRequisition.RevisionPurchaseRequest') }}" method="post">
-                        @csrf
                         <div class="card" style="margin-left: 8%;">
                             <div class="card-body">
                                 <div class="form-group">
@@ -14,14 +12,19 @@
                                             <td style="padding-top: 20px;"><label>Revision Number &nbsp;&nbsp;&nbsp;&nbsp;</label></td>
                                             <td>
                                                 <div class="input-group">
+                                                    <form id="edit_form" action="{{ route('PurchaseRequisition.RevisionPurchaseRequest') }}" method="post">
+                                                        @csrf
+                                                        <input id="modal_purchase_requisition_id" style="border-radius:0;" name="modal_purchase_requisition_id" type="hidden" class="form-control">
+                                                    </form>
+
                                                     <div class="input-group-append">
-                                                        <span style="border-radius:0;cursor:pointer;" class="input-group-text form-control">
+                                                        <span id="modal_purchase_requisition_document_number_icon" style="border-radius:0;cursor:pointer;" class="input-group-text form-control">
                                                             <a data-toggle="modal" data-target="#purchaseRequisitionModal">
                                                                 <img src="{{ asset('AdminLTE-master/dist/img/box.png') }}" width="13" alt="" />
                                                             </a>
                                                         </span>
                                                     </div>
-                                                    <input id="modal_purchase_requisition_id" style="border-radius:0;" name="modal_purchase_requisition_id" type="hidden" class="form-control">
+
                                                     <input id="modal_purchase_requisition_document_number" style="border-radius:0;" type="text" class="form-control" readonly>
                                                 </div>
                                             </td>
@@ -30,16 +33,37 @@
                                 </div>
                             </div>
                         </div>
-                        <button type="reset" class="btn btn-sm" style="margin-left: 38%;background-color:#e9ecef;border:1px solid #ced4da;">
-                            <img src="{{ asset('AdminLTE-master/dist/img/cancel.png') }}" width="13" alt="" title="Edit"> Cancel
-                        </button>
-                        <button type="submit" class="btn btn-sm" style="background-color:#e9ecef;border:1px solid #ced4da;">
+                        <a id="cancel_button" class="btn btn-sm" data-dismiss="modal" style="margin-left: 38%;background-color:#e9ecef;border:1px solid #ced4da;">
+                            <img src="{{ asset('AdminLTE-master/dist/img/cancel.png') }}" width="13" alt="" title="Cancel"> Cancel
+                        </a>
+                        <a id="edit_button" class="btn btn-sm" style="background-color:#e9ecef;border:1px solid #ced4da;">
                             <img src="{{ asset('AdminLTE-master/dist/img/edit.png') }}" width="13" alt="" title="Edit"> Edit
-                        </button>
-                    </form>
+                        </a>
+                    </span>
                 </div>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    $('#edit_button').on('click', function() {
+        let purchaseRequest_RefID = $('#modal_purchase_requisition_id').val();
+
+        if (purchaseRequest_RefID) {
+            ShowLoading();
+
+            $('#edit_form').submit();
+        } else {
+            $('#modal_purchase_requisition_document_number').focus();
+            $('#modal_purchase_requisition_document_number').css("border", "1px solid red");
+            $('#modal_purchase_requisition_document_number_icon').css("border", "1px solid red");
+        }
+    });
+
+    $('#cancel_button').on('click', function() {
+        $('#modal_purchase_requisition_id').val("");
+        $('#modal_purchase_requisition_document_number').val("");
+    });
+</script>
