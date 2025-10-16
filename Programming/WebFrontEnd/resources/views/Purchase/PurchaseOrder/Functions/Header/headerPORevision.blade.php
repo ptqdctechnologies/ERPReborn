0375b81c210014e1321eae088f8d0fe83d319f93
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-10-16 14:43:10 +0700

    feat(revision-purchase-order): merge kolom

--- Programming/WebFrontEnd/resources/views/Purchase/PurchaseOrder/Functions/Header/headerPORevision.blade.php
+++ Programming/WebFrontEnd/resources/views/Purchase/PurchaseOrder/Functions/Header/headerPORevision.blade.php
@@ -1,24 +1,15 @@
 <div class="card-body">
-    <div class="row py-3" style="gap: 15px;">
+    <div class="row py-3" style="gap: 1rem;">
         <!-- LEFT -->
         <div class="col-md-12 col-lg-5">
             <!-- SUPPLIER -->
             <div class="row">
                 <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">Supplier</label>
-                <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0">
-                    <div>
-                        <span style="border-radius:0;" class="input-group-text form-control">
-                            <a href="javascript:;" id="supplier_code2" data-toggle="modal" data-target="#mySupplier" class="mySupplier">
-                                <img src="{{ asset('AdminLTE-master/dist/img/box.png') }}" width="13" alt="" />
-                            </a>
-                        </span>
-                    </div>
-                    <div>
-                        <input id="supplier_id" name="supplier_id" style="border-radius:0;" type="hidden" class="form-control" readonly value="<?= $header['supplierID']; ?>">
-                        <input id="supplier_code" style="border-radius:0;" class="form-control" readonly value="<?= $header['supplierCode'] . ' - ' . $header['supplierName']; ?>">
-                    </div>
+                <div class="col-5 d-flex">
                     <div style="flex: 100%;">
-                        <input id="supplier_name" style="border-radius:0;" class="form-control" name="projectname" readonly value="<?= $header['supplierAddress']; ?>">
+                        <input id="supplier_id" name="supplier_id" style="border-radius:0;" type="hidden" class="form-control" readonly value="<?= $header['supplierID']; ?>">
+                        <input id="supplier_code" style="border-radius:0;" class="form-control" readonly value="<?= "(". $header['supplierCode'] . ") " . $header['supplierName'] . ' - ' . $header['supplierAddress']; ?>">
+                        <input id="supplier_name" style="border-radius:0;" class="form-control" name="projectname" type="hidden" value="<?= $header['supplierAddress']; ?>">
                     </div>
                 </div>
             </div>
@@ -26,9 +17,9 @@
             <!-- DP -->
             <div class="row" style="margin-top: 1rem;">
                 <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">DP</label>
-                <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0">
+                <div class="col-5 d-flex">
                     <div id="dp_section">
-                        <input type="number" id="downPaymentValue" name="downPaymentValue" max="100" style="width: 25%;" value="<?= $header['downPayment']; ?>" /><strong>%</strong>
+                        <input type="number" id="downPaymentValue" name="downPaymentValue" max="100" style="width: 25%;" autocomplete="off" value="<?= $header['downPayment']; ?>" /><strong>%</strong>
                     </div>
                 </div>
             </div>
@@ -44,7 +35,7 @@
             <!-- TOP -->
             <div class="row" style="margin-top: 1rem;">
                 <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">TOP</label>
-                <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0">
+                <div class="col-5 d-flex">
                     <div id="containerLoadingTOP">
                         <div class="d-flex flex-column justify-content-center">
                             <div class="spinner-border spinner-border-sm" role="status">
@@ -65,8 +56,8 @@
             <!-- PAYMENT NOTES -->
             <div class="row" style="margin-top: 1rem;">
                 <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">Payment Notes</label>
-                <div class="col-sm-9 col-md-8 col-lg-7 bg-red d-flex p-0">
-                    <input id="paymentNotes" name="paymentNotes" style="border-radius:0;" type="text" class="form-control" value="<?= $header['paymentNotes']; ?>">
+                <div class="col-5 d-flex">
+                    <input id="paymentNotes" name="paymentNotes" style="border-radius:0;" type="text" class="form-control" value="<?= $header['paymentNotes']; ?>" autocomplete="off">
                 </div>
             </div>
         </div>
@@ -74,18 +65,18 @@
         <!-- RIGHT -->
         <div class="col-md-12 col-lg-5">
             <!-- REMARK PO -->
-            <div class="row" style="margin-bottom: 1rem;">
+            <div class="row">
                 <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">Remark PO</label>
-                <div class="col-sm-9 col-md-8 col-lg-6 d-flex p-0">
-                    <input id="remarkPO" name="remarkPO" style="border-radius:0;" type="text" class="form-control" value="<?= $header['remarkPO']; ?>">
+                <div class="col-5 d-flex">
+                    <input id="remarkPO" name="remarkPO" style="border-radius:0;" type="text" class="form-control" value="<?= $header['remarkPO']; ?>" autocomplete="off">
                 </div>
             </div>
-
+            
             <!-- INTERNAL NOTE -->
-            <div class="row">
+            <div class="row" style="margin-top: 1rem;">
                 <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">Internal Note</label>
-                <div class="col-sm-9 col-md-8 col-lg-6 d-flex p-0">
-                    <textarea name="internalNote" id="internalNote" cols="30" rows="4" class="form-control"><?= $header['internalNote']; ?></textarea>
+                <div class="col-5">
+                    <textarea name="internalNote" id="internalNote" cols="30" rows="4" class="form-control" autocomplete="off"><?= $header['internalNote']; ?></textarea>
                 </div>
             </div>
         </div>
