65065adaeea52449272dbce0bfbf3f685d536513
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-10-17 15:33:36 +0700

    feat(function): create getWarehouses function

--- Programming/WebFrontEnd/resources/views/getFunction/getWarehouses.blade.php
+++ Programming/WebFrontEnd/resources/views/getFunction/getWarehouses.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-dialog-scrollable" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title text-bold">Choose Warehouse Number</h4>
+                <h4 class="modal-title text-bold">Choose Warehouse</h4>
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
             <div class="modal-body">
