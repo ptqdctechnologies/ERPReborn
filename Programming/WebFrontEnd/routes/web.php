f13e9fdcf568061d1ed9af608a65a9db88ee76cd
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-10-24 11:10:07 +0700

    feat(routes): add update revision account payable url

--- Programming/WebFrontEnd/routes/web.php
+++ Programming/WebFrontEnd/routes/web.php
@@ -219,6 +219,7 @@ Route::group(['middleware' => ['prevent-back-history', 'SessionLogin']], functio
     // AP
     Route::get('DataPickList', 'Finance\AccountPayableController@DataPickList')->name('AccountPayable.DataPickList');
     Route::post('RevisionAccountPayable', 'Finance\AccountPayableController@RevisionAccountPayable')->name('AccountPayable.RevisionAccountPayable');
+    Route::post('UpdatesRevisionAccountPayable', 'Finance\AccountPayableController@UpdatesRevisionAccountPayable')->name('AccountPayable.UpdatesRevisionAccountPayable');
     Route::get('ReportAccountPayableSummary', 'Finance\AccountPayableController@ReportAccountPayableSummary')->name('AccountPayable.ReportAccountPayableSummary');
     Route::post('ReportAccountPayableSummaryStore', 'Finance\AccountPayableController@ReportAccountPayableSummaryStore')->name('AccountPayable.ReportAccountPayableSummaryStore');
     Route::post('PrintExportReportAccountPayableSummary', 'Finance\AccountPayableController@PrintExportReportAccountPayableSummary')->name('AccountPayable.PrintExportReportAccountPayableSummary');
