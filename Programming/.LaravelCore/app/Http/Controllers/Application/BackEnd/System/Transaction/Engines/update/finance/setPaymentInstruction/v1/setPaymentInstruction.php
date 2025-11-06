f41610666852fb27d0ed63add9c21bb3c1b3b372
ijonk7 <jookeo.rizal@gmail.com>
2025-11-06 15:43:47 +0700

    Update backend service 'transaction.update.finance.setPaymentInstruction'

--- Programming/.LaravelCore/app/Http/Controllers/Application/BackEnd/System/Transaction/Engines/update/finance/setPaymentInstruction/v1/setPaymentInstruction.php
+++ Programming/.LaravelCore/app/Http/Controllers/Application/BackEnd/System/Transaction/Engines/update/finance/setPaymentInstruction/v1/setPaymentInstruction.php
@@ -43,7 +43,7 @@ namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\up
         | ▪ Method Name     : main                                                                                                 |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Version         : 1.0000.0000000                                                                                       |
-        | ▪ Last Update     : 2025-11-05                                                                                           |
+        | ▪ Last Update     : 2025-11-06                                                                                           |
         | ▪ Creation Date   : 2025-01-11                                                                                           |
         | ▪ Description     : Fungsi Utama Engine                                                                                  |
         +--------------------------------------------------------------------------------------------------------------------------+
@@ -107,6 +107,8 @@ namespace App\Http\Controllers\Application\BackEnd\System\Transaction\Engines\up
                                     $varData['entities']['fatPatDoStatus'],
                                     $varData['entities']['assetStatus'],
                                     $varData['entities']['depreciationAssetCategory_RefID'],
+                                    $varData['entities']['period'],
+                                    $varData['entities']['rate'],
                                     $varData['entities']['depreciationCOA_RefID'],
                                     $varData['entities']['deduction'],
                                     $varData['entities']['remarks'],
