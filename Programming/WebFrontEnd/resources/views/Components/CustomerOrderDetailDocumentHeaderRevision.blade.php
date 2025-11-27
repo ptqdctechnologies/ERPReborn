144536ab085cba02062e7d98e9ee2b2c3b0c1d70
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-11-27 15:04:55 +0700

    feat(document-tracking): create customer order components

---
+++ Programming/WebFrontEnd/resources/views/Components/CustomerOrderDetailDocumentHeaderRevision.blade.php
@@ -0,0 +1,47 @@
+<div class="table-budget-wrapper" style="display: flex;">
+    <!-- Kolom Fixed -->
+    <div class="table-fixed" style="flex: 0 0 auto;">
+        <table class="table table-striped table-hover table-sm">
+            <thead>
+                <tr>
+                    <th class="text-center" colspan="5" style="vertical-align: middle;left: 0px;z-index: 10;line-height: normal;position: sticky;background: #FFF;">
+                        Rev 1 (Actual) - <br /> ( 2025-09-30 10:31 )
+                    </th>
+                </tr>
+                <tr>
+                    <th style="vertical-align: middle; width: 125px; min-width: 125px; max-width: 125px; left: 0px; z-index: 10;position: sticky; background-color: white;" class="text-center">VAT (%)</th>
+                    <th style="vertical-align: middle; width: 200px; min-width: 200px; max-width: 200px; left: 125px; z-index: 10;position: sticky; background-color: white;" class="text-center">Remark</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th style="vertical-align: middle; width: 125px; min-width: 125px; max-width: 125px; left: 0px; z-index: 10;position: sticky; background-color: white;">10.00</th>
+                    <th style="vertical-align: middle; width: 200px; min-width: 200px; max-width: 200px; left: 125px; z-index: 10;position: sticky; background-color: white;">-</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <!-- Kolom Scroll -->
+    <div class="table-scroll" style="overflow-x: auto; width: 100%;">
+        <table class="table table-striped table-hover table-sm">
+            <thead>
+                <tr>
+                    <th class="text-center" colspan="4" style="background-color:#4B586A;color:white;border-right:1px solid #e9ecef;line-height: normal;vertical-align: middle; min-width: 320px;">
+                        Original - <br /> ( 2025-09-29 10:31 )
+                    </th>
+                </tr>
+                <tr>
+                    <th class="text-center" style="background-color:#4B586A;color:white;border-right:1px solid #e9ecef;vertical-align: middle;width: 125px; min-width: 125px; max-width: 125px;">VAT (%)</th>
+                    <th class="text-center" style="background-color:#4B586A;color:white;border-right:1px solid #e9ecef;vertical-align: middle;width: 200px; min-width: 200px; max-width: 200px;">Remark</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="padding: 8px;width: 125px; min-width: 125px; max-width: 125px;">15.00</td>
+                    <td style="padding: 8px;width: 200px; min-width: 200px; max-width: 200px;">-</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
---
+++ Programming/WebFrontEnd/resources/views/Components/CustomerOrderDetailDocumentRevision.blade.php
@@ -0,0 +1,51 @@
+<div class="table-budget-wrapper" style="display: flex;">
+    <!-- Kolom Fixed -->
+    <div class="table-fixed" style="flex: 0 0 auto;">
+        <table class="table table-striped table-hover table-sm">
+            <thead>
+                <tr>
+                    <th class="text-center" colspan="7" style="vertical-align: middle;left: 0px;z-index: 10;line-height: normal;position: sticky;background: #FFF;">
+                        Rev 1 (Actual) - <br /> ( 2025-09-30 10:31 )
+                    </th>
+                </tr>
+                <tr>
+                    <th class="text-center" style="vertical-align: middle; width: 170px; min-width: 170px; max-width: 170px; left: 0px; z-index: 10;position: sticky; background-color: white;">Sub Budget</th>
+                    <th class="text-center" style="vertical-align: middle; width: 145px; min-width: 145px; max-width: 145px; left: 170px; z-index: 10;position: sticky; background-color: white;">Value</th>
+                    <th class="text-center" style="vertical-align: middle; width: 145px; min-width: 145px; max-width: 145px; left: 315px; z-index: 10;position: sticky; background-color: white;">Notes</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="padding: 8px; width: 170px; min-width: 170px; max-width: 170px; position: sticky; background-color: white; left: 0px; z-index: 10;">240 - Cendana Andalas</td>
+                    <td style="padding: 8px; width: 145px; min-width: 145px; max-width: 145px; position: sticky; background-color: white; left: 170px; z-index: 10;">1,000,000,000.00</td>
+                    <td style="padding: 8px; width: 145px; min-width: 145px; max-width: 145px; position: sticky; background-color: white; left: 315px; z-index: 10;">-</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <!-- Kolom Scroll -->
+    <div class="table-scroll" style="overflow-x: auto; width: 100%;">
+        <table class="table table-striped table-hover table-sm">
+            <thead>
+                <tr>
+                    <th class="text-center" colspan="3" style="background-color:#4B586A;color:white;border-right:1px solid #e9ecef;line-height: normal;vertical-align: middle; min-width: 320px;">
+                        Original - <br /> ( 2025-09-29 10:31 )
+                    </th>
+                </tr>
+                <tr>
+                    <th class="text-center" style="background-color:#4B586A;color:white;border-right:1px solid #e9ecef;vertical-align: middle;width: 125px; min-width: 125px; max-width: 125px;">Sub Budget</th>
+                    <th class="text-center" style="background-color:#4B586A;color:white;border-right:1px solid #e9ecef;vertical-align: middle;width: 125px; min-width: 125px; max-width: 125px;">Value</th>
+                    <th class="text-center" style="background-color:#4B586A;color:white;border-right:1px solid #e9ecef;vertical-align: middle;width: 125px; min-width: 125px; max-width: 125px;">Notes</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="padding: 8px;width: 125px; min-width: 125px; max-width: 125px;">256 - XL Center Banda Aceh</td>
+                    <td style="padding: 8px;width: 125px; min-width: 125px; max-width: 125px;">400,000,000.00</td>
+                    <td style="padding: 8px;width: 125px; min-width: 125px; max-width: 125px;">-</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
