9d2476a4988cddb301e967400b116f19b5f59ee3
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-12-03 16:43:19 +0700

    feat(imports): change property in BudgetImport

--- Programming/WebFrontEnd/app/Imports/BudgetImport.php
+++ Programming/WebFrontEnd/app/Imports/BudgetImport.php
@@ -16,13 +16,17 @@ class BudgetImport implements ToCollection, WithCalculatedFormulas
             if ($key === 0) continue;
 
             $data[] = [
-                'sub_budget'    => $row[0],
-                'work_id'       => $row[1],
-                'product'       => $row[2],
-                'currency'      => $row[3],
-                'qty'           => $row[4],
-                'price'         => $row[5],
-                'total'         => $row[6],
+                'sub_budget_code'   => $row[1],
+                'sub_budget_name'   => $row[2],
+                'work_code'         => $row[3],
+                'work_name'         => $row[4],
+                'product_code'      => $row[5],
+                'product_name'      => $row[6],
+                'currency_code'     => $row[7],
+                'currency_name'     => $row[8],
+                'qty'               => $row[9],
+                'price'             => $row[10],
+                'total'             => $row[11]
             ];
         }
 
