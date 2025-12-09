879fbcdb6eb9832e0f903eddff94f84f0e38a890
ijonk7 <jookeo.rizal@gmail.com>
2025-12-09 15:02:22 +0700

    Create example API call 'report.form.documentForm.finance.getAdvanceSummary'

---
+++ Programming/.LaravelCore/app/Http/Controllers/Application/FrontEnd/SandBox/Examples_APICall/report/form/documentForm/finance/getAdvanceSummary/v1/example.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
++----------------------------------------------------------------------------------------------------------------------------------+
+| ▪ Category    : Example - API Call Controller                                                                                    |
+| ▪ Name Space  : \App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\report\form\documentForm\finance             |
+|                 \getAdvanceSummary\v1                                                                                            |
+| ▪ API Key     : report.form.documentForm.finance.getAdvanceSummary                                                               |
+| ▪ API Version : 1                                                                                                                |
+|                                                                                                                                  |
+| ▪ Copyleft 🄯 2025 ijonk7 (rizal.devapps@gmail.com)                                                                               |
++----------------------------------------------------------------------------------------------------------------------------------+
+*/
+namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\report\form\documentForm\finance\getAdvanceSummary\v1
+    {
+    class example extends \App\Http\Controllers\Controller
+        {
+        /*
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Call URL        : http(s)://<HOST>/report.form.documentForm.finance.getAdvanceSummary.v1_throughAPIGateway             |
+        |                     ► http://172.28.0.4/report.form.documentForm.finance.getAdvanceSummary.v1_throughAPIGateway          |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Version         : 1.0000.0000000                                                                                       |
+        | ▪ Last Update     : 2025-12-09                                                                                           |
+        | ▪ Creation Date   : 2025-12-09                                                                                           |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        */
+        public function throughAPIGateway($varAPIWebToken)
+            {
+            //---Parameter Set---
+            if (!$varAPIWebToken) {
+                $varAPIWebToken = \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System();
+                }
+            //---Core---
+            $varData = \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGateway(
+                \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
+                $varAPIWebToken,
+                'report.form.documentForm.finance.getAdvanceSummary',
+                'latest',
+                [
+                'parameter' => [
+                    'CombinedBudgetCode' => 'Q000062',
+                    'CombinedBudgetSectionCode' => '235',
+                    'RequesterWorkerJobsPosition_RefID' => 164000000000559,
+                    'BeneficiaryWorkerJobsPosition_RefID' => 164000000000549,
+                    'StartDate' => '2025-02-25',
+                    'EndDate' => '2025-03-04',
+                    // 'CombinedBudgetCode' => NULL,
+                    // 'CombinedBudgetSectionCode' => NULL,
+                    // 'RequesterWorkerJobsPosition_RefID' => NULL,     // ARF Requester
+                    // 'BeneficiaryWorkerJobsPosition_RefID' => NULL,     // ARF Beneficiary
+                    // 'StartDate' => NULL,
+                    // 'EndDate' => NULL,
+                    ]
+                ]
+                );
+            return $varData;
+            }
+        }
+    }
