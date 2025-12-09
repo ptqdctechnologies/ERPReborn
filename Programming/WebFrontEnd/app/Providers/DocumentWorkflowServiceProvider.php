1d1b92b85fc8cb79d6c30534f7c3d0803dd9e40e
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-12-09 15:04:43 +0700

    feat(providers): remove document workflow composer

--- Programming/WebFrontEnd/app/Providers/DocumentWorkflowServiceProvider.php
+++ Programming/WebFrontEnd/app/Providers/DocumentWorkflowServiceProvider.php
@@ -5,13 +5,11 @@ namespace App\Providers;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use App\Http\ViewComposers\CountDocumentProcessComposer;
-use App\Http\ViewComposers\DocumentWorkflowComposer;
 
 class DocumentWorkflowServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         View::composer('Partials.sidebar', CountDocumentProcessComposer::class);
-        View::composer('Dashboard.index', DocumentWorkflowComposer::class);
     }
 }
