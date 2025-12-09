2897f7bbef9417162c107f5fdf0cbfc03f834d11
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-12-09 15:42:11 +0700

    feat(providers): remove call count document process composer

--- Programming/WebFrontEnd/app/Providers/DocumentWorkflowServiceProvider.php
+++ Programming/WebFrontEnd/app/Providers/DocumentWorkflowServiceProvider.php
@@ -4,12 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
-use App\Http\ViewComposers\CountDocumentProcessComposer;
 
 class DocumentWorkflowServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        View::composer('Partials.sidebar', CountDocumentProcessComposer::class);
     }
 }
