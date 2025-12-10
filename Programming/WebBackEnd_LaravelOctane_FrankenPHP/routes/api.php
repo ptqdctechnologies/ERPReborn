68f6de8a2929083c15fe6128815f9b362d2ace46
Teguh Pratama Januzir S <teguhpjs@gmail.com>
2025-12-10 17:24:27 +0700

    Update Pertanggal 10 Desember 2025 17:12 WIB

---
+++ Programming/WebBackEnd_LaravelOctane_FrankenPHP/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+Route::get(
+    '/user',
+    function (Request $request) {
+        return $request->user();
+        }
+    )->middleware('auth:sanctum');
+*/
+
+
