21ea39ab27f8d4214cfade9661256c0c3fadd20d
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-12-15 09:29:41 +0700

    feat(purchase-request): update process submit workflow

--- Programming/WebFrontEnd/resources/views/Purchase/PurchaseRequisition/Transactions/CreatePurchaseRequisition.blade.php
+++ Programming/WebFrontEnd/resources/views/Purchase/PurchaseRequisition/Transactions/CreatePurchaseRequisition.blade.php
@@ -6,7 +6,7 @@
 @include('getFunction.getProduct')
 @include('getFunction.getSites')
 @include('getFunction.getWarehouses')
-@include('getFunction.getWorkFlow')
+@include('getFunction.getWorkFlows')
 @include('getFunction.getPurchaseRequisition')
 @include('Purchase.PurchaseRequisition.Functions.PopUp.PopUpPrRevision')
 @include('Purchase.PurchaseRequisition.Functions.PopUp.PopUpPurchaseRequisitionSummaryData')
@@ -26,89 +26,86 @@
       @include('Purchase.PurchaseRequisition.Functions.Menu.MenuProcReq')
       @if($var == 0)
       <div class="card">
-        <form method="post" action="{{ route('SelectWorkFlow') }}" id="FormSubmitPurchaseRequisition">
-        @csrf
-          <input type="hidden" name="DocumentTypeID" id="DocumentTypeID" value="<?= $documentType_RefID; ?>">
-          <input type="hidden" name="var_combinedBudget_RefID" id="var_combinedBudget_RefID">
+        <input type="hidden" name="DocumentTypeID" id="DocumentTypeID" value="<?= $documentType_RefID; ?>">
+        <input type="hidden" name="var_combinedBudget_RefID" id="var_combinedBudget_RefID">
 
-          <!-- BUDGET INFORMATION -->
-          <div class="tab-content px-3 pt-4 pb-2" id="nav-tabContent">
-            <div class="row">
-              <div class="col-12">
-                <div class="card">
-                  <!-- HEADER -->
-                  <div class="card-header">
-                    <label class="card-title">
-                      Budget Information
-                    </label>
-                    <div class="card-tools">
-                      <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                        <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
-                      </button>
-                    </div>
+        <!-- BUDGET INFORMATION -->
+        <div class="tab-content px-3 pt-4 pb-2" id="nav-tabContent">
+          <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <!-- HEADER -->
+                <div class="card-header">
+                  <label class="card-title">
+                    Budget Information
+                  </label>
+                  <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                      <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
+                    </button>
                   </div>
-
-                  @include('Purchase.PurchaseRequisition.Functions.Header.HeaderProcReq')
                 </div>
+
+                @include('Purchase.PurchaseRequisition.Functions.Header.HeaderProcReq')
               </div>
             </div>
           </div>
+        </div>
 
-          <!-- PURCHASE REQUEST -->
-          <div class="tab-content px-3 pb-2" id="nav-tabContent">
-            <div class="row">
-              <div class="col-12">
-                <div class="card">
-                  <!-- HEADER -->
-                  <div class="card-header">
-                    <label class="card-title">
-                      Purchase Request
-                    </label>
-                    <div class="card-tools">
-                      <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                        <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
-                      </button>
-                    </div>
+        <!-- PURCHASE REQUEST -->
+        <div class="tab-content px-3 pb-2" id="nav-tabContent">
+          <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <!-- HEADER -->
+                <div class="card-header">
+                  <label class="card-title">
+                    Purchase Request
+                  </label>
+                  <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                      <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
+                    </button>
                   </div>
-
-                  @include('Purchase.PurchaseRequisition.Functions.Header.HeaderProcReq2')
                 </div>
+
+                @include('Purchase.PurchaseRequisition.Functions.Header.HeaderProcReq2')
               </div>
             </div>
           </div>
+        </div>
 
-          <!-- FILE ATTACHMENT -->
-          <div class="tab-content px-3 pb-2" id="nav-tabContent">
-            <div class="row">
-              <div class="col-12">
-                <div class="card">
-                  <!-- HEADER -->
-                  <div class="card-header">
-                    <label class="card-title">
-                      File Attachment
-                    </label>
-                    <div class="card-tools">
-                      <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                        <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
-                      </button>
-                    </div>
+        <!-- FILE ATTACHMENT -->
+        <div class="tab-content px-3 pb-2" id="nav-tabContent">
+          <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <!-- HEADER -->
+                <div class="card-header">
+                  <label class="card-title">
+                    File Attachment
+                  </label>
+                  <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                      <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
+                    </button>
                   </div>
+                </div>
 
-                  <!-- BODY -->
-                  <div class="card-body">
-                    <div class="row py-3">
-                      <div class="col-lg-5">
-                        <div class="row">
-                          <div class="col p-0">
-                            <input type="text" id="dataInput_Log_FileUpload" name="dataInput_Log_FileUpload_1" style="display:none">
-                            <?php echo \App\Helpers\ZhtHelper\General\Helper_JavaScript::getSyntaxCreateDOM_DivCustom_InputFile(\App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
-                              $varAPIWebToken,
-                              'dataInput_Log_FileUpload',
-                              null,
-                              'dataInput_Return'
-                              ).
-                            ''; ?>
-                          </div>
+                <!-- BODY -->
+                <div class="card-body">
+                  <div class="row py-3">
+                    <div class="col-lg-5">
+                      <div class="row">
+                        <div class="col p-0">
+                          <input type="text" id="dataInput_Log_FileUpload" name="dataInput_Log_FileUpload_1" style="display:none">
+                          <?php echo \App\Helpers\ZhtHelper\General\Helper_JavaScript::getSyntaxCreateDOM_DivCustom_InputFile(\App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
+                            $varAPIWebToken,
+                            'dataInput_Log_FileUpload',
+                            null,
+                            'dataInput_Return'
+                            ).
+                          ''; ?>
                         </div>
                       </div>
                     </div>
@@ -117,45 +114,45 @@
               </div>
             </div>
           </div>
+        </div>
 
-          <!-- PURCHASE REQUEST DETAILS -->
-          <div class="tab-content px-3 pb-2" id="nav-tabContent">
-            <div class="row">
-              <div class="col-12">
-                <div class="card">
-                  <!-- HEADER -->
-                  <div class="card-header">
-                    <label class="card-title">
-                      Purchase Request Details
-                    </label>
-                    <div class="card-tools">
-                      <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                        <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
-                      </button>
-                    </div>
+        <!-- PURCHASE REQUEST DETAILS -->
+        <div class="tab-content px-3 pb-2" id="nav-tabContent">
+          <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <!-- HEADER -->
+                <div class="card-header">
+                  <label class="card-title">
+                    Purchase Request Details
+                  </label>
+                  <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                      <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
+                    </button>
                   </div>
-
-                  @include('Purchase.PurchaseRequisition.Functions.Table.getBOQ')
                 </div>
+
+                @include('Purchase.PurchaseRequisition.Functions.Table.getBOQ')
               </div>
             </div>
           </div>
+        </div>
 
-          <!-- BUTTON -->
-          <div class="tab-content px-3 pb-2" id="nav-tabContent">
-            <div class="row">
-              <div class="col">
-                <button type="button" class="btn btn-default btn-sm float-right" onclick="validationForm()" style="margin-left: 5px;background-color:#e9ecef;border:1px solid #ced4da;">
-                  <img src="{{ asset('AdminLTE-master/dist/img/save.png') }}" width="13" alt="" title="Submit to Advance"> Submit
-                </button>
+        <!-- BUTTON -->
+        <div class="tab-content px-3 pb-2" id="nav-tabContent">
+          <div class="row">
+            <div class="col">
+              <button type="button" class="btn btn-default btn-sm float-right" onclick="validationForm()" style="margin-left: 5px;background-color:#e9ecef;border:1px solid #ced4da;">
+                <img src="{{ asset('AdminLTE-master/dist/img/save.png') }}" width="13" alt="" title="Submit to Advance"> Submit
+              </button>
 
-                <a onclick="cancelForm('{{ route('PurchaseRequisition.index', ['var' => 1]) }}')" class="btn btn-default btn-sm float-right" style="background-color:#e9ecef;border:1px solid #ced4da;">
-                  <img src="{{ asset('AdminLTE-master/dist/img/cancel.png') }}" width="13" alt="" title="Cancel Advance List Cart"> Cancel
-                </a>
-              </div>
+              <a onclick="cancelForm('{{ route('PurchaseRequisition.index', ['var' => 1]) }}')" class="btn btn-default btn-sm float-right" style="background-color:#e9ecef;border:1px solid #ced4da;">
+                <img src="{{ asset('AdminLTE-master/dist/img/cancel.png') }}" width="13" alt="" title="Cancel Advance List Cart"> Cancel
+              </a>
             </div>
           </div>
-        </form>
+        </div>
       </div>
       @endif
     </div>
