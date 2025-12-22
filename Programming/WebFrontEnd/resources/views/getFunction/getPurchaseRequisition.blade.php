ae4a831c1adf88489d58b89579d5c6105c7c93a3
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-12-22 13:49:11 +0700

    refactor(get-function): remove unused code in get purchase requisition

--- Programming/WebFrontEnd/resources/views/getFunction/getPurchaseRequisition.blade.php
+++ Programming/WebFrontEnd/resources/views/getFunction/getPurchaseRequisition.blade.php
@@ -67,30 +67,16 @@
             }
         });
 
-        // var keys = 0;
         $.ajax({
             type: 'GET',
             url: '{!! route("getPurchaseRequisitionList") !!}',
             success: function(data) {
                 $(".loadingGetModalPurchaseRequisition").hide();
 
-                // var no = 1;
-                var table = $('#tableGetModalPurchaseRequisition').DataTable();
+                let table = $('#tableGetModalPurchaseRequisition').DataTable();
                 table.clear();
 
                 if (Array.isArray(data) && data.length > 0) {
-                    // $.each(data, function(key, val) {
-                    //     keys += 1;
-                    //     table.row.add([
-                    //         '<input id="sys_id_modal_purchase_requisition' + keys + '" value="' + val.sys_ID + '" data-trigger="sys_id_modal_purchase_requisition" type="hidden">' + no++,
-                    //         '<input id="sys_id_combinedBudget_purchase_requisition' + keys + '" value="' + val.combinedBudget_RefID + '" data-trigger="sys_id_combinedBudget_purchase_requisition" type="hidden">' + val.sys_Text || '-',
-                    //         val.combinedBudgetCode || '-',
-                    //         val.combinedBudgetName || '-',
-                    //         val.combinedBudgetSectionCode || '-',
-                    //         val.combinedBudgetSectionName || '-',
-                    //     ]).draw();
-                    // });
-
                     $('#tableGetModalPurchaseRequisition').DataTable({
                         destroy: true,
                         data: data,
@@ -137,11 +123,6 @@
                     });
 
                     $('#tableGetModalPurchaseRequisition').css("width", "100%");
-
-                    // $("#tableGetModalPurchaseRequisition_length").show();
-                    // $("#tableGetModalPurchaseRequisition_filter").show();
-                    // $("#tableGetModalPurchaseRequisition_info").show();
-                    // $("#tableGetModalPurchaseRequisition_paginate").show();
                 } else {
                     $(".errorModalPurchaseRequisitionMessageContainerSecond").show();
                     $("#errorModalPurchaseRequisitionMessageSecond").text(`Data not found.`);
@@ -173,8 +154,6 @@
 
         $("#modal_purchase_requisition_id").val(sysId);
         $("#modal_purchase_requisition_document_number").val(trano);
-
-        // adjustInputSize(document.getElementById("modal_purchase_requisition_document_number"), "string");
 
         $('#purchaseRequisitionModal').modal('hide');
     });
