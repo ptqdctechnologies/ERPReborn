24694d24fd355a6229bf07c573193202d019b3b9
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2025-12-22 17:30:03 +0700

    feat(purchase-request): add export excel feature

--- Programming/WebFrontEnd/app/Http/Controllers/ExportExcel/PurchaseRequisition/ExportReportPurchaseRequisitionSummary.php
+++ Programming/WebFrontEnd/app/Http/Controllers/ExportExcel/PurchaseRequisition/ExportReportPurchaseRequisitionSummary.php
@@ -4,56 +4,67 @@ namespace App\Http\Controllers\ExportExcel\PurchaseRequisition;
 
 use Illuminate\Support\Facades\Session;
 use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
-use Maatwebsite\Excel\Concerns\WithEvents;
-use Maatwebsite\Excel\Events\BeforeSheet;
-use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
 use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class ExportReportPurchaseRequisitionSummary implements FromCollection, WithHeadings, ShouldAutoSize, WithEvents
+class ExportReportPurchaseRequisitionSummary implements FromCollection, WithHeadings, ShouldAutoSize, WithStyles
 {
+    protected $dataPurchaseRequestSummary;
+
+    public function __construct($dataPurchaseRequestSummary)
+    {
+        $this->dataPurchaseRequestSummary = $dataPurchaseRequestSummary;
+    }
+
     public function collection()
     {
-        $data = Session::get("PurchaseRequisitionReportSummaryDataExcel");
+        $data = $this->dataPurchaseRequestSummary;
+
+        $totalIDR           = 0;
+        $totalOtherCurrency = 0;
+        $totalEquivalentIDR = 0;
+
         $filteredData = [];
         $counter = 1;
-
-        $grandTotalIDR = 0;
-        $grandTotalOtherCurrency = 0;
-
         foreach ($data as $item) {
-            $idr = $item['total_IDR'] ?? 0;
-            $other = $item['total_Other_Currency'] ?? 0;
+            $totalIDR           += is_numeric($item['total_IDR']) ? $item['total_IDR'] : 0;
+            $totalOtherCurrency += is_numeric($item['total_Other_Currency']) ? $item['total_Other_Currency'] : 0;
+            $totalEquivalentIDR += is_numeric($item['grand_Total_Equivalent_IDR']) ? $item['grand_Total_Equivalent_IDR'] : 0;
 
-            $grandTotalIDR += $idr;
-            $grandTotalOtherCurrency += $other;
+            $dateOrigin     = new \DateTime($item['date']);
+            $resultOrigin   = $dateOrigin->format('Y-m-d');
+            $dateDelivery   = new \DateTime($item['dateOfDelivery']);
+            $resultDelivery = $dateDelivery->format('Y-m-d');
 
             $filteredData[] = [
-                'No'                                => $counter++,
-                'PR Number'                         => $item['documentNumber'] ?? null,
-                'Date'                              => date('Y-m-d', strtotime($item['date'])) ?? null,
-                'Sub Budget'                        => null,
-                'Delivery From'                     => null,
-                'Delivery To'                       => null,
-                'Total Idr'                         => $idr,
-                'Total Other Currency'              => $other,
-                'Total Equivalent IDR'              => null,
+                'No'                    => $counter++,
+                'PR Number'             => $item['documentNumber'] ?? '-',
+                'Date'                  => $resultOrigin,
+                'Budget'                => ($item['combinedBudgetCode'] ?? '') . ' - ' . ($item['combinedBudgetName'] ?? ''),
+                'Date Of Delivery'      => $resultDelivery,
+                'Delivery To'           => $item['deliveryTo_NonRefID']['address'] ?? '-',
+                'Total IDR'             => $item['total_IDR'] ?? '-',
+                'Total Other Currency'  => $item['total_Other_Currency'] ?? '-',
+                'Total Equivalent IDR'  => $item['grand_Total_Equivalent_IDR'] ?? '-',
+                'Remark'                => '-',
             ];
         }
 
-        // Tambahkan baris grand total
         $filteredData[] = [
-            'No'                                => '',
-            'PR Number'                         => 'Grand Total:',
-            'Date'                              => '',
-            'Sub Budget'                        => '',
-            'Delivery From'                     => '',
-            'Delivery To'                       => '',
-            'Total Idr'                         => $grandTotalIDR,
-            'Total Other Currency'              => $grandTotalOtherCurrency,
-            'Total Equivalent IDR'              => '',
+            'No'                    => 'GRAND TOTAL',
+            'PR Number'             => '',
+            'Date'                  => '',
+            'Budget'                => '',
+            'Date Of Delivery'      => '',
+            'Delivery To'           => '',
+            'Total IDR'             => $totalIDR,
+            'Total Other Currency'  => $totalOtherCurrency,
+            'Total Equivalent IDR'  => $totalEquivalentIDR,
+            'Remark'                => '',
         ];
 
         return collect($filteredData);
@@ -61,63 +72,147 @@ class ExportReportPurchaseRequisitionSummary implements FromCollection, WithHead
 
     public function headings(): array
     {
-        $data = Session::get("PurchaseRequisitionReportSummaryDataExcel");
+        $data = $this->dataPurchaseRequestSummary;
 
         return [
-            ["Budget", ": " . $data[0]['combinedBudgetCode'] . ' - ' . $data[0]['combinedBudgetName'], "", "", "", "", "", "", ""],
+            [date('F j, Y')],
+            ["PURCHASE REQUEST SUMMARY", " ", " ", " ", " ", " ", " "],
+            [date('h:i A')],
+            ["Budget", ": " . $data[0]['combinedBudgetCode'] . ' - ' . $data[0]['combinedBudgetName'], "", "Date", ": -", "", "", "", ""],
+            ["Sub Budget", ": -", "", "", "", "", "", "", "", "", ""],
             ["", "", "", "", "", "", "", "", "", "", ""],
-            ["No", "PR Number","Date", "Sub Budget", "Delivery From", "Delivery To", "Total IDR", "Total Other Currency", "Total Equivalent IDR"],
+            ["No", "PR Number","Date", "Budget", "Date Of Delivery", "Delivery To", "Total IDR", "Total Other Currency", "Total Equivalent IDR", "Remark"],
         ];
     }
 
-    public function registerEvents(): array
+    public function styles(Worksheet $sheet)
     {
-        return [
-            BeforeSheet::class => function (BeforeSheet $event) {
-                $sheet = $event->sheet->getDelegate();
-
-                $data = Session::get("PurchaseRequisitionReportSummaryDataExcel");
-                // $dataHeader = $data['dataHeader'];
-
-                $sheet->setCellValue('A1', date('F j, Y'))
-                    ->mergeCells('A1:I1')
-                    ->getStyle('A1:I1')
-                    ->applyFromArray([
-                        'font' => [
-                            'bold' => true,
-                            'color' => ['rgb' => '000000'],
-                        ],
-                        'alignment' => [
-                            'horizontal' => Alignment::HORIZONTAL_RIGHT,
-                        ],
-                ]);
-
-                $sheet->setCellValue('A2', 'Purchase Requisition Summary Report')
-                    ->mergeCells('A2:I2')
-                    ->getStyle('A2:I2')
-                    ->applyFromArray([
-                        'font' => [
-                            'bold' => true,
-                            'color' => ['rgb' => '000000'],
-                        ],
-                        'alignment' => [
-                            'horizontal' => Alignment::HORIZONTAL_CENTER,
-                        ],
-                ]);
-
-                $sheet->setCellValue('A3', date('h:i A'))
-                    ->mergeCells('A3:I3')
-                    ->getStyle('A3:I3')
-                    ->applyFromArray([
-                        'font' => [
-                            'bold' => true,
-                            'color' => ['rgb' => '000000'],
-                        ],
-                        'alignment' => [
-                            'horizontal' => Alignment::HORIZONTAL_RIGHT,
-                        ],
-                ]);
-            },
+        $styleArrayHeader0 = [
+            'font' => [
+                'bold' => true,
+                'color' => [
+                    'rgb' => '000000',
+                ],
+            ],
+            'alignment' => [
+                'horizontal' => Alignment::HORIZONTAL_RIGHT,
+            ]
         ];
+
+        $sheet->getStyle('A1:J1')->applyFromArray($styleArrayHeader0);
+        $sheet->mergeCells('A1:J1');
+
+        $styleArrayHeader1 = [
+            'font' => [
+                'bold' => true,
+                'color' => [
+                    'rgb' => '000000',
+                ],
+            ],
+            'alignment' => [
+                'horizontal' => Alignment::HORIZONTAL_CENTER,
+            ]
+        ];
+
+        $sheet->getStyle('A2:J2')->applyFromArray($styleArrayHeader1);
+        $sheet->mergeCells('A2:J2');
+
+        $styleArrayHeader2 = [
+            'font' => [
+                'bold' => true,
+                'color' => [
+                    'rgb' => '000000',
+                ],
+            ],
+            'alignment' => [
+                'horizontal' => Alignment::HORIZONTAL_RIGHT,
+            ]
+        ];
+
+        $sheet->getStyle('A3:J3')->applyFromArray($styleArrayHeader2);
+        $sheet->mergeCells('A3:J3');
+
+        $styleArrayHeader3 = [
+            'font' => [
+                'bold' => true,
+                'color' => [
+                    'rgb' => '000000',
+                ],
+            ],
+            'alignment' => [
+                'horizontal' => Alignment::HORIZONTAL_LEFT,
+            ]
+        ];
+
+        $sheet->getStyle('A4:J4')->applyFromArray($styleArrayHeader3);
+        $sheet->getStyle('A5:J5')->applyFromArray($styleArrayHeader3);
+
+        $styleArrayHeader4 = [
+            'font' => [
+                'bold' => true,
+                'color' => [
+                    'rgb' => '000000',
+                ],
+            ],
+            'alignment' => [
+                'horizontal' => Alignment::HORIZONTAL_CENTER,
+            ],
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => Border::BORDER_THIN,
+                ],
+            ],
+            'fill' => [
+                'fillType' => 'solid',
+                'rotation' => 0,
+                'color' => [
+                    'rgb' => 'E9ECEF',
+                ],
+            ],
+        ];
+
+        $sheet->getStyle('A7:J7')->applyFromArray($styleArrayHeader4);
+
+        $styleArrayContent = [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => Border::BORDER_THIN,
+                ],
+            ],
+            'alignment' => [
+                'horizontal' => Alignment::HORIZONTAL_LEFT,
+            ],
+        ];
+
+        $datas      = $this->dataPurchaseRequestSummary;
+        $totalCell  = count($datas);
+        $lastCell   = 'A7:J' . $totalCell + 7;
+        $sheet->getStyle($lastCell)->applyFromArray($styleArrayContent);
+
+        $styleArrayFooter = [
+            'font' => [
+                'bold' => true,
+                'color' => [
+                    'rgb' => '000000',
+                ],
+            ],
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => Border::BORDER_THIN,
+                ],
+            ],
+            'alignment' => [
+                'horizontal' => Alignment::HORIZONTAL_CENTER,
+            ],
+            'fill' => [
+                'fillType' => 'solid',
+                'rotation' => 0,
+                'color' => [
+                    'rgb' => 'E9ECEF',
+                ],
+            ],
+        ];
+        $sheet->getStyle('A' . $totalCell + 8 . ':' . 'J' . $totalCell + 8)->applyFromArray($styleArrayFooter);
+        $sheet->mergeCells('A' . $totalCell + 8 . ':F' . $totalCell + 8);
     }
 }
