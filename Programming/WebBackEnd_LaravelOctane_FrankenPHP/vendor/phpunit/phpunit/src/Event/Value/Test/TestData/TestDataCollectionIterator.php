d626675654b0cf4a0afc8bd5b730d76999385150
Teguh Pratama Januzir S <teguhpjs@gmail.com>
2026-01-21 10:09:50 +0700

    Update Pertanggal 21 Januari 2026 10:09 WIB

--- Programming/WebBackEnd_LaravelOctane_FrankenPHP/vendor/phpunit/phpunit/src/Event/Value/Test/TestData/TestDataCollectionIterator.php
+++ Programming/WebBackEnd_LaravelOctane_FrankenPHP/vendor/phpunit/phpunit/src/Event/Value/Test/TestData/TestDataCollectionIterator.php
@@ -13,7 +13,7 @@ use function count;
 use Iterator;
 
 /**
- * @template-implements Iterator<int, TestData>
+ * @template-implements Iterator<non-negative-int, TestData>
  *
  * @no-named-arguments Parameter names are not covered by the backward compatibility promise for PHPUnit
  */
@@ -23,6 +23,10 @@ final class TestDataCollectionIterator implements Iterator
      * @var list<TestData>
      */
     private readonly array $data;
+
+    /**
+     * @var non-negative-int
+     */
     private int $position = 0;
 
     public function __construct(TestDataCollection $data)
@@ -40,6 +44,9 @@ final class TestDataCollectionIterator implements Iterator
         return $this->position < count($this->data);
     }
 
+    /**
+     * @return non-negative-int
+     */
     public function key(): int
     {
         return $this->position;
