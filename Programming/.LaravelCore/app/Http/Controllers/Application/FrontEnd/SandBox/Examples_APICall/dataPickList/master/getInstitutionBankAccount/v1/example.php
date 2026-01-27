f3a91157d7924dc4a0123575c227d36b81bc2ab9
ijonk7 <jookeo.rizal@gmail.com>
2026-01-27 10:11:05 +0700

    Create example API call 'dataPickList.master.getInstitutionBankAccount'

---
+++ Programming/.LaravelCore/app/Http/Controllers/Application/FrontEnd/SandBox/Examples_APICall/dataPickList/master/getInstitutionBankAccount/v1/example.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
++----------------------------------------------------------------------------------------------------------------------------------+
+| ▪ Category    : Example - API Call Controller                                                                                    |
+| ▪ Name Space  : \App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\dataPickList\master\getInstitutionBankAccount\v1     |
+| ▪ API Key     : dataPickList.master.getInstitutionBankAccount                                                                    |
+| ▪ API Version : 1                                                                                                                |
+|                                                                                                                                  |
+| ▪ Copyleft 🄯 2026 ijonk7 (rizal.devapps@gmail.com)                                                                               |
++----------------------------------------------------------------------------------------------------------------------------------+
+*/
+namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\dataPickList\master\getInstitutionBankAccount\v1
+    {
+    class example extends \App\Http\Controllers\Controller
+        {
+        /*
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Call URL        : http(s)://<HOST>/dataPickList.master.getInstitutionBankAccount.v1_throughAPIGateway                  |
+        |                     ► http://172.28.0.4/dataPickList.master.getInstitutionBankAccount.v1_throughAPIGateway               |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Version         : 1.0000.0000000                                                                                       |
+        | ▪ Last Update     : 2026-01-27                                                                                           |
+        | ▪ Creation Date   : 2026-01-27                                                                                           |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        */
+        public function throughAPIGateway($varAPIWebToken)
+            {
+            //---Parameter Set---
+            if (!$varAPIWebToken) {
+                $varAPIWebToken = \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System();
+                }
+            //---Core---
+            $varData = \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGateway(
+                \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
+                $varAPIWebToken,
+                'dataPickList.master.getInstitutionBankAccount',
+                'latest',
+                [
+                'parameter' => [
+                    ]
+                ]
+                );
+            return $varData;
+            }
+
+
+        /*
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Call URL        : http(s)://<HOST>/dataPickList.master.getInstitutionBankAccount.v1_throughAPIGatewayJQuery                    |
+        |                     ► http://172.28.0.4/dataPickList.master.getInstitutionBankAccount.v1_throughAPIGatewayJQuery                 |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Version         : 1.0000.0000000                                                                                       |
+        | ▪ Last Update     : 2026-01-27                                                                                           |
+        | ▪ Creation Date   : 2026-01-27                                                                                           |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        */
+        public function throughAPIGatewayJQuery($varAPIWebToken)
+            {
+            //---Parameter Set---
+            if (!$varAPIWebToken) {
+                $varAPIWebToken = \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System();
+                }
+            //---Core---
+            echo \App\Helpers\ZhtHelper\General\Helper_JavaScript::setLibrary(\App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System());
+            $varJQueryFunction = \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGatewayJQuery(
+                \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
+                $varAPIWebToken,
+                'dataPickList.master.getInstitutionBankAccount',
+                'latest',
+                '{'.
+                    '"parameter" : null'.
+                '}'
+                );
+            echo "<button type='button' onclick='javascript:var varData = ".$varJQueryFunction."; $(\"body\").append(JSON.stringify(varData));'>Submit Data</button>";
+            dd($varJQueryFunction);
+            }
+        }
+    }
