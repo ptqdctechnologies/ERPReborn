0eb73fac398c3ea4ba068db7c49d5fcb22409024
ijonk7 <jookeo.rizal@gmail.com>
2026-02-11 14:38:31 +0700

    Update example API call 'report.form.documentForm.finance.getLoanSettlementSummary'

--- Programming/.LaravelCore/app/Http/Controllers/Application/FrontEnd/SandBox/Examples_APICall/report/form/documentForm/finance/getLoanSettlementSummary/v1/example.php
+++ Programming/.LaravelCore/app/Http/Controllers/Application/FrontEnd/SandBox/Examples_APICall/report/form/documentForm/finance/getLoanSettlementSummary/v1/example.php
@@ -21,7 +21,7 @@ namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\rep
         |                     ► http://172.28.0.4/report.form.documentForm.finance.getLoanSettlementSummary.v1_throughAPIGateway   |
         +--------------------------------------------------------------------------------------------------------------------------+
         | ▪ Version         : 1.0000.0000000                                                                                       |
-        | ▪ Last Update     : 2026-02-06                                                                                           |
+        | ▪ Last Update     : 2026-02-11                                                                                           |
         | ▪ Creation Date   : 2025-09-25                                                                                           |
         +--------------------------------------------------------------------------------------------------------------------------+
         */
@@ -39,12 +39,16 @@ namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\rep
                 'latest',
                 [
                 'parameter' => [
-                    'CombinedBudgetCode' => 'Q000055',
+                    'CombinedBudgetCode' => 'Q000062',
                     'Creditor_RefID' => 166000000000001,
                     'Debitor_RefID' => 25000000000001,
+                    'StartDate' => '2025-01-01',
+                    'EndDate' => '2026-06-15',
                     // 'CombinedBudgetCode' => NULL,
                     // 'Creditor_RefID' => NULL,
                     // 'Debitor_RefID' => NULL,
+                    // 'StartDate' => NULL,
+                    // 'EndDate' => NULL,
                     ]
                 ]
                 );
