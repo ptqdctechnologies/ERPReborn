d8e9a8bd3a3e4ecc17eecb680a505858032eccf7
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-03-30 14:15:48 +0700

    feat(purchase-order): adjust position field

--- Programming/WebFrontEnd/resources/views/Purchase/PurchaseOrder/Functions/Table/tablePRDetail.blade.php
+++ Programming/WebFrontEnd/resources/views/Purchase/PurchaseOrder/Functions/Table/tablePRDetail.blade.php
@@ -2,8 +2,8 @@
     <table class="table table-head-fixed text-nowrap table-sm" id="tablePurchaseOrderDetail">
         <thead>
             <tr>
-                <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;">Work</th>
                 <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;">PR Number</th>
+                <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;">Work</th>
                 <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;">Sub Budget</th>
                 <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;">Product</th>
                 <th style="padding-top: 10px;padding-bottom: 10px;border:1px solid #e9ecef;text-align: center;">Qty PR</th>
