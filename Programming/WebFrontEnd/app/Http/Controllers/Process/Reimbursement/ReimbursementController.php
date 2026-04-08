52cd4e815dac58a3d97758bae6665f1e319c27c1
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-04-08 10:18:19 +0700

    feat(report): add validate budget, and date range in reimbursement summary

--- Programming/WebFrontEnd/app/Http/Controllers/Process/Reimbursement/ReimbursementController.php
+++ Programming/WebFrontEnd/app/Http/Controllers/Process/Reimbursement/ReimbursementController.php
@@ -25,29 +25,29 @@ class ReimbursementController extends Controller
     public function __construct(ReimbursementService $reimbursementService, WorkflowService $workflowService)
     {
         $this->reimbursementService = $reimbursementService;
-        $this->workflowService      = $workflowService;
+        $this->workflowService = $workflowService;
     }
 
     public function index(Request $request)
     {
         $varAPIWebToken = $request->session()->get('SessionLogin');
-        $var            = 0;
+        $var = 0;
         if (!empty($_GET['var'])) {
             $var = $_GET['var'];
         }
 
-        $documentTypeRefID  = $this->GetBusinessDocumentsTypeFromRedis('Reimbursement Form');
+        $documentTypeRefID = $this->GetBusinessDocumentsTypeFromRedis('Reimbursement Form');
 
         $compact = [
-            'var'               => $var,
-            'varAPIWebToken'    => $varAPIWebToken,
+            'var' => $var,
+            'varAPIWebToken' => $varAPIWebToken,
             'documentTypeRefID' => $documentTypeRefID
         ];
 
         return view('Process.Reimbursement.Transactions.CreateReimbursement', $compact);
     }
 
-    public function store(Request $request) 
+    public function store(Request $request)
     {
         try {
             $response = $this->reimbursementService->create($request);
@@ -68,8 +68,8 @@ class ReimbursementController extends Controller
             }
 
             $compact = [
-                "documentNumber"    => $response['data']['businessDocument']['documentNumber'],
-                "status"            => $responseWorkflow['metadata']['HTTPStatusCode'],
+                "documentNumber" => $response['data']['businessDocument']['documentNumber'],
+                "status" => $responseWorkflow['metadata']['HTTPStatusCode'],
             ];
 
             return response()->json($compact);
@@ -79,11 +79,11 @@ class ReimbursementController extends Controller
         }
     }
 
-    public function GetReimbursementDetail(Request $request) 
+    public function GetReimbursementDetail(Request $request)
     {
         try {
             $varAPIWebToken = $request->session()->get('SessionLogin');
-            $response       = $this->reimbursementService->getDetail($request->reimbursement_id);
+            $response = $this->reimbursementService->getDetail($request->reimbursement_id);
 
             if ($response['metadata']['HTTPStatusCode'] !== 200) {
                 return response()->json($response['data']);
@@ -99,9 +99,9 @@ class ReimbursementController extends Controller
     public function RevisionReimbursement(Request $request)
     {
         try {
-            $varAPIWebToken     = $request->session()->get('SessionLogin');
-            $response           = $this->reimbursementService->getDetail($request->modal_reimbursement_id);
-            $documentTypeRefID  = $this->GetBusinessDocumentsTypeFromRedis('Reimbursement Revision Form');
+            $varAPIWebToken = $request->session()->get('SessionLogin');
+            $response = $this->reimbursementService->getDetail($request->modal_reimbursement_id);
+            $documentTypeRefID = $this->GetBusinessDocumentsTypeFromRedis('Reimbursement Revision Form');
 
             if ($response['metadata']['HTTPStatusCode'] !== 200) {
                 throw new \Exception('Failed to fetch Detail Reimbursement');
@@ -110,34 +110,34 @@ class ReimbursementController extends Controller
             $data = $response['data']['data'];
 
             $compact = [
-                'varAPIWebToken'    => $varAPIWebToken,
+                'varAPIWebToken' => $varAPIWebToken,
                 'documentTypeRefID' => $documentTypeRefID,
-                'header'            => [
-                    'sys_RefID'                     => $data[0]['Sys_ID_Header'] ?? '',
-                    'combinedBudget_RefID'          => $data[0]['CombinedBudget_RefID'] ?? '',
-                    'combinedBudgetCode'            => $data[0]['CombinedBudgetCode'] ?? '',
-                    'combinedBudgetName'            => $data[0]['CombinedBudgetName'] ?? '',
-                    'combinedBudgetSection_RefID'   => $data[0]['CombinedBudgetSection_RefID'] ?? '',
-                    'combinedBudgetSectionCode'     => $data[0]['CombinedBudgetSectionCode'] ?? '',
-                    'combinedBudgetSectionName'     => $data[0]['CombinedBudgetSectionName'] ?? '',
-                    'customer_RefID'                => $data[0]['Requester_RefID'] ?? '',
-                    'customerCode'                  => $data[0]['RequesterCode'] ?? '',
-                    'customerName'                  => $data[0]['RequesterName'] ?? '',
-                    'beneficiary_RefID'             => $data[0]['Beneficiary_RefID'] ?? '',
-                    'beneficiaryName'               => $data[0]['BeneficiaryName'] ?? '',
-                    'beneficiaryPosition'           => $data[0]['BeneficiaryCode'] ?? '',
-                    'beneficiaryBank_RefID'         => $data[0]['BeneficiaryBank_RefID'] ?? '',
-                    'beneficiaryBankName'           => $data[0]['BeneficiaryBankName'] ?? '',
-                    'beneficiaryBankAcronym'        => $data[0]['BeneficiaryBankAcronym'] ?? '',
-                    'beneficiaryBankAccount_RefID'  => $data[0]['BeneficiaryBankAccount_RefID'] ?? '',
-                    'beneficiaryBankAccountNumber'  => $data[0]['BeneficiaryBankAccountNumber'] ?? '',
-                    'beneficiaryBankAccountName'    => $data[0]['BeneficiaryBankAccountName'] ?? '',
-                    'fileID'                        => $data[0]['Log_FileUpload_Pointer_RefID'] ?? null,
-                    'remark'                        => $data[0]['Remarks_Header'] ?? '',
+                'header' => [
+                    'sys_RefID' => $data[0]['Sys_ID_Header'] ?? '',
+                    'combinedBudget_RefID' => $data[0]['CombinedBudget_RefID'] ?? '',
+                    'combinedBudgetCode' => $data[0]['CombinedBudgetCode'] ?? '',
+                    'combinedBudgetName' => $data[0]['CombinedBudgetName'] ?? '',
+                    'combinedBudgetSection_RefID' => $data[0]['CombinedBudgetSection_RefID'] ?? '',
+                    'combinedBudgetSectionCode' => $data[0]['CombinedBudgetSectionCode'] ?? '',
+                    'combinedBudgetSectionName' => $data[0]['CombinedBudgetSectionName'] ?? '',
+                    'customer_RefID' => $data[0]['Requester_RefID'] ?? '',
+                    'customerCode' => $data[0]['RequesterCode'] ?? '',
+                    'customerName' => $data[0]['RequesterName'] ?? '',
+                    'beneficiary_RefID' => $data[0]['Beneficiary_RefID'] ?? '',
+                    'beneficiaryName' => $data[0]['BeneficiaryName'] ?? '',
+                    'beneficiaryPosition' => $data[0]['BeneficiaryCode'] ?? '',
+                    'beneficiaryBank_RefID' => $data[0]['BeneficiaryBank_RefID'] ?? '',
+                    'beneficiaryBankName' => $data[0]['BeneficiaryBankName'] ?? '',
+                    'beneficiaryBankAcronym' => $data[0]['BeneficiaryBankAcronym'] ?? '',
+                    'beneficiaryBankAccount_RefID' => $data[0]['BeneficiaryBankAccount_RefID'] ?? '',
+                    'beneficiaryBankAccountNumber' => $data[0]['BeneficiaryBankAccountNumber'] ?? '',
+                    'beneficiaryBankAccountName' => $data[0]['BeneficiaryBankAccountName'] ?? '',
+                    'fileID' => $data[0]['Log_FileUpload_Pointer_RefID'] ?? null,
+                    'remark' => $data[0]['Remarks_Header'] ?? '',
                 ],
-                'detail'                            => $data
+                'detail' => $data
             ];
-            
+
             return view('Process.Reimbursement.Transactions.RevisionReimbursement', $compact);
         } catch (\Throwable $th) {
             Log::error("RevisionReimbursement Function Error: " . $th->getMessage());
@@ -166,8 +166,8 @@ class ReimbursementController extends Controller
             }
 
             $compact = [
-                "documentNumber"    => $response['data'][0]['businessDocument']['documentNumber'],
-                "status"            => $responseWorkflow['metadata']['HTTPStatusCode'],
+                "documentNumber" => $response['data'][0]['businessDocument']['documentNumber'],
+                "status" => $responseWorkflow['metadata']['HTTPStatusCode'],
             ];
 
             return response()->json($compact);
@@ -179,41 +179,51 @@ class ReimbursementController extends Controller
 
     public function ReportReimbursementSummary(Request $request)
     {
-        return view('Process.Reimbursement.Reports.ReportReimbursementSummary');
+        $documentTypeRefID = $this->GetBusinessDocumentsTypeFromRedis('Reimbursement Form');
+        $sessionOrganizationalDepartmentName = Session::get('SessionOrganizationalDepartmentName');
+        $sessionOrganizationalJobPositionName = Session::get('SessionOrganizationalJobPositionName');
+
+        $compact = [
+            'documentTypeRefID' => $documentTypeRefID,
+            'sessionOrganizationalDepartmentName' => $sessionOrganizationalDepartmentName,
+            'sessionOrganizationalJobPositionName' => $sessionOrganizationalJobPositionName
+        ];
+
+        return view('Process.Reimbursement.Reports.ReportReimbursementSummary', $compact);
     }
 
     public function ReportReimbursementSummaryStore(Request $request)
     {
         try {
-            $date           = $request->remDate;
-            $budget         = [
-                "id"        => $request->budget_id,
-                "code"      => $request->budget_code,
+            $date = $request->remDate;
+            $budget = [
+                "id" => $request->budget_id,
+                "code" => $request->budget_code,
             ];
-            $customerID     = $request->customer_id;
+            $customerID = $request->customer_id;
 
             $response = $this->reimbursementService->getReimbursementSummary(
-                $budget['code'], 
+                $budget['code'],
                 $customerID,
                 $date
             );
-            
+
             if ($response['metadata']['HTTPStatusCode'] !== 200) {
                 throw new \Exception('Failed to fetch Reimbursement Summary Report');
             }
 
             $compact = [
-                'status'    => $response['metadata']['HTTPStatusCode'],
-                'data'      => $response['data']['data']
+                'status' => $response['metadata']['HTTPStatusCode'],
+                'data' => $response['data']['data']
             ];
 
             return response()->json($compact);
         } catch (\Throwable $th) {
             Log::error("Report Reimbursement Summary Store Function Error:" . $th->getMessage());
-            
+
             $compact = [
-                'status'    => 500,
-                'message'   => $th->getMessage()
+                'status' => 500,
+                'message' => $th->getMessage()
             ];
 
             return response()->json($compact);
@@ -223,26 +233,26 @@ class ReimbursementController extends Controller
     public function PrintExportReportReimbursementSummary(Request $request)
     {
         try {
-            $type                     = $request->printType;
-            $budgetName               = $request->budgetName;
-            $customerName             = $request->customerName;
-            $remDate                  = $request->remDate;
+            $type = $request->printType;
+            $budgetName = $request->budgetName;
+            $customerName = $request->customerName;
+            $remDate = $request->remDate;
             $dataReimbursementSummary = json_decode($request->dataReport, true);
 
             if ($dataReimbursementSummary) {
                 if ($type === "PDF") {
                     $pdf = PDF::loadView('Process.Reimbursement.Reports.ReportReimbursementSummary_pdf', [
-                        'dataRem'       => $dataReimbursementSummary, 
-                        'budgetName'    => $budgetName,
-                        'customerName'  => $customerName,
-                        'remDate'       => $remDate
-                        ])->setPaper('a4', 'landscape');
+                        'dataRem' => $dataReimbursementSummary,
+                        'budgetName' => $budgetName,
+                        'customerName' => $customerName,
+                        'remDate' => $remDate
+                    ])->setPaper('a4', 'landscape');
 
                     $pdf->output();
-                    $dom_pdf    = $pdf->getDomPDF();
-                    $canvas     = $dom_pdf ->get_canvas();
-                    $width      = $canvas->get_width();
-                    $height     = $canvas->get_height();
+                    $dom_pdf = $pdf->getDomPDF();
+                    $canvas = $dom_pdf->get_canvas();
+                    $width = $canvas->get_width();
+                    $height = $canvas->get_height();
                     $canvas->page_text($width - 88, $height - 35, "Page {PAGE_NUM} of {PAGE_COUNT}", null, 10, array(0, 0, 0));
                     $canvas->page_text(34, $height - 35, "Print by " . $request->session()->get("SessionLoginName"), null, 10, array(0, 0, 0));
 
