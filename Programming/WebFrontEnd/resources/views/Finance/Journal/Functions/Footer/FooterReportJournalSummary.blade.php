2ff0b2390b04bf2ea88fff0f7150ed39af5a5afa
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-04-08 14:50:42 +0700

    feat(report): add validate budget, and date range in cash & bank summary

--- Programming/WebFrontEnd/resources/views/Finance/Journal/Functions/Footer/FooterReportJournalSummary.blade.php
+++ Programming/WebFrontEnd/resources/views/Finance/Journal/Functions/Footer/FooterReportJournalSummary.blade.php
@@ -1,28 +1,44 @@
 <script>
-    let isFromTo        = false;
-    let data            = [];
-    const startLimit    = document.getElementById("start_limit");
-    const endLimit      = document.getElementById("end_limit");
-    const totalData     = document.getElementById("total_data");
-    let currentPage     = 1;
-    let rowsPerPage     = 10;
-    let filteredData    = [...data];
-    let sortColumn      = null;
-    let sortOrder       = 'asc'; 
-    let totalPages      = Math.ceil(data.length / rowsPerPage);
+    let isFromTo = false;
+    let data = [];
+    const documentTypeID = document.getElementById("documentTypeRefID");
+    const organizationalDepartmentName = document.getElementById("organizationalDepartmentName"); // Finance & Accounting
+    const organizationalJobPositionName = document.getElementById("organizationalJobPositionName"); // General Manager
+    const startLimit = document.getElementById("start_limit");
+    const endLimit = document.getElementById("end_limit");
+    const totalData = document.getElementById("total_data");
+    const budgetID = document.getElementById("budget_id");
+    const transTypeID = document.getElementById("trans_type_id");
+    const tranoID = document.getElementById("trano_id");
+    const bankID = document.getElementById("bank_id");
+    const fromToID = document.getElementById("from_to_id");
+    const journalDate = document.getElementById("journal_date_range");
+    let currentPage = 1;
+    let rowsPerPage = 10;
+    let filteredData = [...data];
+    let sortColumn = null;
+    let sortOrder = 'asc';
+    let totalPages = Math.ceil(data.length / rowsPerPage);
+
+    function selectBudget(id, code, name) {
+        $("#budget_id").val(id);
+        $("#budget_code").val(code);
+        $("#budget_name").val(`${code} - ${name}`);
+        $("#budget_name").css('background-color', '#e9ecef');
+    }
 
     function pickBanksAccount(value) {
         isFromTo = value;
     }
 
     function resetForm() {
-        isFromTo        = false;
-        data            = [];
-        currentPage     = 1;
-        rowsPerPage     = 10;
-        filteredData    = [...data];
-        sortColumn      = null;
-        sortOrder       = 'asc';
+        isFromTo = false;
+        data = [];
+        currentPage = 1;
+        rowsPerPage = 10;
+        filteredData = [...data];
+        sortColumn = null;
+        sortOrder = 'asc';
 
         $("#budget_name").css('background-color', '#fff');
         $(`#budget_name`).val("");
@@ -47,7 +63,7 @@
         $(`#from_to_name`).val("");
         $(`#from_to_id`).val("");
         $(`#from_to_code`).val("");
-        
+
         $("#journal_date_range").css('background-color', '#fff');
         $(`#journal_date_range`).val("");
     }
@@ -66,7 +82,7 @@
             url: '{!! route("Journal.ReportPaymentJournalStore") !!}',
             data: {},
             dataType: 'json',
-            success: function(response) {
+            success: function (response) {
                 if (response.status === 200 && response.data[0]) {
                     data = response.data;
 
@@ -86,7 +102,7 @@
 
                 HideLoading();
             },
-            error: function(xhr, status, error) {
+            error: function (xhr, status, error) {
                 HideLoading();
                 ErrorNotif("An error occurred while processing the received data. Please try again later.");
                 console.log('xhr, status, error', xhr, status, error);
@@ -242,10 +258,10 @@
         const pageNumbersContainer = document.querySelector('#pageNumbers');
         const prevButton = document.querySelector('#prevPage');
         const nextButton = document.querySelector('#nextPage');
-        
+
         // Clear previous pagination
         pageNumbersContainer.innerHTML = '';
-        
+
         // Calculate the range of entries to be displayed (start and end)
         const startLimit = (currentPage - 1) * rowsPerPage + 1;
         const endLimit = Math.min(currentPage * rowsPerPage, data.length);
@@ -259,7 +275,7 @@
             pageNumber.textContent = i;
             pageNumber.style.padding = '.5em 1em';
             pageNumber.style.cursor = 'pointer';
-            
+
             // Set active page style
             if (i == currentPage) {
                 pageNumber.style.background = 'linear-gradient(to bottom, rgba(230, 230, 230, 0.1) 0%, rgba(0, 0, 0, 0.1) 100%)';
@@ -326,6 +342,96 @@
         renderPagination();
     }
 
+    function validateShowButton() {
+        const isBudgetIDNotEmpty = budgetID.value.trim() !== '';
+        const isTransTypeIDNotEmpty = transTypeID.value.trim() !== '';
+        const isTranoIDNotEmpty = tranoID.value.trim() !== '';
+        const isBankIDNotEmpty = bankID.value.trim() !== '';
+        const isFromToIDNotEmpty = fromToID.value.trim() !== '';
+        const isJournalDateNotEmpty = journalDate.value.trim() !== '';
+
+        const isAuthorizedRole = Utils.isUserAuthorizedForReport();
+
+        if (
+            isBudgetIDNotEmpty ||
+            isTransTypeIDNotEmpty ||
+            isTranoIDNotEmpty ||
+            isBankIDNotEmpty ||
+            isFromToIDNotEmpty ||
+            isJournalDateNotEmpty
+        ) {
+            ErrorHandler.hideErrorInputMessage("#budget_name", "#budgetMessage");
+            ErrorHandler.hideErrorInputMessage("#trans_type_name", "#transactionTypeMessage");
+            ErrorHandler.hideErrorInputMessage("#trano_name", "#tranoMessage");
+            ErrorHandler.hideErrorInputMessage("#bank_name", "#bankMessage");
+            ErrorHandler.hideErrorInputMessage("#from_to_name", "#fromToMessage");
+            ErrorHandler.hideErrorInputMessage("#journal_date_range", "#dateRangeMessage");
+
+            if (isBudgetIDNotEmpty || isAuthorizedRole) {
+                getDataReport();
+            } else {
+                ErrorHandler.showErrorInputMessage("#budget_name", "#budgetMessage");
+            }
+        } else {
+            ErrorHandler.showErrorInputMessage("#budget_name", "#budgetMessage");
+            ErrorHandler.showErrorInputMessage("#trans_type_name", "#transactionTypeMessage");
+            ErrorHandler.showErrorInputMessage("#trano_name", "#tranoMessage");
+            ErrorHandler.showErrorInputMessage("#bank_name", "#bankMessage");
+            ErrorHandler.showErrorInputMessage("#from_to_name", "#fromToMessage");
+            ErrorHandler.showErrorInputMessage("#journal_date_range", "#dateRangeMessage");
+        }
+    }
+
+    function validateExportButton() {
+        if (dataReport.length > 0) {
+            exportDataReport();
+        } else {
+            ErrorNotif("No data available to export. Please display the data first.");
+        }
+    }
+
+    function getWorkflow(combinedBudgetID, combinedBudgetCode, combinedBudgetName) {
+        $.ajax({
+            type: 'POST',
+            url: '{!! route("GetWorkflow") !!}',
+            data: {
+                businessDocumentType_RefID: documentTypeID.value,
+                combinedBudget_RefID: combinedBudgetID
+            }
+        })
+            .done(function (data, textStatus, jqXHR) {
+                console.log("Success:", data);
+
+                if (data.status == 200) {
+                    $("#budget_id").val(combinedBudgetID);
+                    $("#budget_code").val(combinedBudgetCode);
+                    $("#budget_name").val(`${combinedBudgetCode} - ${combinedBudgetName}`);
+                    $("#budget_name").css('background-color', '#e9ecef');
+
+                    getSites(combinedBudgetID);
+
+                    $("#mySitesTrigger").css('cursor', 'pointer');
+                    $("#mySitesTrigger").attr({
+                        "data-toggle": "modal",
+                        "data-target": "#mySites"
+                    });
+                } else {
+                    ErrorHandler.notifToast(
+                        'error',
+                        'You are not included in this budget',
+                        'Error!'
+                    );
+                }
+            })
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                console.error("Error:", errorThrown);
+            })
+            .always(function (jqXHR, textStatus, errorThrown) {
+                $("#loadingBudget").hide();
+                $("#iconBudget").show();
+            });
+    }
+
     // Add sorting functionality to table headers
     document.querySelectorAll('#table_summary th').forEach((header, index) => {
         header.addEventListener('click', () => {
@@ -363,24 +469,28 @@
         }
     });
 
-    $('#tableProjects').on('click', 'tbody tr', function() {
-        const sysId   = $(this).find('input[data-trigger="sys_id_project"]').val();
-        const code    = $(this).find('td:nth-child(2)').text();
-        const name    = $(this).find('td:nth-child(3)').text();
+    $('#tableProjects').on('click', 'tbody tr', function () {
+        const sysId = $(this).find('input[data-trigger="sys_id_project"]').val();
+        const code = $(this).find('td:nth-child(2)').text();
+        const name = $(this).find('td:nth-child(3)').text();
 
-        $("#budget_id").val(sysId);
-        $("#budget_code").val(code);
-        $("#budget_name").val(`${code} - ${name}`);
-        $("#budget_name").css('background-color', '#e9ecef');
+        if (Utils.isUserAuthorizedForReport()) {
+            selectBudget(sysId, code, name);
+        } else {
+            $("#loadingBudget").show();
+            $("#iconBudget").hide();
 
-        getSites(sysId);
+            getWorkflow(sysId, code, name);
+        }
+
+        ErrorHandler.hideErrorInputMessage("#budget_name", "#budgetMessage");
 
         $('#myProjects').modal('toggle');
     });
 
-    $('#tableBanksAccount').on('click', 'tbody tr', function() {
-        const sysID       = $(this).find('input[type="hidden"]').val();
-        const bankName    = $(this).find('td:nth-child(2)').text();
+    $('#tableBanksAccount').on('click', 'tbody tr', function () {
+        const sysID = $(this).find('input[type="hidden"]').val();
+        const bankName = $(this).find('td:nth-child(2)').text();
         const bankAccount = $(this).find('td:nth-child(3)').text();
         const accountName = $(this).find('td:nth-child(4)').text();
 
@@ -388,29 +498,35 @@
             $('#from_to_name').val(`(${bankName}) ${bankAccount} - ${accountName}`);
             $('#from_to_id').val(sysID);
             $('#from_to_code').val(accountName);
-            $("#from_to_name").css({"background-color":"#e9ecef", "border": "1px solid #ced4da"});
+            $("#from_to_name").css({ "background-color": "#e9ecef", "border": "1px solid #ced4da" });
+
+            ErrorHandler.hideErrorInputMessage("#from_to_name", "#fromToMessage");
         } else {
             $('#bank_name').val(`(${bankName}) ${bankAccount} - ${accountName}`);
             $('#bank_id').val(sysID);
             $('#bank_code').val(accountName);
-            $("#bank_name").css({"background-color":"#e9ecef", "border": "1px solid #ced4da"});
+            $("#bank_name").css({ "background-color": "#e9ecef", "border": "1px solid #ced4da" });
+
+            ErrorHandler.hideErrorInputMessage("#bank_name", "#bankMessage");
         }
 
         $('#myBanksAccount').modal('toggle');
     });
 
-    $('#tableGetBusinessDocumentType').on('click', 'tbody tr', function() {
+    $('#tableGetBusinessDocumentType').on('click', 'tbody tr', function () {
         const sysID = $(this).find('input[data-trigger="sys_id_business_document"]').val();
-        const name  = $(this).find('td:nth-child(2)').text();
+        const name = $(this).find('td:nth-child(2)').text();
 
         $('#trans_type_name').val(name);
         $('#trans_type_id').val(sysID);
-        $("#trans_type_name").css({"background-color":"#e9ecef", "border": "1px solid #ced4da"});
+        $("#trans_type_name").css({ "background-color": "#e9ecef", "border": "1px solid #ced4da" });
+
+        ErrorHandler.hideErrorInputMessage("#trans_type_name", "#transactionTypeMessage");
 
         $('#myBusinessDocumentType').modal('toggle');
     });
 
-    $(window).one('load', function() {
+    $(window).one('load', function () {
         getBanksAccount("", "");
         getBusinessDocumentType();
         renderPage();
@@ -424,12 +540,13 @@
             }
         });
 
-        $('#journal_date_range').on('apply.daterangepicker', function(ev, picker) {
+        $('#journal_date_range').on('apply.daterangepicker', function (ev, picker) {
             $("#journal_date_range").css('background-color', '#e9ecef');
             $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+            ErrorHandler.hideErrorInputMessage("#journal_date_range", "#dateRangeMessage");
         });
 
-        $('#journal_date_range').on('cancel.daterangepicker', function(ev, picker) {
+        $('#journal_date_range').on('cancel.daterangepicker', function (ev, picker) {
             $("#journal_date_range").css('background-color', '#fff');
             $(this).val('');
         });
--- Programming/WebFrontEnd/resources/views/Finance/Journal/Functions/Header/HeaderReportJournalSummary.blade.php
+++ Programming/WebFrontEnd/resources/views/Finance/Journal/Functions/Header/HeaderReportJournalSummary.blade.php
@@ -1,134 +1,191 @@
 <div class="col-sm-12 col-md-12 col-lg-3">
     <!-- BUDGET -->
-    <div class="row p-0 align-items-center" style="margin-bottom: 1rem;">
+    <div class="row p-0 align-items-center">
         <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0 text-bold">Budget</label>
         <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0 justify-content-sm-end justify-content-md-end">
             <div>
-                <span id="myProjectsTrigger" class="input-group-text form-control" data-toggle="modal" data-target="#myProjects" style="border-radius:0;cursor:pointer;">
-                    <i class="fas fa-gift"></i>
+                <span id="myProjectsTrigger" class="input-group-text form-control" data-toggle="modal"
+                    data-target="#myProjects" style="border-radius:0;cursor:pointer;">
+                    <i id="iconBudget" class="fas fa-gift"></i>
+
+                    <div id="loadingBudget" class="spinner-border spinner-border-sm" role="status"
+                        style="display: none;">
+                        <span class="sr-only">Loading...</span>
+                    </div>
                 </span>
             </div>
             <div>
-                <input type="text" id="budget_name" class="form-control" style="border-radius:0;background-color:white;" readonly />
+                <input type="text" id="budget_name" class="form-control" style="border-radius:0;background-color:white;"
+                    readonly />
                 <input type="hidden" id="budget_id" class="form-control" style="border-radius:0;" name="budget_id" />
-                <input type="hidden" id="budget_code" class="form-control" style="border-radius:0;" name="budget_code" />
+                <input type="hidden" id="budget_code" class="form-control" style="border-radius:0;"
+                    name="budget_code" />
             </div>
+        </div>
+    </div>
+    <div class="row" id="budgetMessage" style="margin-top: .3rem;display: none;">
+        <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0"></label>
+        <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0 justify-content-sm-end justify-content-md-end text-red">
+            Budget cannot be empty.
         </div>
     </div>
 
     <!-- DATE -->
-    <div class="row p-0 align-items-center">
+    <div class="row p-0 align-items-center" style="margin-top: 1rem;">
         <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0 text-bold">Date</label>
         <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0 justify-content-sm-end justify-content-md-end">
             <div>
                 <div class="input-group" id="journal_date_range_container">
-                    <div class="input-group-prepend" style="margin-right: 0px; width: 27.78px;cursor: pointer;height: 21.8px;">
+                    <div class="input-group-prepend"
+                        style="margin-right: 0px; width: 27.78px;cursor: pointer;height: 21.8px;">
                         <span class="input-group-text" id="journal_date_range_container_icon" style="border-radius: 0;">
                             <i class="far fa-calendar-alt" style="width: 13px; height: 13px;"></i>
                         </span>
                     </div>
-                    <input readonly type="text" class="form-control" style="height: 21.8px;border-radius:0;background-color:white;" id="journal_date_range" name="journal_date_range" />
+                    <input readonly type="text" class="form-control"
+                        style="height: 21.8px;border-radius:0;background-color:white;" id="journal_date_range"
+                        name="journal_date_range" />
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="row" id="dateRangeMessage" style="margin-top: .3rem;display: none;">
+        <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0"></label>
+        <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0 justify-content-sm-end justify-content-md-end text-red">
+            Date Range cannot be empty.
         </div>
     </div>
 </div>
 
 <div class="col-sm-12 col-md-12 col-lg-3">
     <!-- TRANSACTION TYPE -->
-    <div class="row p-0 align-items-center" style="margin-bottom: 1rem;">
+    <div class="row p-0 align-items-center">
         <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0 text-bold">Trans. Type</label>
         <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0 justify-content-sm-end justify-content-md-end">
             <div>
-                <span id="myTranoTrigger" class="input-group-text form-control" data-toggle="modal" data-target="#myBusinessDocumentType" style="border-radius:0;cursor:pointer;">
+                <span id="myTranoTrigger" class="input-group-text form-control" data-toggle="modal"
+                    data-target="#myBusinessDocumentType" style="border-radius:0;cursor:pointer;">
                     <i class="fas fa-gift"></i>
                 </span>
             </div>
             <div>
-                <input type="text" id="trans_type_name" class="form-control" style="border-radius:0;background-color:white;" readonly />
-                <input type="hidden" id="trans_type_id" class="form-control" style="border-radius:0;" name="trans_type_id" />
+                <input type="text" id="trans_type_name" class="form-control"
+                    style="border-radius:0;background-color:white;" readonly />
+                <input type="hidden" id="trans_type_id" class="form-control" style="border-radius:0;"
+                    name="trans_type_id" />
             </div>
+        </div>
+    </div>
+    <div class="row" id="transactionTypeMessage" style="margin-top: .3rem;display: none;">
+        <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0"></label>
+        <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0 justify-content-sm-end justify-content-md-end text-red">
+            Transaction Type cannot be empty.
         </div>
     </div>
 
     <!-- TRANO -->
-    <div class="row p-0 align-items-center">
+    <div class="row p-0 align-items-center" style="margin-top: 1rem;">
         <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0 text-bold">Trano</label>
         <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0 justify-content-sm-end justify-content-md-end">
             <div>
-                <span id="myTranoTrigger" class="input-group-text form-control" data-toggle="modal" data-target="#" style="border-radius:0;cursor:pointer;">
+                <span id="myTranoTrigger" class="input-group-text form-control" data-toggle="modal" data-target="#"
+                    style="border-radius:0;cursor:pointer;">
                     <i class="fas fa-gift"></i>
                 </span>
             </div>
             <div>
-                <input type="text" id="trano_name" class="form-control" style="border-radius:0;background-color:white;" readonly />
+                <input type="text" id="trano_name" class="form-control" style="border-radius:0;background-color:white;"
+                    readonly />
                 <input type="hidden" id="trano_id" class="form-control" style="border-radius:0;" name="trano_id" />
                 <input type="hidden" id="trano_code" class="form-control" style="border-radius:0;" name="trano_code" />
             </div>
+        </div>
+    </div>
+    <div class="row" id="tranoMessage" style="margin-top: .3rem;display: none;">
+        <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0"></label>
+        <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0 justify-content-sm-end justify-content-md-end text-red">
+            Trano cannot be empty.
         </div>
     </div>
 </div>
 
 <div class="col-sm-12 col-md-12 col-lg-3">
     <!-- BANK -->
-    <div class="row p-0 align-items-center" style="margin-bottom: 1rem;">
+    <div class="row p-0 align-items-center">
         <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0 text-bold">Bank</label>
         <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0 justify-content-sm-end justify-content-md-end">
             <div onclick="pickBanksAccount(false)">
-                <span id="myBankTrigger" class="input-group-text form-control" data-toggle="modal" data-target="#myBanksAccount" style="border-radius:0;cursor:pointer;">
+                <span id="myBankTrigger" class="input-group-text form-control" data-toggle="modal"
+                    data-target="#myBanksAccount" style="border-radius:0;cursor:pointer;">
                     <i class="fas fa-gift"></i>
                 </span>
             </div>
             <div>
-                <input type="text" id="bank_name" class="form-control" style="border-radius:0;background-color:white;" readonly />
+                <input type="text" id="bank_name" class="form-control" style="border-radius:0;background-color:white;"
+                    readonly />
                 <input type="hidden" id="bank_id" class="form-control" style="border-radius:0;" name="bank_id" />
                 <input type="hidden" id="bank_code" class="form-control" style="border-radius:0;" name="bank_code" />
             </div>
         </div>
     </div>
+    <div class="row" id="bankMessage" style="margin-top: .3rem;display: none;">
+        <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0"></label>
+        <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0 justify-content-sm-end justify-content-md-end text-red">
+            Bank cannot be empty.
+        </div>
+    </div>
 
     <!-- FROM/TO -->
-    <div class="row p-0 align-items-center">
+    <div class="row p-0 align-items-center" style="margin-top: 1rem;">
         <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0 text-bold">From/To</label>
         <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0 justify-content-sm-end justify-content-md-end">
             <div onclick="pickBanksAccount(true)">
-                <span id="myFromToTrigger" class="input-group-text form-control" data-toggle="modal" data-target="#myBanksAccount" style="border-radius:0;cursor:pointer;">
+                <span id="myFromToTrigger" class="input-group-text form-control" data-toggle="modal"
+                    data-target="#myBanksAccount" style="border-radius:0;cursor:pointer;">
                     <i class="fas fa-gift"></i>
                 </span>
             </div>
             <div>
-                <input type="text" id="from_to_name" class="form-control" style="border-radius:0;background-color:white;" readonly />
+                <input type="text" id="from_to_name" class="form-control"
+                    style="border-radius:0;background-color:white;" readonly />
                 <input type="hidden" id="from_to_id" class="form-control" style="border-radius:0;" name="from_to_id" />
-                <input type="hidden" id="from_to_code" class="form-control" style="border-radius:0;" name="from_to_code" />
+                <input type="hidden" id="from_to_code" class="form-control" style="border-radius:0;"
+                    name="from_to_code" />
             </div>
+        </div>
+    </div>
+    <div class="row" id="fromToMessage" style="margin-top: .3rem;display: none;">
+        <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0"></label>
+        <div class="col-sm-9 col-md-8 col-lg-7 d-flex p-0 justify-content-sm-end justify-content-md-end text-red">
+            From/To cannot be empty.
         </div>
     </div>
 </div>
 
-<div class="col-sm-12 col-md-12 col-lg-3 d-flex flex-column flex-column-reverse">
-    <!-- SUBMIT -->
-    <div class="align-items-center justify-content-sm-end justify-content-md-end justify-content-lg-start row p-0" style="gap: 0.5rem;">
-        <button type="button" class="btn btn-default btn-sm" style="margin-top: -5px;" onclick="getDataReport()">
-            <img src="{{ asset('AdminLTE-master/dist/img/backwards.png') }}" width="12" alt="show" title="Show">
-            Show
-        </button>
-        <button type="button" class="btn btn-secondary btn-sm" style="margin-top: -5px;" onclick="resetForm()">
-            Reset
-        </button>
-    </div>
-    
+<div class="col-sm-12 col-md-12 col-lg-3">
     <!-- EXPORT -->
-    <div class="align-items-center justify-content-sm-end justify-content-md-end justify-content-lg-start row align-items-center p-0" style="margin-bottom: 1rem; gap: 0.5rem;">
+    <div class="row align-items-center" style="margin-bottom: 1rem; gap: 0.5rem;">
         <div>
             <select name="print_type" id="print_type" class="form-control">
                 <option value="PDF">Export PDF</option>
                 <option value="EXCEL">Export Excel</option>
             </select>
         </div>
-        <button type="button" class="btn btn-default btn-sm" onclick="exportDataReport()">
+        <button type="button" class="btn btn-default btn-sm" onclick="validateExportButton()">
             <span>
                 <img src="{{ asset('AdminLTE-master/dist/img/printer.png') }}" width="17" alt="" />
             </span>
+        </button>
+    </div>
+
+    <!-- SUBMIT -->
+    <div class="row" style="gap: 0.5rem;">
+        <button type="button" class="btn btn-default btn-sm" onclick="validateShowButton()" style="margin-top: -5px;">
+            <img src="{{ asset('AdminLTE-master/dist/img/backwards.png') }}" width="12" alt="show" title="Show">
+            Show
+        </button>
+        <button type="button" class="btn btn-secondary btn-sm" onclick="resetForm()" style="margin-top: -5px;">
+            Reset
         </button>
     </div>
 </div>
