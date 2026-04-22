1f278b492bddbf46cd00e82bbc6570711e90e5b0
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-04-22 14:11:47 +0700

    feat(workflow): change name

--- Programming/WebFrontEnd/resources/views/Admin/Workflow/Transactions/CreateWorkflowRoute.blade.php
+++ Programming/WebFrontEnd/resources/views/Admin/Workflow/Transactions/CreateWorkflowRoute.blade.php
@@ -13,7 +13,7 @@
                 <div class="row mb-1" style="background-color:#4B586A;">
                     <div class="col-sm-6" style="height:30px;">
                         <label style="font-size:15px;position:relative;top:7px;color:white;">
-                            Workflow Route
+                            Workflow
                         </label>
                     </div>
                 </div>
