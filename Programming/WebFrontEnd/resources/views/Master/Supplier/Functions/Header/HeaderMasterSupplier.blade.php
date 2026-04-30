3c8482cfa36d1b99480188a74e580fc1c3339f34
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-04-30 08:56:54 +0700

    feat(supplier): customize supplier type

--- Programming/WebFrontEnd/resources/views/Master/Supplier/Functions/Header/HeaderMasterSupplier.blade.php
+++ Programming/WebFrontEnd/resources/views/Master/Supplier/Functions/Header/HeaderMasterSupplier.blade.php
@@ -154,8 +154,31 @@
 
         <!-- RIGHT -->
         <div class="col-md-12 col-lg-5">
-            <!-- CONTACT PERSON -->
+            <!-- LEGAL ENTITY -->
             <div class="row">
+                <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">Legal
+                    Entity</label>
+                <div class="col-5">
+                    <select class="form-control" id="legal_entity" name="legal_entity_value" style="border-radius:0;"
+                        type="text">
+                        <option value="" disabled selected>Select a Legal Entity</option>
+                        <option value="PT (Perseroan Terbatas)">PT (Perseroan Terbatas)</option>
+                        <option value="CV (Commanditaire Vennootschap)">CV (Commanditaire Vennootschap)</option>
+                        <option value="Koperasi">Koperasi</option>
+                        <option value="Yayasan">Yayasan</option>
+                        <option value="Firma (Fa)">Firma (Fa)</option>
+                        <option value="Usaha Perseorangan">Usaha Perseorangan</option>
+                        <option value="Bentuk Lain Khusus (BUT)">Bentuk Lain Khusus (BUT)</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row" id="legalEntityMessage" style="margin-top: .3rem;display: none;">
+                <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0"></label>
+                <div class="col text-red" id="legalEntityMessageText"></div>
+            </div>
+
+            <!-- CONTACT PERSON -->
+            <div class="row" style="margin-top: 1rem;">
                 <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">Contact
                     Person</label>
                 <div class="col-5 d-flex">
