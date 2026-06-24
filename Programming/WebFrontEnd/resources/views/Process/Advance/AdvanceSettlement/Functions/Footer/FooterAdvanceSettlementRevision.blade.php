3d70a0049be4f79eb170ec77c960d4e20f636f27
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-06-24 11:40:27 +0700

    feat: customize api checking workflow in advance settlement revision

--- Programming/WebFrontEnd/resources/views/Process/Advance/AdvanceSettlement/Functions/Footer/FooterAdvanceSettlementRevision.blade.php
+++ Programming/WebFrontEnd/resources/views/Process/Advance/AdvanceSettlement/Functions/Footer/FooterAdvanceSettlementRevision.blade.php
@@ -674,18 +674,20 @@
                 businessDocumentType_RefID: documentTypeID.value,
                 combinedBudget_RefID: projectID.value
             },
-            url: '{!! route("GetWorkflow") !!}',
+            url: '{!! route("Workflow.UserAllowedToSubmit") !!}',
             success: function (response) {
-                if (response.status === 200) {
-                    totalNextApprover = response.data[0].nextApproverPath.length;
-                    dataWorkflow.workFlowPathRefID = response.data[0].sys_ID;
-                    dataWorkflow.approverEntityRefID = response.data[0].submitterEntity_RefID;
+                if (response.status === 200 && response.data[0].signAccess) {
+                    // totalNextApprover = response.data[0].nextApproverPath.length;
+                    // dataWorkflow.workFlowPathRefID = response.data[0].sys_ID;
+                    // dataWorkflow.approverEntityRefID = response.data[0].submitterEntity_RefID;
 
-                    getWorkflows(response.data[0].nextApproverPath);
+                    // getWorkflows(response.data[0].nextApproverPath);
                 } else {
                     $("#button_submit").prop("disabled", true);
 
-                    Swal.fire("Error", "You don't have access", "error");
+                    Swal.fire("Error", "You don't have access", "error").then((res) => {
+                        Utils.cancelForm('{{ route('AdvanceSettlement.index', ['var' => 1]) }}');
+                    });
                 }
             },
             error: function (jqXHR, textStatus, errorThrown) {
@@ -730,7 +732,7 @@
         getModalAdvanceSettlement();
     });
 
-    $(window).on('load', function () {
+    $(document).ready(function () {
         getWorkflow();
         getDetailAdvanceSettlement(dataTable);
     });
