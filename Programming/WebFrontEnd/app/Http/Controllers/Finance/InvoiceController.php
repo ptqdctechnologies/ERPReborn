611ac6dde738929627546d740a73cbcbb5225ab4
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-06-25 17:24:42 +0700

    feat: integrate create invoice

--- Programming/WebFrontEnd/app/Http/Controllers/Finance/InvoiceController.php
+++ Programming/WebFrontEnd/app/Http/Controllers/Finance/InvoiceController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Finance;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall;
+use App\Helpers\ZhtHelper\System\Helper_Environment;
 use Illuminate\Support\Facades\Log;
+use App\Http\Requests\Finance\Invoice\StoreInvoice;
 
 class InvoiceController extends Controller
 {
@@ -54,38 +57,51 @@ class InvoiceController extends Controller
     }
 
     // Simpan post baru ke database
-    public function store(Request $request)
+    public function store(StoreInvoice $request)
     {
-        //
-        // 'transaction.create.finance.setSalesInvoice', 
-        // 'latest',
-        // //-----[ METADATA ]-----(  END  )-----
-        // //-----[ DATA ]-----( START )-----
-        // [
-        // "entities" => [
-        //     "documentDateTimeTZ" => '2026-06-18',
-        //     "log_FileUpload_Pointer_RefID" => 91000000000001,
-        //     "combinedBudget_RefID" => 46000000000001,
-        //     "customer_RefID" => 125000000000001,
-        //     "currency_RefID" => 62000000000001,
-        //     "vatStatus" => 1,
-        //     "vatValue" => 10,
-        //     "vatNumber" => '01.234.567.8-999.000',
-        //     "remarks" => 'My Remarks',
-        //     "additionalData" => [
-        //         "itemList" => [
-        //             "items" => [
-        //                     [
-        //                     "entities" => [
-        //                         "combinedBudgetSectionDetail_RefID" => 169000000000001,
-        //                         "value" => 15080000.00
-        //                             ]
-        //                     ]
-        //                 ]
-        //             ]
-        //         ]
-        //     ]
-        // ]
+        try {
+            $token = Session::get('SessionLogin');
+
+            $response = Helper_APICall::setCallAPIGateway(
+                Helper_Environment::getUserSessionID_System(),
+                $token,
+                'transaction.create.finance.setSalesInvoice',
+                'latest',
+                [
+                    "entities" => [
+                        "documentDateTimeTZ" => date('Y-m-d'),
+                        "log_FileUpload_Pointer_RefID" => null,
+                        "combinedBudget_RefID" => (int) $request->budget_id,
+                        "customer_RefID" => null,
+                        "currency_RefID" => null,
+                        "vatStatus" => null,
+                        "vatValue" => $request->vatValue,
+                        "vatNumber" => null,
+                        "remarks" => $request->remarks,
+                        "additionalData" => [
+                            "itemList" => [
+                                "items" => $request->invoiceDetails
+                            ]
+                        ]
+                    ]
+                ]
+            );
+
+            if ($response['metadata']['HTTPStatusCode'] !== 200) {
+                throw new \Exception('Failed to fetch Store Invoice => ' . $response['data']['message']);
+            }
+
+            $compact = [
+                "documentNumber" => $response['data']['businessDocument']['documentNumber'],
+                "status" => $response['metadata']['HTTPStatusCode']
+            ];
+
+            return response()->json($compact);
+        } catch (\Throwable $th) {
+            Log::error("Store Invoice Function Error: " . $th->getMessage());
+
+            return response()->json(["status" => 500]);
+        }
     }
 
     // Tampilkan satu post berdasarkan ID
