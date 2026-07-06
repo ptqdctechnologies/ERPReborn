a9a752abb055f9ad8dd64c0c19bc47df6c850725
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-07-06 16:30:34 +0700

    feat: added barcode in pdf po

--- Programming/WebFrontEnd/config/app.php
+++ Programming/WebFrontEnd/config/app.php
@@ -234,7 +234,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
-
+        'DNS1D' => Milon\Barcode\Facades\DNS1DFacade::class,
+        'DNS2D' => Milon\Barcode\Facades\DNS2DFacade::class,
     ],
 
 ];
--- Programming/WebFrontEnd/resources/views/Dashboard/index.blade.php
+++ Programming/WebFrontEnd/resources/views/Dashboard/index.blade.php
@@ -16,6 +16,28 @@
           <a href="#" class="d-block" style="font-size:15px;position:relative;top:7px;">Project Dashboard</a>
         </div>
       </div>
+
+      <div>
+        <h2>1. Barcode 1 Dimensi (Code 128)</h2>
+        <div>
+            {!! DNS1D::getBarcodeHTML("ID-9988776655", 'C128', 2, 50) !!}
+        </div>
+        <p>ID-9988776655</p>
+
+        <hr>
+
+        <h2>2. QR Code (2 Dimensi)</h2>
+        <div>
+            {!! DNS2D::getBarcodeHTML("Ini adalah isi dari QR Code", 'QRCODE', 4, 4) !!}
+        </div>
+
+        <hr>
+
+        <h2>3. Barcode dalam format Base64 PNG (Bentuk tag IMG)</h2>
+        <div>
+            <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG('QDC2026', 'C39') }}" alt="barcode" />
+        </div>
+      </div>
     </div>
   </section>
 </div>
