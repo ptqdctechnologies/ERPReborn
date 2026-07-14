64711a110807bc0368a129d4f50f7a4db9430165
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-07-14 17:24:14 +0700

    style: redesign pop-up delivery order revision

--- Programming/WebFrontEnd/resources/views/Inventory/DeliveryOrder/Functions/PopUp/PopUpDoRevision.blade.php
+++ Programming/WebFrontEnd/resources/views/Inventory/DeliveryOrder/Functions/PopUp/PopUpDoRevision.blade.php
@@ -1,76 +1,71 @@
-<div id="myPopUpDoRevision" class="modal fade" role="dialog" aria-hidden="true"
-    style="margin-top: 180px;margin-left:6px;">
-    <div class="modal-dialog modal-dialog-scrollable" role="document">
-        <div class="modal-content" style="width:90%;">
+<div class="modal fade" id="myPopUpDoRevision" tabindex="-1" aria-labelledby="myPopUpDoRevisionLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
             <div class="modal-header">
-                <div class="modal-body">
-                    <span style="font-size: 15px;position:relative;left:25%;font-weight:bold;">DELIVERY ORDER
-                        REVISION</span><br><br><br>
-                    <div class="card" style="margin-left: 8%;">
-                        <div class="card-body">
-                            <div class="form-group">
-                                <table>
-                                    <tr>
-                                        <td><label>Revision Number&nbsp;</label></td>
-                                        <td>
-                                            <div class="input-group">
-                                                <form id="edit_form"
-                                                    action="{{ route('DeliveryOrder.RevisionDeliveryOrderIndex') }}"
-                                                    method="post">
-                                                    @csrf
-                                                    <input id="do_RefID" style="border-radius:0;" name="do_RefID"
-                                                        type="hidden" class="form-control" hidden>
-                                                </form>
-
-                                                <div class="input-group-append">
-                                                    <span id="do_number_icon" class="input-group-text form-control"
-                                                        style="border-radius:0;cursor:pointer;">
-                                                        <a data-toggle="modal" data-target="#myDeliveryOrder">
-                                                            <img src="{{ asset('AdminLTE-master/dist/img/box.png') }}"
-                                                                width="13" alt="">
-                                                        </a>
-                                                    </span>
-                                                </div>
-                                                <input id="do_number" style="border-radius:0;" name="do_number"
-                                                    class="form-control" readonly>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                </table>
+                <h5 class="modal-title" id="myPopUpDoRevisionLabel"
+                    style="font-size: 15px; font-weight:bold; text-align: center;">
+                    DELIVERY ORDER REVISION
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body d-flex justify-content-center">
+                <form id="editForm" method="POST" action="{{ route('AdvanceRequest.RevisionAdvanceIndex') }}">
+                    @csrf
+                    <div class="card mb-0" style="width: fit-content;">
+                        <div class="card-body d-flex align-items-center justify-content-center" style="gap: 1rem;">
+                            <label class="p-0 m-0">Revision Number</label>
+                            <div class="form-group d-flex">
+                                <div>
+                                    <span id="do_number_icon" class="input-group-text form-control" data-toggle="modal"
+                                        data-target="#myDeliveryOrder" style="cursor:pointer; border-radius: 0;">
+                                        <i class="fas fa-gift"></i>
+                                    </span>
+                                </div>
+                                <div>
+                                    <input id="do_number" class="form-control"
+                                        style="border-radius:0; background-color: white;" readonly />
+                                    <input id="do_RefID" class="form-control" name="do_RefID"
+                                        style="border-radius:0; background-color: white;" hidden />
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <a id="cancel_button" class="btn btn-sm" data-dismiss="modal"
-                        style="margin-left: 38%;background-color:#e9ecef;border:1px solid #ced4da;">
-                        <img src="{{ asset('AdminLTE-master/dist/img/cancel.png') }}" width="13" alt="" title="Cancel">
-                        Cancel
-                    </a>
-                    <a id="edit_button" class="btn btn-sm" style="background-color:#e9ecef;border:1px solid #ced4da;">
-                        <img src="{{ asset('AdminLTE-master/dist/img/edit.png') }}" width="13" alt="" title="Edit"> Edit
-                    </a>
-                </div>
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" id="btn-cancel" class="btn btn-sm" data-dismiss="modal"
+                    style="background-color: #e9ecef; border:1px solid #ced4da;">
+                    <img src="{{ asset('AdminLTE-master/dist/img/cancel.png') }}" width="13" alt="" title="Cancel" />
+                    Cancel
+                </button>
+                <button type="button" id="btn-edit" class="btn btn-sm"
+                    style="background-color:#e9ecef;border:1px solid #ced4da;">
+                    <img src="{{ asset('AdminLTE-master/dist/img/edit.png') }}" width="13" alt="" title="Edit" />
+                    Edit
+                </button>
             </div>
         </div>
     </div>
 </div>
 
 <script>
-    $('#edit_button').on('click', function () {
-        let deliveryOrder_RefID = $('#do_RefID').val();
+    $('#btn-edit').on('click', function () {
+        const deliveryOrderRefIDs = $('#do_RefID').val();
 
-        if (deliveryOrder_RefID) {
+        if (deliveryOrderRefIDs) {
             ShowLoading();
 
-            $('#edit_form').submit();
+            $('#editForm').submit();
         } else {
             $('#do_number').focus();
             $('#do_number').css("border", "1px solid red");
-            $('#do_number_icon').css("border", "1px solid red");
         }
     });
 
-    $('#cancel_button').on('click', function () {
+    $('#btn-cancel').on('click', function () {
         $('#do_RefID').val("");
         $('#do_number').val("");
     });
