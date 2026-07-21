279f7faf8db5bb7725647973d9164a8c52f1b426
ijonk7 <jookeo.rizal@gmail.com>
2026-07-21 15:18:55 +0700

    Update backend service 'report.form.documentForm.finance.getPaymentInstructionSummary'

--- Programming/.LaravelCore/app/Models/BackEnd/Database/SchData_OLTP_Finance/General.php
+++ Programming/.LaravelCore/app/Models/BackEnd/Database/SchData_OLTP_Finance/General.php
@@ -6051,7 +6051,7 @@ namespace App\Models\Database\SchData_OLTP_Finance
         +--------------------------------------------------------------------------------------------------------------------------+
         */
         public function getReport_Form_DocumentForm_PaymentInstructionSummary(
-            $varUserSession, int $varSysBranch_RefID, string  $varCombinedBudgetCode = null, string $varCombinedBudgetSectionCode = null, int $varSupplier_RefID = null, string  $varStartDate = null, string  $varEndDate = null,
+            $varUserSession, int $varSysBranch_RefID, string  $varCombinedBudgetCode = null, string $varCombinedBudgetSectionCode = null, int $varSupplier_RefID = null, string  $varStartDate = null, string  $varEndDate = null, array $varPagingStatement = null
             )
             {
             try {
@@ -6067,6 +6067,8 @@ namespace App\Models\Database\SchData_OLTP_Finance
                                 [$varSupplier_RefID, 'bigint' ],
                                 [$varStartDate, 'varchar'],
                                 [$varEndDate, 'varchar'],
+                                [$varPagingStatement['limit'], 'varchar'],
+                                [$varPagingStatement['offset'], 'bigint']
                             ]
                             )
                         );
