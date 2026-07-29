c7efaab075de0e53c440890b2a0892f9c2aaf92e
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-07-29 15:38:48 +0700

    feat: update token web

--- Programming/WebFrontEnd/routes/web.php
+++ Programming/WebFrontEnd/routes/web.php
@@ -28,7 +28,7 @@ $varUserSession =
     \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System();
 
 $varAPIWebToken =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoid2lzbnUudHJlbmdnb25vIiwiaWF0IjoxNzg1MjIzNzMyfQ.OGM5NDQ5NjdmYmRjNDJjNDBkMzlmMjRmN2U2NDNmODdmM2JkY2M5OGNjMzdkYjBmY2MwODRiNDkwYzI2NGQ2Yw';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dnZWRJbkFzIjoid2lzbnUudHJlbmdnb25vIiwiaWF0IjoxNzg0Njk2ODQxfQ.NGE1NTExYjA1OTcyYmI3NTgwOWU0NWZmYjAwZjMyNGUyOGViYTkzYmJhODYzZjgwNjliZmFhZWQ4MDM1Yjc2ZQ';
 
 \App\Helpers\ZhtHelper\System\FrontEnd\Helper_LaravelRoute::setDynamicRoute_Examples_APICall(
     \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
