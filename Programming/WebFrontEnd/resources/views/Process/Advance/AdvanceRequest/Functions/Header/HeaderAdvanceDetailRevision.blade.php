50baeff5b1ead4b84278001ac24988b96b1aee7c
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-08-07 10:33:00 +0700

    feat: menampilkan total payment advance

--- Programming/WebFrontEnd/resources/views/Process/Advance/AdvanceRequest/Functions/Header/HeaderAdvanceDetailRevision.blade.php
+++ Programming/WebFrontEnd/resources/views/Process/Advance/AdvanceRequest/Functions/Header/HeaderAdvanceDetailRevision.blade.php
@@ -8,14 +8,19 @@
                     <div>
                         <span style="border-radius:0;" class="input-group-text form-control">
                             <a href="javascript:;" id="myWorkerSecondTrigger" style="cursor: not-allowed;">
-                                <img src="{{ asset('AdminLTE-master/dist/img/box.png') }}" width="13" alt="myWorkerSecondTrigger">
+                                <img src="{{ asset('AdminLTE-master/dist/img/box.png') }}" width="13"
+                                    alt="myWorkerSecondTrigger">
                             </a>
                         </span>
                     </div>
                     <div style="flex: 100%;">
-                        <input id="worker_id_second" style="border-radius:0;" name="requester_id" class="form-control" hidden value="{{ $headerAdvanceRequestDetail['requesterId'] }}">
-                        <input id="worker_position_second" style="border-radius:0;" name="requester_detail" class="form-control" hidden value="{{ $headerAdvanceRequestDetail['requesterPosition'] }}">
-                        <input id="worker_name_second" style="border-radius:0;" name="requester" class="form-control" readonly value="{{ $headerAdvanceRequestDetail['requesterPosition'] . ' - ' . $headerAdvanceRequestDetail['requesterName'] }}">
+                        <input id="worker_id_second" style="border-radius:0;" name="requester_id" class="form-control"
+                            hidden value="{{ $headerAdvanceRequestDetail['requesterId'] }}">
+                        <input id="worker_position_second" style="border-radius:0;" name="requester_detail"
+                            class="form-control" hidden value="{{ $headerAdvanceRequestDetail['requesterPosition'] }}">
+                        <input id="worker_name_second" style="border-radius:0;" name="requester" class="form-control"
+                            readonly
+                            value="{{ $headerAdvanceRequestDetail['requesterPosition'] . ' - ' . $headerAdvanceRequestDetail['requesterName'] }}">
                     </div>
                 </div>
             </div>
@@ -32,10 +37,15 @@
                         </span>
                     </div>
                     <div style="flex: 100%;">
-                        <input id="beneficiary_second_id" style="border-radius:0;" name="beneficiary_id" class="form-control" hidden value="{{ $headerAdvanceRequestDetail['beneficiaryId'] }}">
-                        <input id="beneficiary_second_person_ref_id" style="border-radius:0;" name="person_refID" class="form-control" hidden value="{{ $headerAdvanceRequestDetail['person_RefId'] }}">
-                        <input id="beneficiary_second_person_name" style="border-radius:0;" name="beneficiary" class="form-control" hidden value="{{ $headerAdvanceRequestDetail['beneficiaryName'] }}">
-                        <input id="beneficiary_second_person_position" style="border-radius:0;" name="beneficiary_detail" class="form-control" size="17" readonly value="{{ $headerAdvanceRequestDetail['beneficiaryPosition'] . ' - ' . $headerAdvanceRequestDetail['beneficiaryName'] }}">
+                        <input id="beneficiary_second_id" style="border-radius:0;" name="beneficiary_id"
+                            class="form-control" hidden value="{{ $headerAdvanceRequestDetail['beneficiaryId'] }}">
+                        <input id="beneficiary_second_person_ref_id" style="border-radius:0;" name="person_refID"
+                            class="form-control" hidden value="{{ $headerAdvanceRequestDetail['person_RefId'] }}">
+                        <input id="beneficiary_second_person_name" style="border-radius:0;" name="beneficiary"
+                            class="form-control" hidden value="{{ $headerAdvanceRequestDetail['beneficiaryName'] }}">
+                        <input id="beneficiary_second_person_position" style="border-radius:0;"
+                            name="beneficiary_detail" class="form-control" size="17" readonly
+                            value="{{ $headerAdvanceRequestDetail['beneficiaryPosition'] . ' - ' . $headerAdvanceRequestDetail['beneficiaryName'] }}">
                     </div>
                 </div>
             </div>
@@ -54,28 +64,57 @@
                         </span>
                     </div>
                     <div style="flex: 100%;">
-                        <input id="bank_name_second_id" style="border-radius:0;" class="form-control" name="bank_code" hidden value="{{ $headerAdvanceRequestDetail['bankId'] }}">
-                        <input id="bank_name_second_name" style="border-radius:0;" name="bank_name" class="form-control" hidden value="{{ $headerAdvanceRequestDetail['bankAcronym'] }}">
-                        <input id="bank_name_second_detail" style="border-radius:0;" class="form-control" name="bank_name_detail" readonly value="{{ $headerAdvanceRequestDetail['bankAcronym'] . ' - ' . $headerAdvanceRequestDetail['bankName'] }}">
+                        <input id="bank_name_second_id" style="border-radius:0;" class="form-control" name="bank_code"
+                            hidden value="{{ $headerAdvanceRequestDetail['bankId'] }}">
+                        <input id="bank_name_second_name" style="border-radius:0;" name="bank_name" class="form-control"
+                            hidden value="{{ $headerAdvanceRequestDetail['bankAcronym'] }}">
+                        <input id="bank_name_second_detail" style="border-radius:0;" class="form-control"
+                            name="bank_name_detail" readonly
+                            value="{{ $headerAdvanceRequestDetail['bankAcronym'] . ' - ' . $headerAdvanceRequestDetail['bankName'] }}">
                     </div>
                 </div>
             </div>
 
-            <!-- BANK ACCOUNT -->
-            <div class="row">
+            <!-- ACCOUNT NUMBER -->
+            <div class="row" style="margin-bottom: 1rem;">
                 <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">Account Number</label>
                 <div class="col-5 d-flex">
                     <div>
                         <span style="border-radius:0;" class="input-group-text form-control">
-                            <a href="javascript:;" id="myBankAccountTrigger" data-toggle="modal" data-target="#myBankAccount" class="myBankAccount" style="cursor: not-allowed;">
+                            <a href="javascript:;" id="myBankAccountTrigger" data-toggle="modal"
+                                data-target="#myBankAccount" class="myBankAccount" style="cursor: not-allowed;">
                                 <img src="{{ asset('AdminLTE-master/dist/img/box.png') }}" width="13" alt="">
                             </a>
                         </span>
                     </div>
                     <div style="flex: 100%;">
-                        <input id="bank_accounts_id" style="border-radius:0;" class="form-control" name="bank_account_id" hidden value="{{ $headerAdvanceRequestDetail['bankAccountId'] }}">
-                        <input id="bank_accounts" style="border-radius:0;" name="bank_account" class="form-control number-without-characters" hidden value="{{ $headerAdvanceRequestDetail['bankAccountNumber'] }}">
-                        <input id="bank_accounts_detail" style="border-radius:0;" class="form-control" name="bank_account_detail" readonly value="{{ $headerAdvanceRequestDetail['bankAccountNumber'] . ' - ' .$headerAdvanceRequestDetail['bankAccountName'] }}">
+                        <input id="bank_accounts_id" style="border-radius:0;" class="form-control"
+                            name="bank_account_id" hidden value="{{ $headerAdvanceRequestDetail['bankAccountId'] }}">
+                        <input id="bank_accounts" style="border-radius:0;" name="bank_account"
+                            class="form-control number-without-characters" hidden
+                            value="{{ $headerAdvanceRequestDetail['bankAccountNumber'] }}">
+                        <input id="bank_accounts_detail" style="border-radius:0;" class="form-control"
+                            name="bank_account_detail" readonly
+                            value="{{ $headerAdvanceRequestDetail['bankAccountNumber'] . ' - ' . $headerAdvanceRequestDetail['bankAccountName'] }}">
+                    </div>
+                </div>
+            </div>
+
+            <!-- TOTAL PAYMENT -->
+            <div class="row">
+                <label class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">Total Payment</label>
+                <div class="col-5 d-flex">
+                    <div>
+                        <span style="border-radius:0;" class="input-group-text form-control">
+                            <a href="javascript:;" id="myBankAccountTrigger" data-toggle="modal"
+                                data-target="#myBankAccount" class="myBankAccount" style="cursor: not-allowed;">
+                                <img src="{{ asset('AdminLTE-master/dist/img/box.png') }}" width="13" alt="">
+                            </a>
+                        </span>
+                    </div>
+                    <div style="flex: 100%;">
+                        <input id="total_payment_arf" style="border-radius:0;" class="form-control"
+                            name="total_payment_arf" readonly value="{{ $headerAdvanceRequestDetail['totalPayment'] }}">
                     </div>
                 </div>
             </div>
