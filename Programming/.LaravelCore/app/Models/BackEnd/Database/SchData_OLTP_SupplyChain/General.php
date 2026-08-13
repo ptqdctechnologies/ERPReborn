6e50aed62520a9017a3727559ef4d70a010003e2
ijonk7 <jookeo.rizal@gmail.com>
2026-08-13 15:44:32 +0700

    Update backend service 'transaction.read.dataList.supplyChain.getPurchaseRequisitionDetail'

--- Programming/.LaravelCore/app/Models/BackEnd/Database/SchData_OLTP_SupplyChain/General.php
+++ Programming/.LaravelCore/app/Models/BackEnd/Database/SchData_OLTP_SupplyChain/General.php
@@ -3050,6 +3050,7 @@ namespace App\Models\Database\SchData_OLTP_SupplyChain
                             $varReturn['data'][$idxArray]['workStructure_RefID'] = $value["WorkStructure_RefID"];
                             $varReturn['data'][$idxArray]['workCode'] = $value["WorkCode"];
                             $varReturn['data'][$idxArray]['workName'] = $value["WorkName"];
+                            $varReturn['data'][$idxArray]['po_Value'] = $value["PO_Value"];
                             $varReturn['data'][$idxArray]['orderSequence'] = $value["OrderSequence"];
                             $idxArray++;
                         }
