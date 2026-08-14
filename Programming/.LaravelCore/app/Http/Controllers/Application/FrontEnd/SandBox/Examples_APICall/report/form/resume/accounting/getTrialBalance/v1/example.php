966c04809fce8af0428e057dd8c971caad1a1ae6
PT. QDC Technologies <pt.qdc.technologies@gmail.com>
2026-08-14 16:18:58 +0700

    Penambahan API report.form.resume.accounting.getTrialBalance, report.form.resume.accounting.getProfitLoss, dan report.form.resume.accounting.getBalanceSheet

---
+++ Programming/.LaravelCore/app/Http/Controllers/Application/FrontEnd/SandBox/Examples_APICall/report/form/resume/accounting/getTrialBalance/v1/example.php
@@ -0,0 +1,106 @@
+<?php
+
+/*
++----------------------------------------------------------------------------------------------------------------------------------+
+| ▪ Category    : Example - API Call Controller                                                                                    |
+| ▪ Name Space  : \App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\report\form\resume\accounting                |
+|                 \getTrialBalance\v1                                                                                              |
+| ▪ API Key     : report.form.resume.accounting.getTrialBalance                                                                    |
+| ▪ API Version : 1                                                                                                                |
+|                                                                                                                                  |
+| ▪ Copyleft 🄯 2026 Zheta (teguhpjs@gmail.com)                                                                                     |
++----------------------------------------------------------------------------------------------------------------------------------+
+*/
+namespace App\Http\Controllers\Application\FrontEnd\SandBox\Examples_APICall\report\form\resume\accounting\getTrialBalance\v1
+    {
+    class example extends \App\Http\Controllers\Controller
+        {
+        /*
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Call URL        : http(s)://<HOST>/report.form.resume.accounting.getTrialBalance.v1_throughAPIGateway                  |
+        |                     ► http://172.28.0.4/report.form.resume.accounting.getTrialBalance.v1_throughAPIGateway               |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Version         : 1.0000.0000000                                                                                       |
+        | ▪ Last Update     : 2026-08-14                                                                                           |
+        | ▪ Creation Date   : 2026-08-14                                                                                           |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        */
+        public function throughAPIGateway($varAPIWebToken)
+            {
+            //-----[ PARAMETER SET ]-----
+            if (!$varAPIWebToken) {
+                $varAPIWebToken =
+                    \App\Helpers\ZhtHelper\System\Helper_Environment::getAPIWebToken_System();
+                }
+
+            //-----[ CORE ]-----
+            $varData =
+                \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGateway(
+                    //-----[ METADATA ]-----( START )-----
+                        \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
+                        $varAPIWebToken, 
+                        'report.form.resume.accounting.getTrialBalance', 
+                        'latest',
+                    //-----[ METADATA ]-----(  END  )-----
+
+                    //-----[ DATA ]---------( START )---
+                        [
+                        'parameter' => [
+                            'startDate' => '2024-01-01',
+                            'finishDate' => '2024-12-31'
+                            ]
+                        ]
+                    //-----[ DATA ]---------(  END  )-----
+                    );
+
+            //-----[ DATA RETURN ]-----
+            return
+                $varData;
+            }
+
+        /*
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Call URL        : http(s)://<HOST>/                                                                                    |
+        |                     report.form.resume.accounting.getTrialBalance.v1_throughAPIGatewayJQuery     |
+        |                     ► http://172.28.0.4/                                                                                 |
+        |                       report.form.resume.accounting.getTrialBalance.v1_throughAPIGatewayJQuery   |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        | ▪ Version         : 1.0000.0000000                                                                                       |
+        | ▪ Last Update     : 2025-01-09                                                                                           |
+        | ▪ Creation Date   : 2025-01-09                                                                                           |
+        +--------------------------------------------------------------------------------------------------------------------------+
+        */
+        public function throughAPIGatewayJQuery($varAPIWebToken)
+            {
+            //---Parameter Set---
+            if (!$varAPIWebToken) {
+                $varAPIWebToken =
+                    \App\Helpers\ZhtHelper\System\Helper_Environment::getAPIWebToken_System();
+                }
+
+            //---Core---
+            echo \App\Helpers\ZhtHelper\General\Helper_JavaScript::setLibrary(\App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System());
+
+            echo '<table border="1" style="border-collapse: collapse;">';
+            echo    '<tr><td colspan="2" bgcolor="#6666cc" align="middle"><p style="color:#ffffff">Business Document Issuance Disposition Count Main Data</p></td></tr>';
+            echo        '<tr><td>RecordID</td><td><input type="text" id="dataInput_RecordID" value="164000000000196"></td></tr>';
+            echo '</table><br>';
+
+            $varJQueryFunction =
+                \App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall::setCallAPIGatewayJQuery(
+                    \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(), 
+                    $varAPIWebToken, 
+                    'report.form.resume.accounting.getTrialBalance', 
+                    'latest', 
+                    '{'.
+                        '"parameter" : {'.
+                            '"recordID" : parseInt(document.getElementById("dataInput_RecordID").value)'.
+                            '}'.
+                    '}'
+                    ); 
+            echo "<button type='button' onclick='javascript:var varData = ".$varJQueryFunction."; $(\"body\").append(JSON.stringify(varData));'>Submit Data</button>";
+
+            dd($varJQueryFunction);
+            }
+        }
+    }
