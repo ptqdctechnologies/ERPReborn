6bf7c003c65f816ce0753a399c989da2156a0ee5
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-08-20 10:59:29 +0700

    feat: add validate currency

--- Programming/WebFrontEnd/resources/views/Master/Rate/Functions/Footer/revision.blade.php
+++ Programming/WebFrontEnd/resources/views/Master/Rate/Functions/Footer/revision.blade.php
@@ -4,10 +4,16 @@
         const code = $(this).find('td:nth-child(2)').text();
         const name = $(this).find('td:nth-child(3)').text();
 
-        $("#currency_id").val(sysId);
-        $("#currency_code").val(code);
-        $("#currency_name").val(`${code} - ${name}`);
-        $("#currency_name").css({ "background-color": "#e9ecef", "border": "1px solid #ced4da" });
+        if (code != "USD" && code != "IDR" && code != "EUR") {
+            Swal.fire("Error", "Please Call Accounting Staffs to Input Current Exchange Rate. Thank You.", "error");
+
+            return;
+        } else {
+            $("#currency_id").val(sysId);
+            $("#currency_code").val(code);
+            $("#currency_name").val(`${code} - ${name}`);
+            $("#currency_name").css({ "background-color": "#e9ecef", "border": "1px solid #ced4da" });
+        }
 
         $('#myCurrencies').modal('toggle');
     });
