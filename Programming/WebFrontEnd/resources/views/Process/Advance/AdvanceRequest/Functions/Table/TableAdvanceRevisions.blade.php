e73906b7aed0581d9000380f71f021878f2e62de
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-09-07 09:41:26 +0700

    feat: add asf value in arf revision

--- Programming/WebFrontEnd/resources/views/Process/Advance/AdvanceRequest/Functions/Table/TableAdvanceRevisions.blade.php
+++ Programming/WebFrontEnd/resources/views/Process/Advance/AdvanceRequest/Functions/Table/TableAdvanceRevisions.blade.php
@@ -3,18 +3,36 @@
     <table class="table table-head-fixed text-nowrap table-sm" id="tableGetBudgetDetails">
         <thead>
             <tr>
-                <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">Work</th>
-                <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">Product</th>
-                <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">Qty Budget</th>
-                <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">Qty Avail</th>
-                <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">UOM</th>
-                <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">Price</th>
-                <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">Total Budget</th>
-                <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">Currency</th>
-                <th class="sticky-col forth-col-arf" style="padding-top: 10px;padding-bottom: 10px;text-align: center;background-color:#4B586A;color:white;">Qty Req</th>
-                <th class="sticky-col third-col-arf" style="padding-top: 10px;padding-bottom: 10px;text-align: center;background-color:#4B586A;color:white;">Price Req</th>
-                <th class="sticky-col second-col-arf" style="padding-top: 10px;padding-bottom: 10px;text-align: center;background-color:#4B586A;color:white;">Total Req</th>
-                <th class="sticky-col first-col-arf" style="padding-top: 10px;padding-bottom: 10px;text-align: center;background-color:#4B586A;color:white;">Balance Qty</th>
+                <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">
+                    Work</th>
+                <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">
+                    Product</th>
+                <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">
+                    Qty Budget</th>
+                <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">
+                    Qty Avail</th>
+                <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">
+                    UOM</th>
+                <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">
+                    Price</th>
+                <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">
+                    Total Budget</th>
+                <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">
+                    ASF Value</th>
+                <th style="padding-top: 10px;padding-bottom: 10px;border-right:1px solid #e9ecef;text-align: center;">
+                    Currency</th>
+                <th class="sticky-col forth-col-arf"
+                    style="padding-top: 10px;padding-bottom: 10px;text-align: center;background-color:#4B586A;color:white;">
+                    Qty Req</th>
+                <th class="sticky-col third-col-arf"
+                    style="padding-top: 10px;padding-bottom: 10px;text-align: center;background-color:#4B586A;color:white;">
+                    Price Req</th>
+                <th class="sticky-col second-col-arf"
+                    style="padding-top: 10px;padding-bottom: 10px;text-align: center;background-color:#4B586A;color:white;">
+                    Total Req</th>
+                <th class="sticky-col first-col-arf"
+                    style="padding-top: 10px;padding-bottom: 10px;text-align: center;background-color:#4B586A;color:white;">
+                    Balance Qty</th>
             </tr>
         </thead>
         <tbody></tbody>
@@ -34,7 +52,8 @@
             <tr class="errorMessageContainerBudgetDetails">
                 <td colspan="11" class="p-0" style="border: 0px;">
                     <div class="d-flex flex-column justify-content-center align-items-center py-3">
-                        <div id="errorMessageBudgetDetails" class="mt-3 text-red" style="font-size: 1rem; font-weight: 700;"></div>
+                        <div id="errorMessageBudgetDetails" class="mt-3 text-red"
+                            style="font-size: 1rem; font-weight: 700;"></div>
                     </div>
                 </td>
             </tr>
