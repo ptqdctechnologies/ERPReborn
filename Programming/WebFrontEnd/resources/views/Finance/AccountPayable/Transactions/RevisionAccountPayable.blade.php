6fc440edc40d479d9f36a202ea9a1f3913c99f29
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-09-08 15:02:15 +0700

    feat: adjust currency input in account payable revision

--- Programming/WebFrontEnd/resources/views/Finance/AccountPayable/Transactions/RevisionAccountPayable.blade.php
+++ Programming/WebFrontEnd/resources/views/Finance/AccountPayable/Transactions/RevisionAccountPayable.blade.php
@@ -1,110 +1,115 @@
 @extends('Partials.app')
 @section('main')
-@include('Partials.navbar')
-@include('Partials.sidebar')
-@include('getFunction.getWorkFlows')
-@include('getFunction.getCategory')
-@include('getFunction.getAccountPayable')
-@include('getFunction.getChartOfAccount')
-@include('getFunction.getPaymentTransfer')
-@include('Finance.AccountPayable.Functions.PopUp.PopUpAccountPayableRevision')
-@include('Finance.AccountPayable.Functions.PopUp.PopUpAccountPayableSummaryData')
+  @include('Partials.navbar')
+  @include('Partials.sidebar')
+  @include('getFunction.getWorkFlows')
+  @include('getFunction.getCurrencies')
+  @include('getFunction.getCategory')
+  @include('getFunction.getAccountPayable')
+  @include('getFunction.getChartOfAccount')
+  @include('getFunction.getPaymentTransfer')
+  @include('Finance.AccountPayable.Functions.PopUp.PopUpAccountPayableRevision')
+  @include('Finance.AccountPayable.Functions.PopUp.PopUpAccountPayableSummaryData')
 
-<div class="content-wrapper">
-  <section class="content">
-    <div class="container-fluid">
-      <!-- TITLE -->
-      <div class="row mb-1" style="background-color:#4B586A;">
-        <div class="col-sm-6" style="height:30px;">
-          <label style="font-size:15px;position:relative;top:7px;color:white;">
-            Revision Account Payable
-          </label>
-        </div>
-      </div>
-
-      @include('Finance.AccountPayable.Functions.Menu.MenuAccountPayable')
-
-      <div class="card">
-        <input type="hidden" name="DocumentTypeID" id="DocumentTypeID" value="<?= $documentTypeRefID; ?>">
-        <input type="hidden" name="var_combinedBudget_RefID" id="var_combinedBudget_RefID" value="<?= $header['combinedBudgetRefID']; ?>">
-
-        <!-- PO INFORMATION -->
-        <div class="tab-content px-3 pt-4 pb-2" id="nav-tabContent">
-          <div class="row">
-            <div class="col-12">
-              <div class="card">
-                <!-- HEADER -->
-                <div class="card-header">
-                  <label class="card-title">
-                    PO Information
-                  </label>
-                  <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                      <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
-                    </button>
-                  </div>
-                </div>
-
-                @include('Finance.AccountPayable.Functions.Header.HeaderPORevisionAccountPayable')
-              </div>
-            </div>
+  <div class="content-wrapper">
+    <section class="content">
+      <div class="container-fluid">
+        <!-- TITLE -->
+        <div class="row mb-1" style="background-color:#4B586A;">
+          <div class="col-sm-6" style="height:30px;">
+            <label style="font-size:15px;position:relative;top:7px;color:white;">
+              Revision Account Payable
+            </label>
           </div>
         </div>
 
-        <!-- ACCOUNT PAYABLE -->
-        <div class="tab-content px-3 pb-2" id="nav-tabContent">
-          <div class="row">
-            <div class="col-12">
-              <div class="card">
-                <!-- HEADER -->
-                <div class="card-header">
-                  <label class="card-title">
-                    Account Payable 
-                  </label>
-                  <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                      <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
-                    </button>
-                  </div>
-                </div>
+        @include('Finance.AccountPayable.Functions.Menu.MenuAccountPayable')
 
-                @include('Finance.AccountPayable.Functions.Header.HeaderRevisionAccountPayable')
+        <div class="card">
+          <input type="hidden" name="DocumentTypeID" id="DocumentTypeID" value="<?= $documentTypeRefID; ?>">
+          <input type="hidden" name="var_combinedBudget_RefID" id="var_combinedBudget_RefID"
+            value="<?= $header['combinedBudgetRefID']; ?>">
+
+          <!-- PO INFORMATION -->
+          <div class="tab-content px-3 pt-4 pb-2" id="nav-tabContent">
+            <div class="row">
+              <div class="col-12">
+                <div class="card">
+                  <!-- HEADER -->
+                  <div class="card-header">
+                    <label class="card-title">
+                      PO Information
+                    </label>
+                    <div class="card-tools">
+                      <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
+                      </button>
+                    </div>
+                  </div>
+
+                  @include('Finance.AccountPayable.Functions.Header.HeaderPORevisionAccountPayable')
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <!-- FILE ATTACHMENT -->
-        <div class="tab-content px-3 pb-2" id="nav-tabContent">
-          <div class="row">
-            <div class="col-12">
-              <div class="card">
-                <!-- HEADER -->
-                <div class="card-header">
-                  <label class="card-title">
-                    Attachment
-                  </label>
-                  <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                      <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
-                    </button>
+          <!-- ACCOUNT PAYABLE -->
+          <div class="tab-content px-3 pb-2" id="nav-tabContent">
+            <div class="row">
+              <div class="col-12">
+                <div class="card">
+                  <!-- HEADER -->
+                  <div class="card-header">
+                    <label class="card-title">
+                      Account Payable
+                    </label>
+                    <div class="card-tools">
+                      <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
+                      </button>
+                    </div>
                   </div>
-                </div>
 
-                <!-- BODY -->
-                <div class="card-body">
-                  <div class="row py-3">
-                    <div class="col-lg-5">
-                      <div class="row">
-                        <div class="col p-0">
-                          <input type="text" id="dataInput_Log_FileUpload" name="dataInput_Log_FileUpload_1" style="display:none">
-                          <?php echo \App\Helpers\ZhtHelper\General\Helper_JavaScript::getSyntaxCreateDOM_DivCustom_InputFile(\App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
-                            $varAPIWebToken,
-                            'dataInput_Log_FileUpload',
-                            null,
-                            'dataInput_Return'
-                            ).
-                          ''; ?>
+                  @include('Finance.AccountPayable.Functions.Header.HeaderRevisionAccountPayable')
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- FILE ATTACHMENT -->
+          <div class="tab-content px-3 pb-2" id="nav-tabContent">
+            <div class="row">
+              <div class="col-12">
+                <div class="card">
+                  <!-- HEADER -->
+                  <div class="card-header">
+                    <label class="card-title">
+                      Attachment
+                    </label>
+                    <div class="card-tools">
+                      <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
+                      </button>
+                    </div>
+                  </div>
+
+                  <!-- BODY -->
+                  <div class="card-body">
+                    <div class="row py-3">
+                      <div class="col-lg-5">
+                        <div class="row">
+                          <div class="col p-0">
+                            <input type="text" id="dataInput_Log_FileUpload" name="dataInput_Log_FileUpload_1"
+                              style="display:none">
+                            <?php echo \App\Helpers\ZhtHelper\General\Helper_JavaScript::getSyntaxCreateDOM_DivCustom_InputFile(
+    \App\Helpers\ZhtHelper\System\Helper_Environment::getUserSessionID_System(),
+    $varAPIWebToken,
+    'dataInput_Log_FileUpload',
+    null,
+    'dataInput_Return'
+  ) .
+    ''; ?>
+                          </div>
                         </div>
                       </div>
                     </div>
@@ -113,50 +118,53 @@
               </div>
             </div>
           </div>
-        </div>
 
-        <!-- ACCOUNT PAYABLE DETAILS -->
-        <div class="tab-content px-3 pb-2" id="nav-tabContent">
-          <div class="row">
-            <div class="col-12">
-              <div class="card">
-                <!-- HEADER -->
-                <div class="card-header">
-                  <label class="card-title">
-                    Account Payable Details
-                  </label>
-                  <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                      <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
-                    </button>
+          <!-- ACCOUNT PAYABLE DETAILS -->
+          <div class="tab-content px-3 pb-2" id="nav-tabContent">
+            <div class="row">
+              <div class="col-12">
+                <div class="card">
+                  <!-- HEADER -->
+                  <div class="card-header">
+                    <label class="card-title">
+                      Account Payable Details
+                    </label>
+                    <div class="card-tools">
+                      <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-angle-down btn-sm" style="color:black;"></i>
+                      </button>
+                    </div>
                   </div>
-                </div>
 
-                @include('Finance.AccountPayable.Functions.Table.TableAccountPayable')
+                  @include('Finance.AccountPayable.Functions.Table.TableAccountPayable')
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- BUTTON -->
+          <div class="tab-content px-3 pb-2" id="nav-tabContent">
+            <div class="row">
+              <div class="col">
+                <button type="button" class="btn btn-default btn-sm float-right" onclick="validationForm()"
+                  style="margin-left: 5px;background-color:#e9ecef;border:1px solid #ced4da;">
+                  <img src="{{ asset('AdminLTE-master/dist/img/save.png') }}" width="13" alt=""
+                    title="Submit to Account Payable"> Submit
+                </button>
+
+                <a onclick="cancelForm('{{ route('AccountPayable.index', ['var' => 1]) }}')"
+                  class="btn btn-default btn-sm float-right" style="background-color:#e9ecef;border:1px solid #ced4da;">
+                  <img src="{{ asset('AdminLTE-master/dist/img/cancel.png') }}" width="13" alt=""
+                    title="Cancel Account Payable List Cart"> Cancel
+                </a>
               </div>
             </div>
           </div>
         </div>
-
-        <!-- BUTTON -->
-        <div class="tab-content px-3 pb-2" id="nav-tabContent">
-          <div class="row">
-            <div class="col">
-              <button type="button" class="btn btn-default btn-sm float-right" onclick="validationForm()" style="margin-left: 5px;background-color:#e9ecef;border:1px solid #ced4da;">
-                <img src="{{ asset('AdminLTE-master/dist/img/save.png') }}" width="13" alt="" title="Submit to Account Payable"> Submit
-              </button>
-
-              <a onclick="cancelForm('{{ route('AccountPayable.index', ['var' => 1]) }}')" class="btn btn-default btn-sm float-right" style="background-color:#e9ecef;border:1px solid #ced4da;">
-                <img src="{{ asset('AdminLTE-master/dist/img/cancel.png') }}" width="13" alt="" title="Cancel Account Payable List Cart"> Cancel
-              </a>
-            </div>
-          </div>
-        </div>
       </div>
-    </div>
-  </section>
-</div>
+    </section>
+  </div>
 
-@include('Partials.footer')
-@include('Finance.AccountPayable.Functions.Footer.FooterRevisionAccountPayable')
+  @include('Partials.footer')
+  @include('Finance.AccountPayable.Functions.Footer.FooterRevisionAccountPayable')
 @endsection
