d2238912f6d308875de243450aff4299b4c3f705
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2026-09-14 11:18:49 +0700

    feat: add total payment reimbursement

--- Programming/WebFrontEnd/resources/views/Process/Reimbursement/Functions/Header/HeaderRevisionReimbursementDetail.blade.php
+++ Programming/WebFrontEnd/resources/views/Process/Reimbursement/Functions/Header/HeaderRevisionReimbursementDetail.blade.php
@@ -13,10 +13,21 @@
             </span>
           </div>
           <div style="flex: 100%;">
-            <input id="customer_name" class="form-control" style="border-radius:0;" readonly value="<?= $header['customerCode'] . " - " . $header['customerName']; ?>" />
-            <input id="customer_code" class="form-control" style="border-radius:0;" hidden value="<?= $header['customerCode']; ?>" />
-            <input id="customer_id" class="form-control" style="border-radius:0;" hidden value="<?= $header['customer_RefID']; ?>" name="customer_id" />
+            <input id="customer_name" class="form-control" style="border-radius:0;" readonly
+              value="<?= $header['customerCode'] . " - " . $header['customerName']; ?>" />
+            <input id="customer_code" class="form-control" style="border-radius:0;" hidden
+              value="<?= $header['customerCode']; ?>" />
+            <input id="customer_id" class="form-control" style="border-radius:0;" hidden
+              value="<?= $header['customer_RefID']; ?>" name="customer_id" />
           </div>
+        </div>
+      </div>
+
+      <!-- TOTAL PAYMENT -->
+      <div class="row" style="margin-top: 1rem;">
+        <label class="col-sm-3 col-md-4 col-lg-3 col-form-label p-0">Total Payment</label>
+        <div class="col-5">
+          <input id="total_payment" class="form-control" style="border-radius:0;" readonly />
         </div>
       </div>
     </div>
@@ -24,7 +35,8 @@
     <div class="col-md-12 col-lg-5">
       <!-- BENEFICIARY -->
       <div class="row">
-        <label for="beneficiary_second_person_position" class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">Beneficiary</label>
+        <label for="beneficiary_second_person_position"
+          class="col-sm-3 col-md-4 col-lg-4 col-form-label p-0">Beneficiary</label>
         <div class="col-5 d-flex">
           <div>
             <span style="border-radius:0;" class="input-group-text form-control">
@@ -34,10 +46,14 @@
             </span>
           </div>
           <div style="flex: 100%;">
-            <input id="beneficiary_second_person_name" class="form-control" style="border-radius:0;" readonly value="<?= $header['beneficiaryPosition'] . " - " . $header['beneficiaryName']; ?>" />
-            <input id="beneficiary_second_person_position" class="form-control" style="border-radius:0;" hidden value="<?= $header['beneficiaryPosition']; ?>" />
-            <input id="beneficiary_second_id" class="form-control" style="border-radius:0;" hidden value="<?= $header['beneficiary_RefID']; ?>" name="beneficiary_id" />
-            <input id="beneficiary_second_person_ref_id" class="form-control" style="border-radius:0;" hidden name="person_refID" />
+            <input id="beneficiary_second_person_name" class="form-control" style="border-radius:0;" readonly
+              value="<?= $header['beneficiaryPosition'] . " - " . $header['beneficiaryName']; ?>" />
+            <input id="beneficiary_second_person_position" class="form-control" style="border-radius:0;" hidden
+              value="<?= $header['beneficiaryPosition']; ?>" />
+            <input id="beneficiary_second_id" class="form-control" style="border-radius:0;" hidden
+              value="<?= $header['beneficiary_RefID']; ?>" name="beneficiary_id" />
+            <input id="beneficiary_second_person_ref_id" class="form-control" style="border-radius:0;" hidden
+              name="person_refID" />
           </div>
         </div>
       </div>
@@ -54,9 +70,12 @@
             </span>
           </div>
           <div style="flex: 100%;">
-            <input id="bank_name_second_detail" class="form-control" style="border-radius:0;" readonly value="<?= $header['beneficiaryBankAcronym'] . " - " . $header['beneficiaryBankName']; ?>" />
-            <input id="bank_name_second_name" class="form-control" style="border-radius:0;" hidden value="<?= $header['beneficiaryBankAcronym']; ?>" />
-            <input id="bank_name_second_id" class="form-control" style="border-radius:0;" hidden value="<?= $header['beneficiaryBank_RefID']; ?>" name="bank_code" />
+            <input id="bank_name_second_detail" class="form-control" style="border-radius:0;" readonly
+              value="<?= $header['beneficiaryBankAcronym'] . " - " . $header['beneficiaryBankName']; ?>" />
+            <input id="bank_name_second_name" class="form-control" style="border-radius:0;" hidden
+              value="<?= $header['beneficiaryBankAcronym']; ?>" />
+            <input id="bank_name_second_id" class="form-control" style="border-radius:0;" hidden
+              value="<?= $header['beneficiaryBank_RefID']; ?>" name="bank_code" />
           </div>
         </div>
       </div>
@@ -73,9 +92,12 @@
             </span>
           </div>
           <div style="flex: 100%;">
-            <input id="bank_accounts_detail" class="form-control" style="border-radius:0;" readonly value="<?= $header['beneficiaryBankAccountNumber'] . " - " . $header['beneficiaryBankAccountName']; ?>" />
-            <input id="bank_accounts" class="form-control" style="border-radius:0;" hidden value="<?= $header['beneficiaryBankAccountNumber']; ?>" />
-            <input id="bank_accounts_id" class="form-control" style="border-radius:0;" hidden value="<?= $header['beneficiaryBankAccount_RefID']; ?>" name="bank_account_id" />
+            <input id="bank_accounts_detail" class="form-control" style="border-radius:0;" readonly
+              value="<?= $header['beneficiaryBankAccountNumber'] . " - " . $header['beneficiaryBankAccountName']; ?>" />
+            <input id="bank_accounts" class="form-control" style="border-radius:0;" hidden
+              value="<?= $header['beneficiaryBankAccountNumber']; ?>" />
+            <input id="bank_accounts_id" class="form-control" style="border-radius:0;" hidden
+              value="<?= $header['beneficiaryBankAccount_RefID']; ?>" name="bank_account_id" />
           </div>
         </div>
       </div>
