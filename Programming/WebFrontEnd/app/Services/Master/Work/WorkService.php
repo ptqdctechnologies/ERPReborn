f291d2df09370e3f63b7972422ced99731e9970e
PT. QDC Technologies <pt.qdc.technologies@gmail.com>
2026-09-15 17:22:56 +0700

    API Key Reclass : 1. report.form.dataPickList.master.getBank --> report.form.dataPickList.master.bank.getAll 2. report.form.dataPickList.master.getBankAccount --> report.form.dataPickList.master.bankAccount.getAll 3. report.form.dataPickList.master.getInstitutionType --> report.form.dataPickList.master.institutionType.getAll 4. report.form.dataPickList.master.getPerson --> report.form.dataPickList.master.person.getAll 5. report.form.dataPickList.master.getWorkStructure --> report.form.dataPickList.master.workStructure.getAll 6. report.form.dataList.master.getWorkStructure --> report.form.dataList.master.workStructure.getAll

--- Programming/WebFrontEnd/app/Services/Master/Work/WorkService.php
+++ Programming/WebFrontEnd/app/Services/Master/Work/WorkService.php
@@ -7,85 +7,89 @@ use App\Helpers\ZhtHelper\System\FrontEnd\Helper_APICall;
 use App\Helpers\ZhtHelper\System\Helper_Environment;
 
 class WorkService
-{
-    public function create($request)
     {
+    public function create($request)
+        {
         $token = Session::get('SessionLogin');
 
-        return Helper_APICall::setCallAPIGateway(
-            Helper_Environment::getUserSessionID_System(),
-            $token,
-            'transaction.create.master.setWorkStructure',
-            'latest',
-            [
+        return
+            Helper_APICall::setCallAPIGateway(
+                Helper_Environment::getUserSessionID_System(),
+                $token,
+                'transaction.create.master.setWorkStructure',
+                'latest',
+                [
                 'entities' => [
                     "code" => $request->work_code,
                     "name" => $request->work_name
+                    ]
                 ]
-            ]
-        );
-    }
+                );
+            }
 
     public function revision($request, $id)
-    {
+        {
         $token = Session::get('SessionLogin');
 
-        return Helper_APICall::setCallAPIGateway(
-            Helper_Environment::getUserSessionID_System(),
-            $token,
-            'transaction.update.master.setWorkStructure',
-            'latest',
-            [
+        return
+            Helper_APICall::setCallAPIGateway(
+                Helper_Environment::getUserSessionID_System(),
+                $token,
+                'transaction.update.master.setWorkStructure',
+                'latest',
+                [
                 'recordID' => (int) $id,
                 'entities' => [
                     "code" => $request->work_code,
                     "name" => $request->work_name,
                     "status" => (int) $request->work_status
+                    ]
                 ]
-            ]
-        );
-    }
+            );
+        }
 
     public function detail($workCode)
-    {
+        {
         $token = Session::get('SessionLogin');
 
-        return Helper_APICall::setCallAPIGateway(
-            Helper_Environment::getUserSessionID_System(),
-            $token,
-            'report.form.dataList.master.getWorkStructure',
-            'latest',
-            [
+        return
+            Helper_APICall::setCallAPIGateway(
+                Helper_Environment::getUserSessionID_System(),
+                $token,
+                'report.form.dataList.master.workStructure.getAll',
+                'latest',
+                [
                 'parameter' => [
                     'pagination' => [
                         'pageSize' => 10,
                         'pageShow' => 1
-                    ],
+                        ],
                     'dataFilter' => [
                         'name' => NULL,     //'Bank'
                         'code' => $workCode   //'BCA'
-                    ],
+                        ],
+                    ]
                 ]
-            ]
-        );
-    }
+                );
+        }
 
     public function picklist($formatted)
-    {
+        {
         $sessionToken = Session::get('SessionLogin');
 
-        return Helper_APICall::setCallAPIGateway(
-            Helper_Environment::getUserSessionID_System(),
-            $sessionToken,
-            'report.form.dataPickList.master.getWorkStructure',
-            'latest',
-            [
+        return
+            Helper_APICall::setCallAPIGateway(
+                Helper_Environment::getUserSessionID_System(),
+                $sessionToken,
+                'report.form.dataPickList.master.workStructure.getAll',
+                'latest',
+                [
                 'parameter' => $formatted
-            ]
-        );
-    }
+                ]
+                );
+        }
 
     public function summary($search, $limit = 10, $offset = 0)
-    {
+        {
+        }
     }
-}
